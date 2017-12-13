96174f4821c9692febd33b90504d9519ffe1823b
Aicha <34491280+AkinAicha@users.noreply.github.com>
2017-12-13 21:23:09 +0100

    Update poll.php

--- resources/lang/de/poll.php
+++ resources/lang/de/poll.php
@@ -6,14 +6,14 @@
 */
 
 return [
-    'poll' => 'Poll',
-    'polls' => 'Polls',
-    'current' => 'Current Poll(s)',
-    'total' => 'Total Votes Ever',
-    'vote-now' => 'Get Your Vote In Now!',
-    'results' => 'Poll Results',
-    'multiple-choice' => 'This is a multiple choice poll. Select as many answers as you like.',
-    'ip-checking' => 'This poll has duplicate vote checking. You can only vote once.',
-    'vote' => 'Vote',
-    'votes' => 'Votes',
+    'poll' => 'Umfrage',
+    'polls' => 'Umfragen',
+    'current' => 'Neuste Umfrage(n)',
+    'total' => 'Gesamte Anzahl Stimmen',
+    'vote-now' => 'Stimme jetzt abgeben!',
+    'results' => 'Ergebnis der Umfrage',
+    'multiple-choice' => 'Dies ist eine Multiple-Choice-Umfrage. Sie können so viele Stimmen abgeben wie Sie wollen.',
+    'ip-checking' => 'Sie können auf diese Umfrage nur einmal antworten!',
+    'vote' => 'Stimme',
+    'votes' => 'Stimmen',
 ];
