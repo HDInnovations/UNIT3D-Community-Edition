dda4ac93fb607658fe31b36dd38e5b4024ad54bf
HDVinnie <blucrew2016@gmail.com>
2017-12-19 15:52:11 -0500

    (Update) Example env + database config

--- config/database.php
+++ config/database.php
@@ -110,14 +110,14 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
+            'database' => env('REDIS_DATABASE', 0),
         ],
 
         'session' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 1,
+            'database' => env('REDIS_DATABASE', 1),
         ],
 
     ],
