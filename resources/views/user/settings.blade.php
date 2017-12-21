222d2e6d2d84bdd5629cf16c5baf604f6662343f
Hyleus <Hyleus@tuta.io>
2017-12-21 16:56:43 +0100

    Add the ability to set a custom CSS URL

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -56,6 +56,12 @@
         <div class="radio-inline">
             <label><input type="radio" name="sidenav" @if($user->nav == 0) checked @endif value="0">Compact</label>
         </div>
+        <br>
+        <br>
+        <div class="form-group">
+            <label for="custom_css" class="control-label">Custom CSS URL</label>
+            <input type="text" name="custom_css" class="form-control" value="@if($user->custom_css) {{ $user->custom_css }} @else @endif" placeholder="Custom CSS URL">
+        </div>
 
         <h3>Privacy Settings</h3>
         <hr>
