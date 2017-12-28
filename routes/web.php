b68afcb015d652d7053b1cb81299ce0fb9ee62d5
Hyleus <Hyleus@tuta.io>
2017-12-28 16:15:30 +0100

    Rename parameter for messages route

--- routes/web.php
+++ routes/web.php
@@ -258,7 +258,7 @@ Route::group(['middleware' => 'auth'], function () {
 */
 Route::group(['prefix' => 'shoutbox', 'middleware' => 'auth'], function () {
     Route::get('/', 'HomeController@home')->name('shoutbox-home');
-    Route::get('/messages/{since?}', 'ShoutboxController@fetch')->name('shoutbox-fetch');
+    Route::get('/messages/{after?}', 'ShoutboxController@fetch')->name('shoutbox-fetch');
     Route::post('/send', 'ShoutboxController@send')->name('shoutbox-send');
     Route::get('/delete/{id}', 'ShoutboxController@deleteShout')->name('shout-delete');
 });
