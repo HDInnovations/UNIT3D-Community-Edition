11a6e839ea3ae61bae065d1c7d495895b80726af
KaEdRoSCL <35111670+KaEdRoSCL@users.noreply.github.com>
2018-01-04 15:10:29 -0300

    Update blocks.php

--- resources/lang/es/blocks.php
+++ resources/lang/es/blocks.php
@@ -7,24 +7,24 @@
 
 return [
     // Chat
-	'chatbox' => 'Chat Box',
+	'chatbox' => 'Chat',
 
     // Featured Torrents
-	'featured-torrents' => 'Featured Torrents',
+	'featured-torrents' => 'Torrents Destacados',
 
     // Latest Torrents
-	'latest-torrents' => 'Latest Torrents',
+	'latest-torrents' => 'Últimos Torrents',
 
     // Latest Posts
-	'latest-posts' => 'Latest Posts',
+	'latest-posts' => 'Últimos Mensajes',
 
     // Latest Topics
-	'latest-topics' => 'Latest Topics',
+	'latest-topics' => 'Últimos Temas',
 
     // Users Online
-	'users-online' => 'Users Online',
+	'users-online' => 'Usuarios En Línea',
 
     // News/Articles
-	'new-news' => 'New News',
-    'check-news' => 'News (Check Daily)',
+	'new-news' => 'Nuevas Noticias',
+    'check-news' => 'Noticias (Revisar Diariamente)',
 ];
