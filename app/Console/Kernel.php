4603c80413733889f38da0fc0fcf40e0f5a73c27
Laravel Shift <shift@laravelshift.com>
2018-01-10 01:51:32 +0000

    Shift console commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -57,6 +57,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
+        $this->load(__DIR__.'/Commands');
         require base_path('routes/console.php');
     }
 }
