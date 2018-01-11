da24e8bbaddd862c2398e54756752151f4a41581
Hyleus <Hyleus@tuta.io>
2018-01-11 21:16:24 +0100

    Improve comments for torrent.php

--- config/torrent.php
+++ config/torrent.php
@@ -1,8 +1,9 @@
 <?php
 return [
-    //This will be set in the torrents to generate an unique infohash.
+    // This sets a value in the torrents to generate an unique infohash.
     'source' => 'UNIT3D',
     'created_by' => 'Edited by UNIT3D',
+    // This appends the created_by comment instead of overwriting it.
     'created_by_append' => true,
     'comment' => "This torrent was downloaded from UNIT3D",
 ];
