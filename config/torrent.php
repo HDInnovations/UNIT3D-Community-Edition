01eb36339e7d2d7c720c12c93e0b03233ab56e3c
Hyleus <Hyleus@tuta.io>
2018-01-11 22:00:19 +0100

    Add notice of license to torrent.php

--- config/torrent.php
+++ config/torrent.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ * @license    https://choosealicense.com/licenses/gpl-3.0/  GNU General Public License v3.0
+ * @author     Ryuu
+ */
+
 return [
     // This sets a value in the torrents to generate an unique infohash.
     'source' => 'UNIT3D',
