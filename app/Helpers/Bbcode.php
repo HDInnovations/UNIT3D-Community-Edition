0c9f2e9c5f990a955cd9f12cc1bafb1632d6c7f8
Hyleus <Hyleus@tuta.io>
2018-01-16 20:39:52 +0100

    Add Bbcode helper

---
+++ app/Helpers/Bbcode.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ * @license    https://choosealicense.com/licenses/gpl-3.0/  GNU General Public License v3.0
+ * @author     Ryuu
+ */
+
+namespace App\Helpers;
+
+use Decoda\Decoda;
+use App\Hook\ClickableHook;
+use Illuminate\Database\Eloquent\Model;
+
+use Config;
+
+class Bbcode {
+    private function __construct()
+    {
+    
+    }
+
+    public static function decodaWithDefaults($data)
+    {
+        $code = new Decoda($data);
+        $code->defaults();
+        $code->removeHook('Censor');
+        $code->removeHook('Clickable');
+        $code->addHook(new ClickableHook());
+        $code->setXhtml(false);
+        $code->setStrict(false);
+        $code->setLineBreaks(true);
+        return $code;
+    }
+
+    public static function parse($data)
+    {
+        return self::decodaWithDefaults($data)->parse();
+    }
+}
