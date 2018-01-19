445ff65d7104a468f4e70209fa8a6f02066cb66a
xWanheda <35610059+xWanheda@users.noreply.github.com>
2018-01-19 22:04:08 +0300

    Update blocks.php

--- resources/lang/tr/blocks.php
+++ resources/lang/tr/blocks.php
@@ -7,24 +7,24 @@
 
 return [
     // Chat
-	'chatbox' => 'Chat Box',
+	'chatbox' => 'Sohbet Kutusu',
 
     // Featured Torrents
-	'featured-torrents' => 'Featured Torrents',
+	'featured-torrents' => 'Öne Çıkan Torrentler',
 
     // Latest Torrents
-	'latest-torrents' => 'Latest Torrents',
+	'latest-torrents' => 'En Son Eklenen Torrentler',
 
     // Latest Posts
-	'latest-posts' => 'Latest Posts',
+	'latest-posts' => 'En Son Yapılan Yorumlar',
 
     // Latest Topics
-	'latest-topics' => 'Latest Topics',
+	'latest-topics' => 'En Son Açılan Konular',
 
     // Users Online
-	'users-online' => 'Users Online',
+	'users-online' => '(10) Kullanıcı Çevrimiçi',
 
     // News/Articles
-	'new-news' => 'New News',
-    'check-news' => 'News (Check Daily)',
+	'new-news' => 'Yeni Haberler',
+    'check-news' => 'Haberler (Günlük olarak kontrol edin.)',
 ];
