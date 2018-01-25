540b4760b30b1954c946549c6e70b2861051da01
Hyleus <Hyleus@tuta.io>
2018-01-25 12:57:29 +0100

    Refactor latest posts block

--- resources/views/blocks/latest_posts.blade.php
+++ resources/views/blocks/latest_posts.blade.php
@@ -16,7 +16,7 @@
         </thead>
         <tbody>
           @foreach($posts as $p)
-          @if ($p->viewable())
+          @if ($p->topic->viewable())
           <tr class="">
             <td><a href="{{ route('forum_topic', array('slug' => $p->topic->slug, 'id' => $p->topic->id)) }}?page={{$p->getPageNumber()}}#post-{{$p->id}}">{{ preg_replace('#\[[^\]]+\]#', '', str_limit($p->content), 75) }}...</a></td>
             <td>{{ $p->topic->name }}</td>
