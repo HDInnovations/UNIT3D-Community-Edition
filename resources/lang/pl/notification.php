61042dca4b01be5f19705459c751121cd1d1faeb
VerioPL <veriopl@gmail.com>
2018-01-26 14:25:54 +0100

    Translation of notification views en/pl
    (Update) notification view
    (Added) new lang file notification.php

---
+++ resources/lang/pl/notification.php
@@ -0,0 +1,19 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Notification Language Lines
+|--------------------------------------------------------------------------
+*/
+
+return [
+	'notifications' => 'Powiadomienia',
+    'mark-all-read' => 'Oznacz wszystkie wszystkie jako przeczytane',
+    'mark-read' => 'Oznacz jako przeczytane',
+    'delete-all' => 'Skasuj wszystkie powiadomienia',
+    'title' => 'Tytuł',
+    'message' => 'Treść',
+    'date' => 'Data',
+    'Read' => 'Przeczytane',
+    'delete' => 'Skasuj',
+    'no-notifications' => 'Nie znaleziono żadnych powiadomień',
+];
