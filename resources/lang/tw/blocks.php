909a81ee7b58c377229c56812a12721024af4616
yinghuodt007 <yinghuodt007@gmail.com>
2018-02-04 20:47:16 +0800

    Update blocks.php

--- resources/lang/tw/blocks.php
+++ resources/lang/tw/blocks.php
@@ -6,24 +6,24 @@
  */
 return [
     // Chat
-    'chatbox' => 'Chat Box',
+    'chatbox' => '聊天室',
     
     // Featured Torrents
-    'featured-torrents' => 'Featured Torrents',
+    'featured-torrents' => '特色種子',
     
     // Latest Torrents
-    'latest-torrents' => 'Latest Torrents',
+    'latest-torrents' => '最近的種子',
     
     // Latest Posts
-    'latest-posts' => 'Latest Posts',
+    'latest-posts' => '最近的帖子',
     
     // Latest Topics
-    'latest-topics' => 'Latest Topics',
+    'latest-topics' => '最近的主題',
     
     // Users Online
-    'users-online' => 'Users Online',
+    'users-online' => '在綫用戶',
     
     // News/Articles
-    'new-news' => 'New News',
-    'check-news' => 'News (Check Daily)'
+    'new-news' => '新新聞',
+    'check-news' => '新聞（每日必看）'
 ];
