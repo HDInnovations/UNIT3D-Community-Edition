3d3119f002d1308de9ee4755971221dda0244ec3
yinghuodt007 <yinghuodt007@gmail.com>
2018-02-04 18:42:25 +0800

    Update notification.php

--- resources/lang/tw/notification.php
+++ resources/lang/tw/notification.php
@@ -5,14 +5,14 @@
  * |--------------------------------------------------------------------------
  */
 return [
-    'notifications' => 'Notifications',
-    'mark-all-read' => 'Mark all as read',
-    'mark-read' => 'Mark as read',
-    'delete-all' => 'Delete all notifications',
-    'title' => 'Title',
-    'message' => 'Message',
-    'date' => 'Date',
-    'read' => 'Read',
-    'delete' => 'Delete',
-    'no-notifications' => 'There are no notifications found'
+    'notifications' => '通知',
+    'mark-all-read' => '全部標為已讀',
+    'mark-read' => '標為已讀',
+    'delete-all' => '刪除所有通知',
+    'title' => '標題',
+    'message' => '消息',
+    'date' => '日期',
+    'read' => '已讀',
+    'delete' => '刪除',
+    'no-notifications' => '暫無通知'
 ];
