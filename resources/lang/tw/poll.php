a84e4b5d327d3ded5bb10b19a31d4c1996775789
yinghuodt007 <yinghuodt007@gmail.com>
2018-02-04 20:18:54 +0800

    Update poll.php

--- resources/lang/tw/poll.php
+++ resources/lang/tw/poll.php
@@ -5,14 +5,14 @@
  * |--------------------------------------------------------------------------
  */
 return [
-    'poll' => 'Poll',
-    'polls' => 'Polls',
-    'current' => 'Current Poll(s)',
-    'total' => 'Total Votes Ever',
-    'vote-now' => 'Get Your Vote In Now!',
-    'results' => 'Poll Results',
-    'multiple-choice' => 'This is a multiple choice poll. Select as many answers as you like.',
-    'ip-checking' => 'This poll has duplicate vote checking. You can only vote once.',
-    'vote' => 'Vote',
-    'votes' => 'Votes'
+    'poll' => '調查',
+    'polls' => '調查',
+    'current' => '目前進行的調查',
+    'total' => '總計票數',
+    'vote-now' => '進入投票!',
+    'results' => '結果',
+    'multiple-choice' => '多選項調查，按個人喜好可投多票',
+    'ip-checking' => '本次調查有重複性檢測，請勿重複投票',
+    'vote' => '投票',
+    'votes' => '投票'
 ];
