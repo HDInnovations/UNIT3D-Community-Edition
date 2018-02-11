7b843ac0205bf5c9d8eaf6f92e69b23b612161cb
HDVinnie <unit3d@protonmail.com>
2018-02-11 09:21:46 -0500

    (Update) Backup Manager Lang
    - Added French and Spanish

---
+++ resources/lang/es/backup.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    /*
+     * |--------------------------------------------------------------------------
+     * | Backup Manager Language Lines
+     * |--------------------------------------------------------------------------
+     */
+    'backup'                         => 'Copia de seguridad',
+    'create_a_new_backup'            => 'Crear una copia de seguridad',
+    'existing_backups'               => 'Copias de seguridad existentes',
+    'date'                           => 'Fecha',
+    'file_size'                      => 'Tamaño del fichero',
+    'actions'                        => 'Acciones',
+    'download'                       => 'Descargar',
+    'delete'                         => 'Eliminar',
+    'delete_confirm'                 => '¿Estás seguro que quieres borrar esta copia de seguridad?',
+    'delete_confirmation_title'      => 'Confirmado',
+    'delete_confirmation_message'    => 'La copia de seguridad fue eliminada.',
+    'delete_error_title'             => 'Ups, ha ocurrido un error',
+    'delete_error_message'           => 'La copia de seguridad NO se pudo eliminar.',
+    'delete_cancel_title'            => 'La operación ha sido cancelada',
+    'delete_cancel_message'          => 'La copia de seguridad NO se pudo eliminar.',
+    'create_confirmation_title'      => 'Se ha completado la copia de seguridad',
+    'create_confirmation_message'    => 'Recargando la página en 3 segundos.',
+    'create_error_title'             => 'Error al realizar la copia de seguridad',
+    'create_error_message'           => 'La copia de seguridad NO se pudo crear.',
+    'create_warning_title'           => 'Estamos presentando problemas',
+    'create_warning_message'         => 'La copia de seguridad puede que no se haya realizado. Por favor verifica los logs para más detalles.',
+    'location'                       => 'Ubicación',
+    'no_disks_configured'            => 'No existe ningún disco configurado config/laravel-backup.php',
+    'backup_doesnt_exist'            => 'La copia de seguridad no existe.',
+    'only_local_downloads_supported' => 'Solo se permiten descargas del sistema de archivos local.',
+];
