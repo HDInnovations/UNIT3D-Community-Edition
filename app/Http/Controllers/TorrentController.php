c25185cc68a068640ab8d0bacef6e5b6b9a6a1fd
HDVinnie <unit3d@protonmail.com>
2018-03-05 14:01:03 -0500

    (Update) IRC Messages

--- app/Http/Controllers/TorrentController.php
+++ app/Http/Controllers/TorrentController.php
@@ -327,11 +327,11 @@ class TorrentController extends Controller
                 if (config('irc-bot.enabled') == true) {
                 if ($torrent->anon == 0) {
                     $bot->message("#announce", "[" . config('app.name') . "] User " . $user->username . " has uploaded " . $torrent->name . " grab it now!");
-                    $bot->message("#announce", "[Category:" . $torrent->category->name . "]" "[Type:" . $torrent->type . "]" "[Size:" . $torrent->getSize() . "]");
+                    $bot->message("#announce", "[Category:" . $torrent->category->name . "] [Type:" . $torrent->type . "] [Size:" . $torrent->getSize() . "]");
                     $bot->message("#announce", "[Link: {$appurl}/torrents/" . $torrent->slug . "." . $torrent->id "]");
                 } else {
                     $bot->message("#announce", "[" . config('app.name') . "] An anonymous user has uploaded " . $torrent->name . " grab it now!");
-                    $bot->message("#announce", "[Category:" . $torrent->category->name . "]" "[Type:" . $torrent->type . "]" "[Size:" . $torrent->getSize() . "]");
+                    $bot->message("#announce", "[Category:" . $torrent->category->name . "] [Type:" . $torrent->type . "] [Size:" . $torrent->getSize() . "]");
                     $bot->message("#announce", "[Link: {$appurl}/torrents/" . $torrent->slug . "." . $torrent->id "]");
                 }
                 }
