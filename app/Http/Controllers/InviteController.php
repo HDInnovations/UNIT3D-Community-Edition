675a3a8e48a4bff8534c75e661419a4481c4d016
HDVinnie <unit3d@protonmail.com>
2018-03-17 14:27:39 -0400

    Update InviteController.php
    - use helpers instead of calling facades

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -14,7 +14,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Config;
 use App\User;
 use App\Invite;
 use App\Mail\InviteUser;
@@ -41,8 +40,8 @@ class InviteController extends Controller
     {
         $current = new Carbon();
         $user = auth()->user();
-        $invites_restricted = Config::get('config.invites_restriced', false);
-        $invite_groups = Config::get('config.invite_groups', []);
+        $invites_restricted = config('config.invites_restriced', false);
+        $invite_groups = config('config.invite_groups', []);
         if ($invites_restricted && !in_array($user->group->name, $invite_groups)) {
             return redirect()->route('invite')->with(Toastr::error('Invites are currently disabled for your userclass.', 'Whoops!', ['options']));
         }
