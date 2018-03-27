1b1b679afae351dd50349e495d7db2fe8b2e94b8
HDVinnie <unit3d@protonmail.com>
2018-03-27 18:45:44 -0400

    (Fix) Forums New Replay Email Template

--- resources/views/emails/new_reply.blade.php
+++ resources/views/emails/new_reply.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # {{ trans('email.newreply-header') }}: {{ $topic->name }}
 
-**{{ trans('email.newreply-message') }}:** <a href="{{ route('profil', ['username' => $user->username, 'id' => $user->id]) }}">{{ $user->username }}</a> {{ strtolower(trans('email.newreply-replied')) }}
+**{{ trans('email.newreply-message') }}:** <a href="{{ route('profile', ['username' => $user->username, 'id' => $user->id]) }}">{{ $user->username }}</a> {{ strtolower(trans('email.newreply-replied')) }}
 <a href="{{ route('forum_topic', ['slug' => $topic->slug, 'id' => $topic->id]) }}">{{ $topic->name }}</a>
 
 @component('mail::button', ['url' => route('forum_topic', ['slug' => $topic->slug, 'id' => $topic->id]), 'color' => 'blue'])
