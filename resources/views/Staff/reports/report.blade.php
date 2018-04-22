316592c11d5c49ebc64d54278a03d68a0fc0452b
poppabear8883 <servnx@gmail.com>
2018-04-22 17:49:09 -0400

    (Fix) Report view ref #209
    Submit button should not be visible after solved
    Put the solved results in a "well"
    Removed broken markup
    Reformated markup

--- resources/views/Staff/reports/report.blade.php
+++ resources/views/Staff/reports/report.blade.php
@@ -1,99 +1,101 @@
 @extends('layout.default')
 
 @section('title')
-<title>Reports - Staff Dashboard - {{ config('other.title') }}</title>
+    <title>Reports - Staff Dashboard - {{ config('other.title') }}</title>
 @endsection
 
 @section('meta')
-<meta name="description" content="Reports - Staff Dashboard"> @endsection
+    <meta name="description" content="Reports - Staff Dashboard"> @endsection
 
 @section('breadcrumb')
-<li>
-  <a href="{{ route('staff_dashboard') }}" itemprop="url" class="l-breadcrumb-item-link">
-    <span itemprop="title" class="l-breadcrumb-item-link-title">Staff Dashboard</span>
-  </a>
-</li>
-<li>
-  <a href="{{ route('getReports') }}" itemprop="url" class="l-breadcrumb-item-link">
-    <span itemprop="title" class="l-breadcrumb-item-link-title">Reports</span>
-  </a>
-</li>
-<li class="active">
-  <a href="{{ route('getReport',['report_id'=>$report->id]) }}" itemprop="url" class="l-breadcrumb-item-link">
-    <span itemprop="title" class="l-breadcrumb-item-link-title">Report</span>
-  </a>
-</li>
+    <li>
+        <a href="{{ route('staff_dashboard') }}" itemprop="url" class="l-breadcrumb-item-link">
+            <span itemprop="title" class="l-breadcrumb-item-link-title">Staff Dashboard</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('getReports') }}" itemprop="url" class="l-breadcrumb-item-link">
+            <span itemprop="title" class="l-breadcrumb-item-link-title">Reports</span>
+        </a>
+    </li>
+    <li class="active">
+        <a href="{{ route('getReport',['report_id'=>$report->id]) }}" itemprop="url" class="l-breadcrumb-item-link">
+            <span itemprop="title" class="l-breadcrumb-item-link-title">Report</span>
+        </a>
+    </li>
 @endsection
 
 @section('content')
-<div class="container">
-  <div class="block">
-    <h2>Report Details
-  @if($report->solved == 0)
-  <span class="text-red"><strong><i class="fa fa-times"></i> UNSOLVED </strong></span>
-  @else
-  <span class="text-green"><strong><i class="fa fa-check"></i> SOLVED BY <a class="name" href="{{ route('profile', ['username' => $report->staff->username, 'id' => $report->staff_id ]) }}">{{ $report->staff->username }}</a></strong></span>
-  @endif
-  </h2>
-    <hr>
-    <div class="row">
-      <div class="col-sm-12">
-        <h3>Reported User:</h3>
-        <p class="well well-sm">
-          <a href="{{ route('profile', ['username' => $report->reported->username, 'id' => $report->reported->id]) }}">
-            {{ $report->reported->username }}
-          </a>
-        </p>
-        <h3>Reported By:</h3>
-        <p class="well well-sm">
-          <a href="{{ route('profile', ['username' => $report->reporter->username, 'id' => $report->reporter->id]) }}">
-            {{ $report->reporter->username }}
-          </a>
-        </p>
+    <div class="container">
+        <div class="block">
+            <h2>Report Details
+                @if($report->solved == 0)
+                    <span class="text-red"><strong><i class="fa fa-times"></i> UNSOLVED </strong></span>
+                @else
+                    <span class="text-green"><strong><i class="fa fa-check"></i> SOLVED BY <a class="name"
+                                                                                              href="{{ route('profile', ['username' => $report->staff->username, 'id' => $report->staff_id ]) }}">{{ $report->staff->username }}</a></strong></span>
+                @endif
+            </h2>
+            <hr>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Reported User:</h3>
+                    <p class="well well-sm">
+                        <a href="{{ route('profile', ['username' => $report->reported->username, 'id' => $report->reported->id]) }}">
+                            {{ $report->reported->username }}
+                        </a>
+                    </p>
+                    <h3>Reported By:</h3>
+                    <p class="well well-sm">
+                        <a href="{{ route('profile', ['username' => $report->reporter->username, 'id' => $report->reporter->id]) }}">
+                            {{ $report->reporter->username }}
+                        </a>
+                    </p>
 
-        <h3>Title:</h3>
-        <p class="well well-sm">
-          <a href="{{ route('torrent', ['slug' => str_slug($report->title), 'id' => $report->torrent_id]) }}">
-            {{ $report->title }}
-          </a>
-        </p>
+                    <h3>Title:</h3>
+                    <p class="well well-sm">
+                        <a href="{{ route('torrent', ['slug' => str_slug($report->title), 'id' => $report->torrent_id]) }}">
+                            {{ $report->title }}
+                        </a>
+                    </p>
 
-        <h3>Message:</h3>
-        <p class="well well-lg">
-          {{ $report->message }}
-        </p>
+                    <h3>Message:</h3>
+                    <p class="well well-lg">
+                        {{ $report->message }}
+                    </p>
 
-        <h3>Referenced Links:</h3>
-        <p class="well">
-          @foreach($urls as $url)
-            <a href="{{$url}}" target="_blank">{{$url}}</a><br/>
-          @endforeach
-        </p>
-      </div>
-    </div>
+                    <h3>Referenced Links:</h3>
+                    <p class="well">
+                        @foreach($urls as $url)
+                            <a href="{{$url}}" target="_blank">{{$url}}</a><br/>
+                        @endforeach
+                    </p>
+                </div>
+            </div>
 
-      <h2>Resolve Report</h2>
-      <hr>
-      <div class="row">
-        <div class="col-sm-12">
-		<form role="form" method="POST" action="{{ route('solveReport',['report_id'=>$report->id]) }}">
-		{{ csrf_field() }}
-          @if($report->solved == 0)
-          <div class="form-group">
-            <label for="message">Verdict</label>
-            <textarea name="verdict" class="form-control"></textarea>
-          </div>
-          @else
-          <div class="form-group">
-            <label for="message">Verdict</label>
-            <textarea name="verdict" class="form-control" disabled="true">{{ $report->verdict }}</textarea>
-          </div>
-          @endif
-          <button type="submit" class="btn btn-primary">Submit</button>
-		</form>
+            <h2>Resolve Report</h2>
+            <hr>
+            <div class="row">
+                <div class="col-sm-12">
+                    <form role="form" method="POST" action="{{ route('solveReport',['report_id'=>$report->id]) }}">
+                        {{ csrf_field() }}
+                        @if($report->solved == 0)
+                            <div class="form-group">
+                                <label for="message">Verdict</label>
+                                <textarea name="verdict" class="form-control"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        @else
+                            <div class="form-group">
+                                <h3>Verdict</h3>
+                                <p class="well">
+                                    {{ $report->verdict }}
+                                </p>
+                            </div>
+                        @endif
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-</div>
 @endsection
