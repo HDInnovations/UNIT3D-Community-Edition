7e40eb53a2b5a541323f47e5581a35f1dfa570bf
HDVinnie <unit3d@protonmail.com>
2018-04-23 13:38:15 -0400

    (Fix) Poll Block

--- resources/views/blocks/poll.blade.php
+++ resources/views/blocks/poll.blade.php
@@ -1,5 +1,4 @@
-@php $user_has_voted = $poll->voters->where('user_id', auth()->user()->id)->isEmpty(); @endphp
-@if($user_has_voted)
+@if($poll && $poll->voters->where('user_id', auth()->user()->id)->isEmpty())
     <div class="col-md-10 col-sm-10 col-md-offset-1">
         <div class="clearfix visible-sm-block"></div>
         <div class="panel panel-chat shoutbox">
