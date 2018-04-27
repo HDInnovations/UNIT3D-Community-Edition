3a95b6ccaf982c8cd1843c3625915d15a3604b53
HDVinnie <unit3d@protonmail.com>
2018-04-27 11:17:45 -0400

    (Update) Version Checker
    - add loading state
    - require’s `npm run dev` to be run

--- resources/assets/js/components/Version.vue
+++ resources/assets/js/components/Version.vue
@@ -1,39 +1,51 @@
 <template>
     <div class="text-center">
-        <button @click="checkUpdate()" class="btn btn-primary btn-lg">Check For Update</button>
+        <button @click="checkUpdate()" class="btn btn-primary btn-lg">
+            <i v-if="loading" class="fa fa-circle-o-notch fa-spin"></i> {{ loading ? 'Loading...' : 'Check For Update' }}
+        </button>
     </div>
 </template>
 
 <script>
-    import Swal from 'sweetalert2'
+  import Swal from 'sweetalert2'
 
-    export default {
-        methods: {
-            checkUpdate() {
-                axios.get('/staff_dashboard/check-update')
-                    .then((response) => {
-                        if (response.data.updated === false) {
-                            Swal({
-                                position: 'top-end',
-                                type: 'warning',
-                                title: 'There Is A Update Available!',
-                                showConfirmButton: false,
-                                timer: 4500
-                            })
-                        } else {
-                            Swal({
-                                position: 'top-end',
-                                type: 'success',
-                                title: 'You Are Running The Latest Version Of UNIT3D!',
-                                showConfirmButton: false,
-                                timer: 4500
-                            })
-                        }
-                    })
-                    .catch((error) => {
-                        Swal('Oops...', error.response.data, 'error')
-                    })
-            },
-        }
+  export default {
+
+    data() {
+      return {
+        loading: false,
+      }
+    },
+
+    methods: {
+      checkUpdate() {
+        this.loading = true;
+        axios.get('/staff_dashboard/check-update')
+          .then((response) => {
+            if (response.data.updated === false) {
+              this.loading = false;
+              Swal({
+                position: 'center',
+                type: 'warning',
+                title: 'There Is A Update Available!',
+                showConfirmButton: false,
+                timer: 4500
+              })
+            } else {
+              this.loading = false;
+              Swal({
+                position: 'center',
+                type: 'success',
+                title: 'You Are Running The Latest Version Of UNIT3D!',
+                showConfirmButton: false,
+                timer: 4500
+              })
+            }
+          })
+          .catch((error) => {
+            Swal('Oops...', error.response.data, 'error')
+          })
+      },
     }
+  }
 </script>
