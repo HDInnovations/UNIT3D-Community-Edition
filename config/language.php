7421ade4a8ba76cb9b214fcd2ff0aad12c1ed7d3
HDVinnie <unit3d@protonmail.com>
2018-05-07 13:53:41 -0400

    (Update) Add Bulgarian Language

--- config/language.php
+++ config/language.php
@@ -97,7 +97,7 @@ return [
     |
     */
 
-    'allowed'       => ['ar', 'cs', 'da', 'de', 'en', 'es', 'et', 'ir', 'fr', 'gr', 'it', 'nl', 'pl', 'pt', 'br', 'ro', 'ru', 'tr', 'tw'],
+    'allowed'       => ['ar', 'bg', 'cs', 'da', 'de', 'en', 'es', 'et', 'ir', 'fr', 'gr', 'it', 'nl', 'pl', 'pt', 'br', 'ro', 'ru', 'tr', 'tw'],
 
     /*
     |--------------------------------------------------------------------------
