2afb5fd4dbf5825f113122210eba5d879c9552e1
HDVinnie <unit3d@protonmail.com>
2018-05-07 14:24:11 -0400

    (Update) Add Bengali Language

--- config/language.php
+++ config/language.php
@@ -97,7 +97,7 @@ return [
     |
     */
 
-    'allowed'       => ['ar', 'bg', 'cs', 'da', 'de', 'en', 'es', 'et', 'ir', 'fr', 'gr', 'it', 'nl', 'pl', 'pt', 'br', 'ro', 'ru', 'tr', 'tw'],
+    'allowed'       => ['ar', 'bg', 'bn', 'cs', 'da', 'de', 'en', 'es', 'et', 'ir', 'fr', 'gr', 'it', 'nl', 'pl', 'pt', 'br', 'ro', 'ru', 'tr', 'tw'],
 
     /*
     |--------------------------------------------------------------------------
