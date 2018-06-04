c1c8a465428d01f9d61a14cfa332473a8d21d99d
HDVinnie <unit3d@protonmail.com>
2018-06-03 22:44:22 -0400

    (Update) Gallery Index

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -20,11 +20,9 @@
     <div class="container">
         <div class="block">
             <div class="well text-center">
-                <h1>Gallery Rules</h1>
-                <li><p>BLAH</p></li>
-                <li><p>BLAH</p></li>
-                <li><p>BLAH</p></li>
-                <a href="{{ route('create_album_form') }}" class="btn btn-lg btn-success">Create New Album</a>
+                <h2>Welcome To The Gallery</h2>
+                <h4>Here you will find albums that contain hi-res images of cover sleeves and disc art. Enjoy!</h4>
+                <a href="{{ route('create_album_form') }}" class="btn btn-md btn-success">Create New Album</a>
             </div>
             <div class="row">
                 @foreach($albums as $album)
@@ -37,7 +35,7 @@
                                 <h4><span class="label label-default">{{ count($album->artwork) }} image(s)</span></h4>
                                 <h4>Created: {{ $album->created_at->toDayDateTimeString() }}</h4>
                                 <a href="{{ route('show_album', ['id' => $album->id]) }}"
-                                      class="btn btn-md btn-primary">View Album</a>
+                                   class="btn btn-md btn-primary">View Album</a>
                             </div>
                         </div>
                     </div>
