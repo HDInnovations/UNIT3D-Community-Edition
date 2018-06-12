c7435dc483ce02cde1ee3d01a7f2304a7bf30ed8
Alex Silva <ialexsilva@users.noreply.github.com>
2018-06-12 15:30:29 -0300

    Create email.php

---
+++ resources/lang/pt-br/email.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * |--------------------------------------------------------------------------
+ * | Email Language Lines
+ * |--------------------------------------------------------------------------
+ */
+return [
+    'activate-account' => 'Ativar conta',
+    'ban-reason' => 'Razão',
+    'banned-footer' => 'Isso é o que você ganha por não seguir as regras',
+    'banned-header' => 'Você foi banido',
+    'bug-description' => 'Problema',
+    'bug-footer' => 'Corrija esta merd@',
+    'bug-header' => 'Novo relatório de problema de',
+    'bug-priority' => 'Prioridade',
+    'bug-title' => 'Título do problema',
+    'contact-header' => 'Novo e-mail de contato de',
+    'contact-message' => 'Mensagem',
+    'contact-name' => 'Nome',
+    'invite-header' => 'Convite para',
+    'invite-invited' => 'Você foi convidado para',
+    'invite-message' => 'Mensagem',
+    'invite-signup' => 'Registre-se agora',
+    'newreply-header' => 'Há uma nova resposta no seu tópico',
+    'newreply-message' => 'Mensagem',
+    'newreply-replied' => 'Respondeu ao seu tópico',
+    'newreply-view' => 'Ver agora',
+    'register-code' => 'Para completar a ativação da sua conta, clique no botão abaixo',
+    'register-footer' => 'Se o botão acima não funcionar, copie e cole a URL na barra de endereços do seu navegador.',
+    'register-header' => 'Oi! Obrigado por se registrar',
+    'report-comment' => 'Comentar',
+    'report-email' => 'E-mail',
+    'report-header' => 'Olá administrador, um link foi relatado em',
+    'report-link' => 'Link',
+    'report-link-hash' => 'Link real',
+    'unban-footer' => 'Bem vindo de volta!',
+    'unban-header' => 'Você foi desbanido',
+    'unban-reason' => 'Razão'
+];
