55e337f0c6d33ae6e329de2eb296701b90baf2d6
Alex Silva <ialexsilva@users.noreply.github.com>
2018-06-12 12:43:44 -0300

    Create blocks.php

---
+++ resources/lang/pt-br/blocks.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * |--------------------------------------------------------------------------
+ * | Blocks Language Lines
+ * |--------------------------------------------------------------------------
+ */
+return [
+    // Chat
+    'chatbox' => 'Chat Box',
+    'click' => 'Clique',
+    'to-enable-editor' => 'para ativar o editor',
+    
+    // Featured Torrents
+    'featured-by' => 'Destacado por',
+    'featured-torrents' => 'Torrents em destaque',
+    'featured-torrents-intro' => 'Baixe enquanto você pode!',
+    'featured-until' => 'Este torrent está destacado até',
+    
+    // Latest Torrents
+    'latest-torrents' => 'Últimos Torrents',
+    
+    // Latest Posts
+    'latest-posts' => 'Últimas Postagens',
+    
+    // Latest Topics
+    'latest-topics' => 'Tópicos Recentes',
+    
+    // Users Online
+    'active-in-last' => 'Ativo nos últimos',
+    'users-online' => 'Usuários Online',
+    
+    // News/Articles
+    'check-news' => 'Notícias (verifique diariamente)',
+    'new-news' => 'Novas Notícias',
+    
+    // Top torrents
+    'new-torrents' => 'Novos torrents'
+];
