dd2892c8f197dca92d70e880ec584cde4d8a7be2
Alex Silva <ialexsilva@users.noreply.github.com>
2018-06-12 12:50:14 -0300

    Create bon.php
    pt-br

---
+++ resources/lang/pt-br/bon.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * |--------------------------------------------------------------------------
+ * | BON Language Lines
+ * |--------------------------------------------------------------------------
+ */
+return [
+    'activated' => 'Ativado',
+    'active' => 'Ativado!',
+    'bon' => 'BON',
+    'bonus' => 'Bônus',
+    'earning' => 'Ganhando',
+    'exchange' => 'Trocar',
+    'exchange-warning' => 'Trocas de pontos não podem ser desfeitas. Por favor, verifique suas escolhas antes de fazer uma troca.',
+    'gift' => 'Doar pontos de bônus',
+    'gift-to' => 'Doar pontos de bônus para?',
+    'item' => 'Item',
+    'no-refund' => 'NENHUM REEMBOLSO!',
+    'per-hour' => 'Pontos por hora',
+    'points' => 'Pontos',
+    'store' => 'Loja',
+    'total' => 'Ganhos totais',
+    'your-points' => 'Seus pontos'
+];
