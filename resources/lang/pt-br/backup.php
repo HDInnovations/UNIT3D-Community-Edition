f958b45fda26fd2c7545b00c6e12918a2887f3e1
Alex Silva <ialexsilva@users.noreply.github.com>
2018-06-12 12:02:52 -0300

    Create backup.php
    pt-br

---
+++ resources/lang/pt-br/backup.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    /*
+     * |--------------------------------------------------------------------------
+     * | Backup Manager Language Lines
+     * |--------------------------------------------------------------------------
+     */
+    'actions'                        => 'Ações',
+    'backup'                         => 'Backup',
+    'backup_doesnt_exist'            => "O arquivo de backup não existe.",
+    'create_a_new_backup'            => 'Criar novo backup',
+    'create_confirmation_message'    => 'Recarregando a página em 3 segundos.',
+    'create_confirmation_title'      => 'Backup efetuado',
+    'create_error_message'           => 'O arquivo de backup NÃO pôde ser criado.',
+    'create_error_title'             => 'Erro de backup',
+    'create_warning_message'         => 'Seu backup pode não ter sido criado. Por favor, verifique os arquivos de log para mais detalhes.',
+    'create_warning_title'           => 'Erro desconhecido',
+    'date'                           => 'Data',
+    'delete'                         => 'Excluir',
+    'delete_cancel_message'          => 'O arquivo de backup NÃO foi excluído.',
+    'delete_cancel_title'            => "Está bem",
+    'delete_confirm'                 => 'Você tem certeza de que deseja excluir este arquivo de backup?',
+    'delete_confirmation_message'    => 'O arquivo de backup foi excluído.',
+    'delete_confirmation_title'      => 'Pronto',
+    'delete_error_message'           => 'O arquivo de backup NÃO foi excluído.',
+    'delete_error_title'             => 'Erro',
+    'download'                       => 'Download',
+    'existing_backups'               => 'Backups existentes',
+    'file_size'                      => 'Tamanho do arquivo',
+    'location'                       => 'Localização',
+    'manager'                        => 'Gerenciador',
+    'no_disks_configured'            => 'Nenhum disco de backup configurado em config/backup.php',
+    'only_local_downloads_supported' => 'Apenas downloads do sistema de arquivos local é suportado.',
+];
