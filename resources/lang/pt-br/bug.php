e393a33b8b6d4976ec8236713bc5b1e866f02110
Alex Silva <ialexsilva@users.noreply.github.com>
2018-06-12 12:59:09 -0300

    Create bug.php
    pt-br

---
+++ resources/lang/pt-br/bug.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * |--------------------------------------------------------------------------
+ * | Bug Language Lines
+ * |--------------------------------------------------------------------------
+ */
+return [
+    'bug-report' => 'Relatar problema',
+    'bug-report-description' => 'Relatar problema no site',
+    'enter-description' => 'Descreva o problema da melhor forma possível',
+    'enter-email' => 'Digite o seu e-mail',
+    'enter-title' => 'Escolha um assunto apropriado',
+    'enter-username' => 'Digite seu nome de usuário',
+    'high' => 'Alta',
+    'low' => 'Baixa',
+    'priority' => 'Prioridade',
+    'priority-description' => 'Somente escolha a opção muito alta se o problema impossibilitar a navegação dentro do site.',
+    'very-high' => 'Muito Alta'
+];
