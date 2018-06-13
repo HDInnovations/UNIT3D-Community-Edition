d8983654a4b35389d52a8e3c03ad529b6f79e37a
Alex Silva <ialexsilva@users.noreply.github.com>
2018-06-12 23:11:33 -0300

    Create staff.php
    pt-br

---
+++ resources/lang/pt-br/staff.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * |--------------------------------------------------------------------------
+ * | Staff Language Lines
+ * |--------------------------------------------------------------------------
+ */
+return [
+    'activity-log' => 'Registro de atividade',
+    'articles' => 'Artigos',
+    'bans-log' => 'Registro de banimentos',
+    'blocks' => 'Bloqueios',
+    'catalog-groups' => 'Catálogos de grupos',
+    'catalog-torrents' => 'Catálogo de torrents',
+    'config-manager' => 'Gerenciador de configuração',
+    'dashboard' => 'Painel de controle',
+    'failed-login-log' => 'Falha ao logar no registro',
+    'flush-ghost-peers' => 'Limpar peers fantasmas',
+    'forums' => 'Forúns',
+    'frontend' => 'Frontend',
+    'general-tools' => 'Ferramentas Gerais',
+    'groups' => 'Grupos',
+    'invites-log' => 'Registo de convites',
+    'laravel-log' => 'Registro do laravel',
+    'links' => 'Links',
+    'logs' => 'Registros',
+    'mass-pm' => 'PM em massa',
+    'moderation' => 'Moderação',
+    'pages' => 'Páginas',
+    'please-moderate' => 'Por favor, moderar este torrent!',
+    'polls' => 'Enquetes',
+    'reports-log' => 'Registro de problemas',
+    'staff-dashboard' => 'Painel de controle da staff',
+    'torrent-categories' => 'Categorias de torrent',
+    'torrent-moderation' => 'Moderação de Torrent',
+    'torrent-tools' => 'Ferramentas torrent',
+    'torrent-types' => 'Tipos de torrent',
+    'torrents' => 'Torrents',
+    'user-gifting' => 'Presentes de usuário',
+    'user-notes' => 'Registro de notas de usuário',
+    'user-search' => 'Pesquisa de usuário',
+    'user-tools' => 'Ferramentas de usuário',
+    'warnings-log' => 'Registro de avisos',
+    'you-have' => 'Você tem'
+];
