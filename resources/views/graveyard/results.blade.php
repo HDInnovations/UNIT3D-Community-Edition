6f29aca3241b795d1b4833efb26f5791a316e4a4
HDVinnie <unit3d@protonmail.com>
2018-07-05 12:10:25 -0400

    (Update) Cleanup Graveyard View Partial

--- resources/views/graveyard/results.blade.php
+++ resources/views/graveyard/results.blade.php
@@ -1,19 +1,18 @@
- <div class="table-responsive">
-        <table class="table table-condensed table-hover table-bordered">
-            <thead>
-            <tr>
-                <th class="torrents-icon"></th>
-                <th class="torrents-filename">{{ trans('torrent.name') }}</th>
-                <th>{{ trans('torrent.age') }}</th>
-                <th>{{ trans('torrent.size') }}</th>
-                <th>{{ trans('torrent.short-seeds') }}</th>
-                <th>{{ trans('torrent.short-leechs') }}</th>
-                <th>{{ trans('torrent.short-completed') }}</th>
-                <th>{{ trans('graveyard.ressurect') }}</th>
-            </tr>
-            </thead>
-            <tbody>
-
+<div class="table-responsive">
+    <table class="table table-condensed table-hover table-bordered">
+        <thead>
+        <tr>
+            <th class="torrents-icon"></th>
+            <th class="torrents-filename">{{ trans('torrent.name') }}</th>
+            <th>{{ trans('torrent.age') }}</th>
+            <th>{{ trans('torrent.size') }}</th>
+            <th>{{ trans('torrent.short-seeds') }}</th>
+            <th>{{ trans('torrent.short-leechs') }}</th>
+            <th>{{ trans('torrent.short-completed') }}</th>
+            <th>{{ trans('graveyard.ressurect') }}</th>
+        </tr>
+        </thead>
+        <tbody>
             @foreach($torrents as $torrent)
                 @php $history = DB::table('history')->where('info_hash', '=', $torrent->info_hash)->where('user_id', '=', $user->id)->first(); @endphp
                 <tr class="">
@@ -35,21 +34,31 @@
                         </div>
                     </td>
                     <td>
-            <span data-toggle="tooltip" title=""
-                  data-original-title="">{{ $torrent->created_at->diffForHumans() }}</span>
+                        <span data-toggle="tooltip" title="" data-original-title="">
+                            {{ $torrent->created_at->diffForHumans() }}
+                        </span>
                     </td>
                     <td>
-                        <span class="">{{ $torrent->getSize() }}</span>
+                        <span>
+                            {{ $torrent->getSize() }}
+                        </span>
                     </td>
-                    <td>{{ $torrent->seeders }}</td>
-                    <td>{{ $torrent->leechers }}</td>
-                    <td>{{ $torrent->times_completed }}</td>
+                    <td>
+                        {{ $torrent->seeders }}
+                    </td>
+                    <td>
+                        {{ $torrent->leechers }}
+                    </td>
+                    <td>
+                        {{ $torrent->times_completed }}
+                    </td>
                     <td>
                         @php $resurrected = DB::table('graveyard')->where('torrent_id', '=', $torrent->id)->first(); @endphp
                         @if(!$resurrected)
-                            <button data-toggle="modal" data-target="#resurrect-{{ $torrent->id }}"
-                                    class="btn btn-sm btn-default">
-                                <span class="icon">@emojione(':zombie:') {{ trans('graveyard.ressurect') }}</span>
+                            <button data-toggle="modal" data-target="#resurrect-{{ $torrent->id }}" class="btn btn-sm btn-default">
+                                <span class="icon">
+                                    @emojione(':zombie:') {{ trans('graveyard.ressurect') }}
+                                </span>
                             </button>
                             {{-- Resurrect Modal --}}
                             <div class="modal fade" id="resurrect-{{ $torrent->id }}" tabindex="-1" role="dialog"
@@ -57,27 +66,46 @@
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                                                        aria-hidden="true">&times;</span></button>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
                                             <h2>
-                                                <i class="fa fa-thumbs-up"></i>{{ trans('graveyard.ressurect') }} {{ strtolower(trans('torrent.torrent')) }}
-                                                ?</h2>
+                                                <i class="fa fa-thumbs-up"></i>{{ trans('graveyard.ressurect') }} {{ strtolower(trans('torrent.torrent')) }} ?
+                                            </h2>
                                         </div>
 
                                         <div class="modal-body">
-                                            <p class="text-center text-bold"><span
-                                                        class="text-green"><em>{{ $torrent->name }} </em></span>
+                                            <p class="text-center text-bold">
+                                                <span class="text-green">
+                                                    <em>{{ $torrent->name }} </em>
+                                                </span>
                                             </p>
-                                            <p class="text-center text-bold">{{ trans('graveyard.howto') }}</p>
+                                            <p class="text-center text-bold">
+                                                {{ trans('graveyard.howto') }}
+                                            </p>
                                             <br>
                                             <div class="text-center">
                                                 <p>{!! trans('graveyard.howto-desc1', ['name' => $torrent->name]) !!}
-                                                    <span class="text-red text-bold">@if(!$history)
-                                                            0 @else {{ App\Helpers\StringHelper::timeElapsed($history->seedtime) }} @endif</span> {{ strtolower(trans('graveyard.howto-hits')) }}
-                                                    <span class="text-red text-bold">@if(!$history) {{ App\Helpers\StringHelper::timeElapsed(config('graveyard.time')) }} @else {{ App\Helpers\StringHelper::timeElapsed($history->seedtime + config('graveyard.time')) }} @endif</span> {{ strtolower(trans('graveyard.howto-desc2')) }}
-                                                    <span class="badge-user text-bold text-pink"
-                                                          style="background-image:url(https://i.imgur.com/F0UCb7A.gif);">{{ config('graveyard.reward') }} {{ trans('torrent.freeleech') }}
-                                                        Token(s)!</span></p>
+                                                    <span class="text-red text-bold">
+                                                        @if(!$history)
+                                                            0
+                                                        @else
+                                                            {{ App\Helpers\StringHelper::timeElapsed($history->seedtime) }}
+                                                        @endif
+                                                    </span>
+                                                        {{ strtolower(trans('graveyard.howto-hits')) }}
+                                                    <span class="text-red text-bold">
+                                                        @if(!$history)
+                                                            {{ App\Helpers\StringHelper::timeElapsed(config('graveyard.time')) }}
+                                                        @else
+                                                            {{ App\Helpers\StringHelper::timeElapsed($history->seedtime + config('graveyard.time')) }}
+                                                        @endif
+                                                    </span>
+                                                        {{ strtolower(trans('graveyard.howto-desc2')) }}
+                                                    <span class="badge-user text-bold text-pink" style="background-image:url(https://i.imgur.com/F0UCb7A.gif);">
+                                                        {{ config('graveyard.reward') }} {{ trans('torrent.freeleech') }} Token(s)!
+                                                    </span>
+                                                </p>
                                                 <div class="btns">
                                                     <form id="resurrect-torrent" role="form" method="POST"
                                                           action="{{ route('resurrect', ['torrent_id' => $torrent->id]) }}">
@@ -89,12 +117,12 @@
                                                             <input hidden="seedtime" name="seedtime" id="seedtime"
                                                                    value="{{ $history->seedtime + config('graveyard.time') }}">
                                                         @endif
-                                                        <button type="submit"
-                                                                class="btn btn-success">{{ trans('graveyard.ressurect') }}
-                                                            !
+                                                        <button type="submit" class="btn btn-success">
+                                                            {{ trans('graveyard.ressurect') }} !
                                                         </button>
-                                                        <button type="button" class="btn btn-warning"
-                                                                data-dismiss="modal">{{ trans('common.cancel') }}</button>
+                                                        <button type="button" class="btn btn-warning" data-dismiss="modal">
+                                                            {{ trans('common.cancel') }}
+                                                        </button>
                                                     </form>
                                                 </div>
                                             </div>
@@ -104,7 +132,9 @@
                             </div>
                         @else
                             <button class="btn btn-sm btn-info" disabled>
-                                <span class="icon">@emojione(':angel_tone2:') {{ strtolower(trans('graveyard.pending')) }}</span>
+                                <span class="icon">
+                                    @emojione(':angel_tone2:') {{ strtolower(trans('graveyard.pending')) }}
+                                </span>
                             </button>
                         @endif
                     </td>
@@ -112,9 +142,9 @@
             @endforeach
             </tbody>
         </table>
-        <div class="text-center">
-            {{ $torrents->links() }}
-        </div>
+    <div class="text-center">
+        {{ $torrents->links() }}
     </div>
+</div>
 
 
