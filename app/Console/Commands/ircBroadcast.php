ec30bddb311cd1897004dfb5e59269ecd023399a
HDVinnie <unit3d@protonmail.com>
2018-07-11 11:05:01 -0400

    (Update) IRC Announce System
    - reformat to adopt PSR-2 coding style

--- app/Console/Commands/ircBroadcast.php
+++ app/Console/Commands/ircBroadcast.php
@@ -17,66 +17,67 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use App\Bots\IRCAnnounceBot;
 
-class ircBroadcast extends Command {
+class ircBroadcast extends Command
+{
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'irc:broadcast';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'irc:broadcast';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Broadcast to all IRC Channels in config';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Broadcast to all IRC Channels in config';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function handle()
-	{
-		$this->info("Broadcasting: " . $this->argument('message'));
-		$bot = new IRCAnnounceBot();
-		$bot->broadcast($this->argument('message'));
-	}
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info("Broadcasting: " . $this->argument('message'));
+        $bot = new IRCAnnounceBot();
+        $bot->broadcast($this->argument('message'));
+    }
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return [
-			['message', InputArgument::REQUIRED, 'Message you would like to send'],
-		];
-	}
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['message', InputArgument::REQUIRED, 'Message you would like to send'],
+        ];
+    }
 
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	// protected function getOptions()
-	// {
-	// 	return array(
-	// 		array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
-	// 	);
-	// }
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    // protected function getOptions()
+    // {
+    // 	return array(
+    // 		array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+    // 	);
+    // }
 
 }
