5caeb67c71bd3fa07412f6e58a7947f10d8c3913
Laravel Shift <shift@laravelshift.com>
2018-09-04 18:24:25 +0000

    Move assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,8 +23,8 @@ mix
   /*
    * Sourced asset dependencies via node_modules and JS bootstrapping
    */
-  .js('resources/assets/js/app.js', 'public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css')
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
 
   /*
    * Themes
@@ -32,18 +32,18 @@ mix
    * Here we are compiling blur and dark themes to temporary files to later "merge" the corresponding wysibb editor
    * theme into a single theme file.
    *
-   * Note: Default wysibb theme is compiled into public/css/app.css from resources/assets/sass/app.scss
+   * Note: Default wysibb theme is compiled into public/css/app.css from resources/sass/app.scss
    *
    */
-  .sass('resources/assets/sass/themes/blur.scss', 'tmp/blur.css')
-  .sass('resources/assets/sass/themes/dark.scss', 'tmp/dark.css')
+  .sass('resources/sass/themes/blur.scss', 'tmp/blur.css')
+  .sass('resources/sass/themes/dark.scss', 'tmp/dark.css')
 
   /*
    * Compiling the blur theme and blur wysibb theme into a single file
    */
   .styles([
     'public/tmp/blur.css',
-    'resources/assets/js/wysibb/theme/blur/wbbtheme.css'
+    'resources/js/wysibb/theme/blur/wbbtheme.css'
   ], 'public/css/themes/blur.css')
 
   /*
@@ -51,13 +51,13 @@ mix
    */
   .styles([
     'public/tmp/dark.css',
-    'resources/assets/js/wysibb/theme/dark/wbbtheme.css'
+    'resources/js/wysibb/theme/dark/wbbtheme.css'
   ], 'public/css/themes/dark.css')
 
   /*
    * Compiling the galactic theme (uses default wysibb theme)
    */
-  .sass('resources/assets/sass/themes/galactic.scss', 'public/css/themes/galactic.css')
+  .sass('resources/sass/themes/galactic.scss', 'public/css/themes/galactic.css')
 
   /*
    * Login and TwoStep Auth styles
@@ -66,9 +66,9 @@ mix
    *
    * Note: These will likely be reworked into VueJS component(s)
    */
-  .sass('resources/assets/sass/main/login.scss', 'public/css/main/login.css')
-  .sass('resources/assets/sass/main/twostep.scss', 'public/css/main/twostep.css')
-  .babel('resources/assets/js/unit3d/twostep.js', 'public/js/twostep.js')
+  .sass('resources/sass/main/login.scss', 'public/css/main/login.css')
+  .sass('resources/sass/main/twostep.scss', 'public/css/main/twostep.css')
+  .babel('resources/js/unit3d/twostep.js', 'public/js/twostep.js')
 
   /*
    * Here we take all these scripts and compile them into a single 'unit3d.js' file that will be loaded after 'app.js'
@@ -76,9 +76,9 @@ mix
    * Note: The order of this array will matter, no different then linking these assets manually in the html
    */
   .babel([
-    'resources/assets/js/unit3d/hoe.js',
-    'resources/assets/js/unit3d/vendor/livicons.js',
-    'resources/assets/js/unit3d/blutopia.js'
+    'resources/js/unit3d/hoe.js',
+    'resources/js/unit3d/vendor/livicons.js',
+    'resources/js/unit3d/blutopia.js'
   ], 'public/js/unit3d.js')
   
   /*
