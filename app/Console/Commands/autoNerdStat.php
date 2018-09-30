6bc05ff948006f7c697620608414be35ccced214
HDVinnie <unit3d@protonmail.com>
2018-09-29 22:51:57 -0400

    (Update) NerdBot
    - added option to chat config to turn on and off nerd bot
    - cleaned up nerd bot command

--- app/Console/Commands/autoNerdStat.php
+++ app/Console/Commands/autoNerdStat.php
@@ -14,7 +14,7 @@ namespace App\Console\Commands;
 
 use App\Repositories\ChatRepository;
 use Illuminate\Console\Command;
-use App\Message;
+use App\Peer;
 use App\User;
 use App\Torrent;
 use Carbon\Carbon;
@@ -54,45 +54,51 @@ class autoNerdStat extends Command
      */
     public function handle()
     {
-        // Current Timestamp
-        $current = Carbon::now();
-        // Site Birthday
-        $bday = config('other.birthdate');
-        // Logins Count Last 24hours
-        $logins = User::whereNotNull('last_login')->where('last_login', '>', $current->subDay())->count();
-        // Torrents Uploaded Count Last 24hours
-        $uploads = Torrent::where('created_at', '>', $current->subDay())->count();
-        // New Users Count Last 24hours
-        $users = User::where('created_at', '>', $current->subDay())->count();
-        // Top Banker
-        $banker = User::latest('seedbonus')->first();
-        // Most Snatched Torrent
-        $snatched = Torrent::latest('times_completed')->first();
-        // Most Seeded Torrent
-        $seeders = Torrent::latest('seeders')->first();
-        // Most Leeched Torrent
-        $leechers = Torrent::latest('leechers')->first();
-        // FL Torrents
-        $fl = Torrent::where('free', 1)->count();
-        // DU Torrents
-        $du = Torrent::where('doubleup', 1)->count();
+        if (config('chat.nerd_bot') == true) {
 
-        // Select A Random Nerd Stat
-        $statArray = ["In The Last 24 Hours " . $logins . " Unique Users Have Logged Into " . config('other.title') . "!",
-            "In The Last 24 Hours " . $uploads . " Torrents Have Been Uploaded To " . config('other.title') . "!",
-            "In The Last 24 Hours " . $users . " Users Have Registered To " . config('other.title') . "!",
-            "There Are Currently " . $fl . " Freeleech Torrents On " . config('other.title') . "!",
-            "There Are Currently " . $du . " DoubleUpload Torrents On " . config('other.title') . "!",
-            "Currently " . $seeders->name . " Is The Best Seeded Torrent On " . config('other.title') . "!",
-            "Currently " . $leechers->name . " Is The Most Leeched Torrent On " . config('other.title') . "!",
-            "Currently " . $snatched->name . " Is The Most Snatched Torrent On " . config('other.title') . "!",
-            "Currently " . $banker->username . " Is The Top BON Holder On " . config('other.title') . "!",
-            config('other.title') . " Birthday Is " . $bday . "!",
-            config('other.title') . "  Is King!"
-        ];
-        $selected = mt_rand(0, count($statArray) - 1);
+            // Current Timestamp
+            $current = Carbon::now();
+            // Site Birthday
+            $bday = config('other.birthdate');
+            // Logins Count Last 24hours
+            $logins = User::whereNotNull('last_login')->where('last_login', '>', $current->subDay())->count();
+            // Torrents Uploaded Count Last 24hours
+            $uploads = Torrent::where('created_at', '>', $current->subDay())->count();
+            // New Users Count Last 24hours
+            $users = User::where('created_at', '>', $current->subDay())->count();
+            // Top Banker
+            $banker = User::latest('seedbonus')->first();
+            // Most Snatched Torrent
+            $snatched = Torrent::latest('times_completed')->first();
+            // Most Seeded Torrent
+            $seeders = Torrent::latest('seeders')->first();
+            // Most Leeched Torrent
+            $leechers = Torrent::latest('leechers')->first();
+            // FL Torrents
+            $fl = Torrent::where('free', 1)->count();
+            // DU Torrents
+            $du = Torrent::where('doubleup', 1)->count();
+            // Peers Count
+            $peers = Peer::count();
 
-        // Auto Shout Nerd Stat
-        $this->chat->systemMessage(":nerd: [b]Random Nerd Stat:[/b] " . $statArray[$selected] . " :nerd:");
+            // Select A Random Nerd Stat
+            $statArray = ["In The Last 24 Hours " . $logins . " Unique Users Have Logged Into " . config('other.title') . "!",
+                "In The Last 24 Hours " . $uploads . " Torrents Have Been Uploaded To " . config('other.title') . "!",
+                "In The Last 24 Hours " . $users . " Users Have Registered To " . config('other.title') . "!",
+                "There Are Currently " . $fl . " Freeleech Torrents On " . config('other.title') . "!",
+                "There Are Currently " . $du . " DoubleUpload Torrents On " . config('other.title') . "!",
+                "Currently " . $seeders->name . " Is The Best Seeded Torrent On " . config('other.title') . "!",
+                "Currently " . $leechers->name . " Is The Most Leeched Torrent On " . config('other.title') . "!",
+                "Currently " . $snatched->name . " Is The Most Snatched Torrent On " . config('other.title') . "!",
+                "Currently " . $banker->username . " Is The Top BON Holder On " . config('other.title') . "!",
+                config('other.title') . " Birthday Is " . $bday . "!",
+                config('other.title') . "  Is King!",
+                "Currently There Is " . $peers . " Peers On " . config('other.title') . "!",
+            ];
+            $selected = mt_rand(0, count($statArray) - 1);
+
+            // Auto Shout Nerd Stat
+            $this->chat->systemMessage(":nerd: [b][color=#c09fe0]NerdBot[/color][/b] : {$statArray[$selected]}");
+        }
     }
 }
