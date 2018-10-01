0b6f96b58c9f19a07ec53e3d916c8a5136d3b0b3
werrpy <werrpy@gmail.com>
2018-10-01 14:47:33 -0400

    (Update) Webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -29,25 +29,9 @@ mix
   /*
    * Themes
    *
-   * Here we are compiling blur and dark themes to temporary files to later "merge" the corresponding wysibb editor
-   * theme into a single theme file.
-   *
    * Note: Default wysibb theme is compiled into public/css/app.css from resources/assets/sass/app.scss
    *
    */
-  .sass('resources/assets/sass/themes/dark.scss', 'tmp/dark.css')
-
-  /*
-   * Compiling the dark theme and dark wysibb theme into a single file
-   */
-  .styles([
-    'public/tmp/dark.css',
-    'resources/assets/js/wysibb/theme/dark/wbbtheme.css'
-  ], 'public/css/themes/dark.css')
-  
-  /*
-   * Compiling themes
-   */  
   .sass('resources/assets/sass/themes/galactic.scss', 'public/css/themes/galactic.css')
   .sass('resources/assets/sass/themes/dark-blue.scss', 'public/css/themes/dark-blue.css')
   .sass('resources/assets/sass/themes/dark-green.scss', 'public/css/themes/dark-green.css')
