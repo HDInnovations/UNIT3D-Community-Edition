f56e868ed0c42eda43f37171511ca42b041f0e8a
VerioPL <veriopl@gmail.com>
2018-11-14 19:20:39 +0100

    - exclude "1" to $sender_id from ProcessMassPM()

--- app/Http/Controllers/Staff/MassPMController.php
+++ app/Http/Controllers/Staff/MassPMController.php
@@ -14,6 +14,7 @@ namespace App\Http\Controllers\Staff;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Jobs\ProcessMassPM;
 use App\User;
 use App\PrivateMessage;
 use Brian2694\Toastr\Toastr;
@@ -70,13 +71,8 @@ class MassPMController extends Controller
                 ->with($this->toastr->error($v->errors()->toJson(), 'Whoops!', ['options']));
         } else {
             foreach ($users as $user) {
-                // Send Private Message
-                $pm = new PrivateMessage;
-                $pm->sender_id = 1;
-                $pm->receiver_id = $user->id;
-                $pm->subject = $subject;
-                $pm->message = $message;
-                $pm->save();
+                $sender_id = 1;
+                $this->dispatch(new ProcessMassPM($sender_id, $user->id, $subject, $message));
             }
 
             // Activity Log
