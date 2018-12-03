2091d008c6beded2ef36b17eb0535ddc14e6fe23
HDVinnie <unit3d@protonmail.com>
2018-12-02 22:56:42 -0500

    (Add) Models

---
+++ app/Application.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Hootlex\Moderation\Moderatable;
+
+class Application extends Model
+{
+    use Moderatable;
+
+    /**
+     * Belongs To A User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault([
+            'username' => 'System',
+            'id' => '1'
+        ]);
+    }
+
+    /**
+     * Application Has Been Moderated By
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function moderated()
+    {
+        return $this->belongsTo(User::class, 'moderated_by')->withDefault([
+            'username' => 'System',
+            'id' => '1'
+        ]);
+    }
+
+    /**
+     * A Application Has Many Image Proofs
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function imageProofs()
+    {
+        return $this->hasMany(ApplicationImageProof::class);
+    }
+
+    /**
+     * A Application Has Many URL Proofs
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function urlProofs()
+    {
+        return $this->hasMany(ApplicationUrlProof::class);
+    }
+}
