072e88015b285a72f8232ea0f40d5589268dac3f
HDVinnie <unit3d@protonmail.com>
2019-01-09 10:31:23 -0500

    (Update) Categories Migration :rocket:
    - lets handle meta_data a better way

---
+++ database/migrations/2019_01_09_151754_alter_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('meta');
+            $table->boolean('movie_meta')->default(0);
+            $table->boolean('tv_meta')->default(0);
+            $table->boolean('game_meta')->default(0);
+            $table->boolean('music_meta')->default(0);
+            $table->boolean('no_meta')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
