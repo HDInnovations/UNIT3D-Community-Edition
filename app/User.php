812e1954eab5d3b0cfbe21d5161a206007891a02
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-01-10 01:43:35 -0500

    Revert "Update User.php"
    This reverts commit 5139b49288d430623ceb4710883e8cf82e9f86aa.

--- app/User.php
+++ app/User.php
@@ -54,19 +54,8 @@ class User extends Authenticatable
     public function group()
     {
         return $this->belongsTo(Group::class)->withDefault([
-            'color'  => config('user.group.defaults.color'),
-            'effect'  => config('user.group.defaults.effect'),
-            'icon'  => config('user.group.defaults.icon'),
-            'name'  => config('user.group.defaults.name'),
-            'slug'  => config('user.group.defaults.slug'),
-            'is_admin'  => config('user.group.defaults.is_admin'),
-            'is_freeleech'  => config('user.group.defaults.is_freeleech'),
-            'is_immune'  => config('user.group.defaults.is_immune'),
-            'is_incognito'  => config('user.group.defaults.is_incognito'),
-            'is_internal'  => config('user.group.defaults.is_internal'),
-            'is_modo'  => config('user.group.defaults.is_modo'),
-            'is_trusted'  => config('user.group.defaults.is_trusted'),
-            'can_upload'  => config('user.group.defaults.can_upload')
+            'color' => '#FF9966',
+            'icon'  => 'fal fa-robot',
         ]);
     }
 
