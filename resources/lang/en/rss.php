7417eb05f7a117faeaecc54ec9e777661fa62c82
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-01-12 15:37:03 -0500

    (Add) RSS English Language File
    Add rss english language words... provides base for translations.

---
+++ resources/lang/en/rss.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * |--------------------------------------------------------------------------
+ * | RSS Language Lines
+ * |--------------------------------------------------------------------------
+ */
+return [
+    'create'        => 'Create',
+    'create-private-feed'   => 'Create Private RSS Feed',
+    'create-public-feed'   => 'Create Public RSS Feed',
+    'delete'        => 'Delete',
+    'edit'          => 'Edit',
+    'edit-private-feed'     => 'Edit Private RSS Feed',
+    'edit-public-feed'     => 'Edit Public RSS Feed',
+    'feed'          => 'Feed',
+    'feeds'         => 'Feeds',
+    'name'          => 'Name',
+    'public'        => 'Public',
+    'private'       => 'Private',
+    'rss'           => 'RSS',
+    'type'          => 'Type',
+];
