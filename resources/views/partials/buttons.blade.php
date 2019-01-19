d01b8bb789f0f05cf14d8aaf5e76223578af0a14
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-01-19 00:09:18 -0500

    (Add) Buttons Partial
    If need to be extended just pass a var...

---
+++ resources/views/partials/buttons.blade.php
@@ -0,0 +1,27 @@
+<div class="button-holder">
+    <div class="button-left">
+        <a href="{{ route('cards') }}" class="btn btn-sm btn-primary">
+            <i class="{{ config('other.font-awesome') }} fa-image"></i> @lang('torrent.cards')
+        </a>
+        <a href="{{ route('catalogs') }}" class="btn btn-sm btn-primary">
+            <i class="{{ config('other.font-awesome') }} fa-book"></i> @lang('torrent.catalogs')
+        </a>
+        <a href="{{ route('categories') }}" class="btn btn-sm btn-primary">
+            <i class="{{ config('other.font-awesome') }} fa-file"></i> @lang('torrent.categories')
+        </a>
+        <a href="{{ route('groupings') }}" class="btn btn-sm btn-primary">
+            <i class="{{ config('other.font-awesome') }} fa-clone"></i> @lang('torrent.groupings')
+        </a>
+        <a href="{{ route('torrents') }}" class="btn btn-sm btn-primary">
+            <i class="{{ config('other.font-awesome') }} fa-list"></i> @lang('torrent.list')
+        </a>
+        <a href="{{ route('rss.index') }}" class="btn btn-sm btn-warning">
+            <i class="{{ config('other.font-awesome') }} fa-rss"></i> @lang('rss.rss') @lang('rss.feeds')
+        </a>
+    </div>
+    <div class="button-right">
+        <a href="#/" class="btn btn-sm btn-danger" id="facetedFiltersToggle">
+            <i class="{{ config('other.font-awesome') }} fa-sliders-h"></i> @lang('torrent.filters')
+        </a>
+    </div>
+</div>
