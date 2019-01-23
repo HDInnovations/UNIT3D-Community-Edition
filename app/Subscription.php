14277e6f893c67d5937bb5ff8ff4345d2327147b
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-01-23 14:37:53 -0500

    (Add/Update) Subscription Model
    Clone + update from TopicSubscription

---
+++ app/Subscription.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ *
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie
+ */
+
+namespace App;
+
+use App\Notifications\NewTopic;
+use App\Notifications\NewTopicPost;
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    /**
+     * Belongs To A User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault([
+            'username' => 'System',
+            'id'       => '1',
+        ]);
+    }
+
+    /**
+     * Belongs To A Topic.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    /**
+     * Belongs To A Forum.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function forum()
+    {
+        return $this->belongsTo(Forum::class);
+    }
+
+    /**
+     * Notify Subscribers Of A Topic When New Post Is Made.
+     *
+     * @return string
+     */
+    public function notifyTopic($post)
+    {
+        User::find($this->user_id)->notify(new NewTopicPost($post));
+    }
+
+    /**
+     * Notify Subscribers Of A Forum When New Topic Is Made.
+     *
+     * @return string
+     */
+    public function notifyForum($topic)
+    {
+        User::find($this->user_id)->notify(new NewTopic($topic));
+    }
+}
