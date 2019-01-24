cf744e8915e655de70b8ad0888806cb8fa2e800d
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-01-23 22:12:23 -0500

    (Add) NewUploadTip Notifications
    Wasn't sure on this one.
    
    Technically belongs to Torrent, but upload is an option.  THis is a trigger on a specific torrent (uploaded).
    
    Where Torrent hooks should be tied to torrents in general.

---
+++ app/Notifications/NewUploadTip.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ *
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie, singularity43
+ */
+
+namespace App\Notifications;
+
+use App\Torrent;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewUploadTip extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $type;
+    public $tipper;
+    public $torrent;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Torrent $torrent
+     *
+     * @return void
+     */
+    public function __construct(string $type, string $tipper, Torrent $torrent)
+    {
+        $this->type = $type;
+        $this->tipper = $tipper;
+        $this->torrent = $torrent;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        $appurl = config('app.url');
+
+        return [
+            'title' => $this->tipper.' Has Tipped One Of Your Torrent Uploads',
+            'body'  => $this->tipper.' has tipped one of your Torrent Uploads: '.$this->torrent->name,
+            'url'   => "/torrents/{$this->torrent->slug}.{$this->torrent->id}",
+        ];
+    }
+}
