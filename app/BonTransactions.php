22747b201890774312a5531c9d52c1b35aff19e7
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-01-30 21:33:12 -0500

    (Update) BonTransactions Model
    Establish relationship with receiverObj (no conflict with receiver)

--- app/BonTransactions.php
+++ app/BonTransactions.php
@@ -39,7 +39,7 @@ class BonTransactions extends Model
     protected $dateFormat = 'U';
 
     /**
-     * Belongs To A Receiver.
+     * Belongs To A Sender.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
@@ -51,6 +51,36 @@ class BonTransactions extends Model
         return $this->belongsTo(User::class, 'sender', 'id')->withDefault([
             'username' => 'System',
             'id'       => '1',
+        ]);
+    }
+
+    /**
+     * Belongs To A Receiver.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+
+    // Bad name to not conflict with sender (not sender_id)
+
+    public function receiverObj()
+    {
+        return $this->belongsTo(User::class, 'receiver', 'id')->withDefault([
+            'username' => 'System',
+            'id'       => '1',
+        ]);
+    }
+
+    /**
+     * Belongs To BonExchange.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+
+    public function exchange()
+    {
+        return $this->belongsTo(BonExchange::class, 'itemID', 'id')->withDefault([
+            'value' => 0,
+            'cost' => 0,
         ]);
     }
 }
