99f2d060d21eae4d85da095cafa86a5bcccf7b8c
HDVinnie <unit3d@protonmail.com>
2019-02-01 20:35:54 -0500

    (Update) Add Cloudfare Support :rocket:

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -57,7 +57,7 @@ window.io = require('socket.io-client');
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: window.location.hostname + ':6001',
+    host: window.location.hostname + ':8443',
 });
 
 /**
