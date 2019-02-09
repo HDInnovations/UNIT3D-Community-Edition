fdb4a5297f0288f742ce26f906b6a80fee8c2dce
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-02-09 15:23:43 -0500

    (Add) Staff Bot Edit View
    To edit bots.

---
+++ resources/views/Staff/bots/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('layout.default')
+
+@section('title')
+    <title>@lang('bot.edit-bot') - {{ config('other.title') }}</title>
+@endsection
+
+@section('breadcrumb')
+    <li>
+        <a href="{{ route('staff_dashboard') }}" itemprop="url" class="l-breadcrumb-item-link">
+            <span itemprop="title" class="l-breadcrumb-item-link-title">Staff Dashboard</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('Staff.bots.index') }}" itemprop="url" class="l-breadcrumb-item-link">
+            <span itemprop="title" class="l-breadcrumb-item-link-title">@lang('bot.bots')</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('Staff.bots.edit', ['id' => $bot->id]) }}" itemprop="url" class="l-breadcrumb-item-link">
+            <span itemprop="title" class="l-breadcrumb-item-link-title">@lang('bot.edit-bot')</span>
+        </a>
+    </li>
+@endsection
+
+
+@section('content')
+    <div class="container box">
+        <h2>@lang('bot.edit-bot')</h2>
+        <form role="form" method="POST" action="{{ route('Staff.bots.update', ['id' => $bot->id]) }}">
+            @csrf
+            @method('PUT')
+                    <div class="form-group">
+                        <label for="name">@lang('bot.name')</label>
+                        <input type="text" class="form-control" id="name" name="name" value="{{ $bot->name }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">@lang('common.position')</label>
+                        <input type="number" id="position" name="position" value="{{ $bot->position }}" class="form-control">
+                    </div>
+            <div class="form-group">
+                <label for="name">@lang('bot.command')</label>
+                <input type="text" class="form-control" id="command" name="command" value="{{ $bot->command }}">
+            </div>
+            <div class="form-group">
+                <label for="name">@lang('bot.info')</label>
+                <input type="text" class="form-control" id="info" name="info" value="{{ $bot->info }}">
+            </div>
+            <div class="form-group">
+                <label for="name">@lang('bot.about')</label>
+                <input type="text" class="form-control" id="about" name="about" value="{{ $bot->about }}">
+            </div>
+            <div class="form-group">
+                <label for="name">@lang('bot.emoji-code')</label>
+                <input type="text" class="form-control" id="emoji" name="emoji" value="{{ $bot->emoji }}">
+            </div>
+            <div class="form-group">
+                <label for="name">@lang('bot.icon')</label>
+                <input type="text" class="form-control" id="icon" name="icon" value="{{ $bot->icon }}">
+            </div>
+            <div class="form-group">
+                <label for="name">@lang('bot.color')</label>
+                <input type="text" class="form-control" id="color" name="color" value="{{ $bot->color }}">
+            </div>
+            <div class="form-group">
+                <label for="name">@lang('bot.help')</label>
+            </div>
+            <div class="form-group">
+                <textarea name="help" cols="30" rows="10" class="form-control">{{ $bot->help }}</textarea>
+            </div>
+                    <br>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">@lang('common.edit')</button>
+                    </div>
+        </form>
+    </div>
+@endsection
