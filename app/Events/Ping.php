be642d069f33c71fec7cdfaaeb4dce617b9d836b
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-02-09 14:57:20 -0500

    (Add) Ping Event
    For heartbeart + audible.

---
+++ app/Events/Ping.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ *
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie
+ */
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use App\Http\Resources\ChatMessageResource;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+
+class Ping implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $room;
+    public $ping;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($room, $id)
+    {
+        $this->room = $room;
+        $this->ping = ['type' => 'room', 'id' => $id];
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        // $this->dontBroadcastToCurrentUser();
+
+        return new PresenceChannel('chatroom.'.$this->room);
+    }
+
+    public function broadcastAs()
+    {
+        return 'new.ping';
+    }
+}
