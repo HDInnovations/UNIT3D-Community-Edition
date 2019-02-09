a97a3530736f26562c9f1af0f354d597e6d7adb8
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-02-09 15:20:04 -0500

    (Update) pmMethods JS
    For SweetAlert2 support.

--- resources/js/components/chat/mixins/pmMethods.js
+++ resources/js/components/chat/mixins/pmMethods.js
@@ -4,59 +4,56 @@ export default {
       if (user.id === this.$parent.auth.id) {
         return false
       }
-
       swal({
         title: `Send Private Message to ${user.username}`,
-        input: 'textarea',
         width: '800px',
         height: '600px',
         inputAttributes: {
           autocapitalize: 'off'
         },
-
+        html:
+            '<div class="text-left">'+
+            '<input type="hidden" id="receiver-id" name="receiver-id" value="'+user.id+'">\n' +
+            '<textarea id="chat-message-pm"\n' +
+            '        name="message-pm"\n' +
+            '        placeholder="Write your message..."\n' +
+            '        cols="30"\n' +
+            '        rows="5">\n' +
+            '</textarea>'+
+            '</div>',
         showCancelButton: true,
         confirmButtonText: 'Send',
         showLoaderOnConfirm: true,
-
         onOpen: () => {
-          this.editor = $('.swal2-textarea').wysibb({})
+          this.editor = $('#chat-message-pm').wysibb()
+          this.target = $('#receiver-id').val();
         },
-
         onClose: () => {
-          this.editor = null
+          this.editor = null;
+          this.target = null;
         },
-
         preConfirm: (msg) => {
-
-          msg = this.editor.bbcode().trim()
-
+          let target = this.target;
+          msg = this.editor.bbcode().trim();
           if (msg !== null && msg !== '') {
-
             this.$emit('pm-sent', {
               message: msg,
               save: true,
               user_id: this.$parent.auth.id,
-              receiver_id: user.id
-            })
-
-            $('.wysibb-body').html('')
+              receiver_id: target
+            });
+            $('#chat-message-pm').html('');
           }
-
-          return user
-
+          return user;
         },
-
         allowOutsideClick: false
-
       }).then(result => {
-        // console.log(result)
-
         if (result.value) {
           swal({
             title: `Sent Private Message to ${result.value.username}`,
             timer: 1500,
             onOpen: () => {
-              swal.showLoading()
+              swal.showLoading();
             }
           }).then((result) => {
             if (result.dismiss === swal.DismissReason.timer) {
