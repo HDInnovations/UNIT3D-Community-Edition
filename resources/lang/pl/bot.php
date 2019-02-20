7261a71246958a665a1442002d05e4fbeb955209
HDVinnie <unit3d@protonmail.com>
2019-02-20 12:22:50 -0500

    (Add) Polish RSS + Bot Multilingual Support :earth_americas:

---
+++ resources/lang/pl/bot.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'about' => 'O',
+    'bot' => 'Nerw',
+    'bots' => 'Boty',
+    'color' => 'Kolor',
+    'command' => 'Dowództwo',
+    'edit-bot' => 'Edytuj Bot',
+    'emoji-code' => 'Kod Emoji',
+    'help' => 'Wsparcie',
+    'icon' => 'Ikona',
+    'info' => 'Informacje',
+    'name' => 'Imię',
+];
