3bb012e048e5a89532d8ea87ff523ca60c0ed996
HDVinnie <unit3d@protonmail.com>
2019-02-20 12:23:16 -0500

    (Add) Portuguese RSS + Bot Multilingual Support :earth_americas:

---
+++ resources/lang/pt/bot.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'about' => 'Sobre',
+    'bot' => 'Robô',
+    'bots' => 'Botinhas',
+    'color' => 'Cor',
+    'command' => 'Comando',
+    'edit-bot' => 'Editar bot',
+    'emoji-code' => 'Código Emoji',
+    'help' => 'Socorro',
+    'icon' => 'Ícone',
+    'info' => 'Info',
+    'name' => 'Nome',
+];
