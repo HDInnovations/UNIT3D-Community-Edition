ff44c8c8ce5134a7386853be5544234e283277ae
HDVinnie <unit3d@protonmail.com>
2019-02-20 12:20:00 -0500

    (Add) Estonian RSS + Bot Multilingual Support :earth_americas:

---
+++ resources/lang/et/bot.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'about' => 'Umbes',
+    'bot' => 'Bot',
+    'bots' => 'Botid',
+    'color' => 'Värv',
+    'command' => 'Käsk',
+    'edit-bot' => 'Edit Bot',
+    'emoji-code' => 'Emoji kood',
+    'help' => 'Abi',
+    'icon' => 'Ikoon',
+    'info' => 'Info',
+    'name' => 'Nimi',
+];
