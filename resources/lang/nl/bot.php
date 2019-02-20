aa0b0db57f8b04479b3094c3162873e2e5c1499d
HDVinnie <unit3d@protonmail.com>
2019-02-20 12:22:27 -0500

    (Add) Dutch RSS + Bot Multilingual Support :earth_americas:

---
+++ resources/lang/nl/bot.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'about' => 'Wat betreft',
+    'bot' => 'Bot',
+    'bots' => 'bots',
+    'color' => 'Kleur',
+    'command' => 'Commando',
+    'edit-bot' => 'Bewerk Bot',
+    'emoji-code' => 'Emoji-code',
+    'help' => 'Helpen',
+    'icon' => 'Icoon',
+    'info' => 'info',
+    'name' => 'Naam',
+];
---
+++ resources/lang/nl/rss.php
@@ -0,0 +1,18 @@
+<?php 
+
+return [
+    'create' => 'creëren',
+    'create-private-feed' => 'Maak een privé RSS-feed',
+    'create-public-feed' => 'Maak openbare RSS-feed',
+    'delete' => 'Verwijder',
+    'edit' => 'Bewerk',
+    'edit-private-feed' => 'Bewerk privé RSS-feed',
+    'edit-public-feed' => 'Bewerk openbare RSS-feed',
+    'feed' => 'Voeden',
+    'feeds' => 'feeds',
+    'name' => 'Naam',
+    'public' => 'Openbaar',
+    'private' => 'Privaat',
+    'rss' => 'RSS',
+    'type' => 'Type',
+];
