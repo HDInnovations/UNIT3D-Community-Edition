3b30e87c96e7f0b0ab55757da8555640a7bffaa8
HDVinnie <unit3d@protonmail.com>
2019-02-20 12:19:12 -0500

    (Add) Spanish RSS + Bot Multilingual Support :earth_americas:

---
+++ resources/lang/es/bot.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'about' => 'Acerca de',
+    'bot' => 'Larva del moscardón',
+    'bots' => 'Bots',
+    'color' => 'Color',
+    'command' => 'Mando',
+    'edit-bot' => 'Editar Bot',
+    'emoji-code' => 'Codigo emoji',
+    'help' => 'Ayuda',
+    'icon' => 'Icono',
+    'info' => 'Información',
+    'name' => 'Nombre',
+];
---
+++ resources/lang/es/rss.php
@@ -0,0 +1,18 @@
+<?php 
+
+return [
+    'create' => 'Crear',
+    'create-private-feed' => 'Crear un feed RSS privado',
+    'create-public-feed' => 'Crear RSS público',
+    'delete' => 'Borrar',
+    'edit' => 'Editar',
+    'edit-private-feed' => 'Editar RSS privado',
+    'edit-public-feed' => 'Editar RSS Public Feed',
+    'feed' => 'Alimentar',
+    'feeds' => 'Feeds',
+    'name' => 'Nombre',
+    'public' => 'Público',
+    'private' => 'Privado',
+    'rss' => 'RSS',
+    'type' => 'Tipo',
+];
