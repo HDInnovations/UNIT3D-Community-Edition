d22f6db881248155d8f55cc8c21dd3bec62c7564
HDVinnie <unit3d@protonmail.com>
2019-02-20 12:20:21 -0500

    (Add) French RSS + Bot Multilingual Support :earth_americas:

---
+++ resources/lang/fr/bot.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'about' => 'Sur',
+    'bot' => 'Bot',
+    'bots' => 'Bots',
+    'color' => 'Couleur',
+    'command' => 'Commander',
+    'edit-bot' => 'Edit Bot',
+    'emoji-code' => 'Code Emoji',
+    'help' => 'Aidez-moi',
+    'icon' => 'Icône',
+    'info' => 'Info',
+    'name' => 'prénom',
+];
