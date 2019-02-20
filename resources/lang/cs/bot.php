4a27cdd1ba8feafdbc61a46f3b5ddca04fbce482
HDVinnie <unit3d@protonmail.com>
2019-02-20 12:17:30 -0500

    (Add) Czech RSS + Bot Multilingual Support :earth_americas:

---
+++ resources/lang/cs/bot.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'about' => 'O',
+    'bot' => 'Bot',
+    'bots' => 'Bots',
+    'color' => 'Barva',
+    'command' => 'Příkaz',
+    'edit-bot' => 'Upravit bot',
+    'emoji-code' => 'Emojiho kód',
+    'help' => 'Pomoc',
+    'icon' => 'Ikona',
+    'info' => 'Info',
+    'name' => 'název',
+];
---
+++ resources/lang/cs/rss.php
@@ -0,0 +1,18 @@
+<?php 
+
+return [
+    'create' => 'Vytvořit',
+    'create-private-feed' => 'Vytvořit soukromý RSS kanál',
+    'create-public-feed' => 'Vytvořit veřejný RSS kanál',
+    'delete' => 'Odstranit',
+    'edit' => 'Upravit',
+    'edit-private-feed' => 'Upravte soukromý RSS kanál',
+    'edit-public-feed' => 'Upravit veřejný RSS kanál',
+    'feed' => 'Krmivo',
+    'feeds' => 'Feeds',
+    'name' => 'název',
+    'public' => 'Veřejnost',
+    'private' => 'Soukromé',
+    'rss' => 'RSS',
+    'type' => 'Typ',
+];
