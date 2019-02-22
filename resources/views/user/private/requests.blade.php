04d00a4c8b881cca039781c41434bf0cd25f91b0
singularity43 <46550600+singularity43@users.noreply.github.com>
2019-02-21 21:17:16 -0500

    (Update) Request View
    Nuke stats

--- resources/views/user/private/requests.blade.php
+++ resources/views/user/private/requests.blade.php
@@ -31,23 +31,6 @@
                     </h1>
                 </div>
             </div>
-            <div class="button-holder some-padding">
-                <div class="button-left">
-
-                </div>
-                <div class="button-right">
-            <span class="badge-user"><strong>@lang('user.total-download'):</strong>
-        <span class="badge-extra text-red">{{ App\Helpers\StringHelper::formatBytes($his_downl,2) }}</span>
-        <span class="badge-extra text-orange" data-toggle="tooltip"
-              data-original-title="@lang('user.credited-download')">{{ App\Helpers\StringHelper::formatBytes($his_downl_cre,2) }}</span>
-    </span>
-                    <span class="badge-user"><strong>@lang('user.total-upload'):</strong>
-        <span class="badge-extra text-green">{{ App\Helpers\StringHelper::formatBytes($his_upl,2) }}</span>
-        <span class="badge-extra text-blue" data-toggle="tooltip"
-              data-original-title="@lang('user.credited-upload')">{{ App\Helpers\StringHelper::formatBytes($his_upl_cre,2) }}</span>
-    </span>
-                </div>
-            </div>
             <hr class="some-padding">
             <div class="container well search mt-5">
                 <form role="form" method="GET" action="UserController@myFilters" class="form-horizontal form-condensed form-torrent-search form-bordered">
