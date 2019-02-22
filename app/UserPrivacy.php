12397c9e6adad19efaab5b1afa90a92f0170ee89
Laravel Shift <shift@laravelshift.com>
2019-02-22 03:22:51 +0000

    Adopt PSR-2 coding style
    The Laravel framework adopts the PSR-2 coding style in version 5.1.
    Laravel apps *should* adopt this coding style as well. Read the
    [PSR-2 coding style guide][1] for more details and check out [PHPCS][2]
    to use as a code formatting tool.
    
    [1]: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md
    [2]: https://github.com/squizlabs/PHP_CodeSniffer

--- app/UserPrivacy.php
+++ app/UserPrivacy.php
@@ -94,7 +94,7 @@ class UserPrivacy extends Model
      */
     public function setDefaultValues($type = 'default')
     {
-        foreach ($this->casts as $k=>$v) {
+        foreach ($this->casts as $k => $v) {
             if ($v == 'array') {
                 $this->$k = $this->expected_groups;
             }
