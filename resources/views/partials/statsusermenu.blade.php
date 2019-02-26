bd6ea8407877a6ed17c7e1dff68ed33b9c14820e
yourguessismine <39930693+yourguessismine@users.noreply.github.com>
2019-02-26 11:13:18 -0500

    Fix Top Uploaders (Count) @lang value
    Fixed the value for Top Uploaders (Count) translation value within statsusermenu.blade.php.

--- resources/views/partials/statsusermenu.blade.php
+++ resources/views/partials/statsusermenu.blade.php
@@ -9,7 +9,7 @@
         <a href="{{ route('leechers') }}" class="btn btn-sm btn-primary"><i
                     class="{{ config('other.font-awesome') }} fa-arrow-down"></i> @lang('user.top-leechers')</a>
         <a href="{{ route('uploaders') }}" class="btn btn-sm btn-primary"><i
-                    class="{{ config('other.font-awesome') }} fa-upload"></i> @lang('user.top-uploaders-torrents')</a>
+                    class="{{ config('other.font-awesome') }} fa-upload"></i> @lang('user.top-uploaders-count')</a>
         <a href="{{ route('bankers') }}" class="btn btn-sm btn-primary"><i
                     class="{{ config('other.font-awesome') }} fa-star"></i> @lang('user.top-bankers')</a>
         {{--<a href="{{ route('seedtime') }}" class="btn btn-sm btn-primary"><i class="{{ config('other.font-awesome') }} fa-clock"></i> @lang('user.top-seedtime')</a>--}}
