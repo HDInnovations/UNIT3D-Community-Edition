410d2a0982abe03154605a80ac1aeac137ff8c2a
Morgan Wong <pbodq2@yahoo.com.hk>
2019-04-14 21:27:13 +0800

    (Update) Replace all robot translations by human

--- resources/lang/cn/auth.php
+++ resources/lang/cn/auth.php
@@ -1,47 +1,67 @@
 <?php
 
 return [
-    'attemptsRemaining'          => '尝试剩余|尝试剩余',
-    'email'                      => '电子邮件',
-    'exceededTitle'              => '验证尝试超出',
-    'failed'                     => '这些凭证与我们的记录不符。',
-    'inputAlt1'                  => '代码输入1',
-    'inputAlt2'                  => '代码输入2',
-    'inputAlt3'                  => '代码输入3',
-    'inputAlt4'                  => '代码输入4',
-    'lockedUntil'                => '帐户已锁定至：',
-    'login'                      => '登录',
-    'login-now-on'               => '立即登录',
-    'logout'                     => '登出',
-    'lost-password'              => '忘记密码了吗？',
-    'lost-username'              => '忘记了你的用户名？',
-    'missingCode'                => '没有收到验证码？',
-    'need-invite'                => '开放注册已禁用，您需要邀请才能访问！',
-    'not-a-member'                => '尚未成员？小于30秒注册。',
-    'password'                   => '密码',
-    'recover-my-password'        => '恢复我的密码',
-    'remember-me'                => '记住我',
-    'returnButton'               => '回家',
-    'signup'                     => '注册',
-    'subtitle'                   => '需要验证',
-    'throttle'                   => '登录尝试过多。请再试一次 :seconds 秒。',
-    'title'                      => '两步验证',
-    'titleFailed'                => '验证失败',
-    'titlePassed'                => '大家好消息！',
-    'tryAgainIn'                 => '再试一次',
-    'unlock'                     => '开锁',
-    'user-icon'                  => '用户图标',
-    'username'                   => '用户名',
-    'verificationEmailButton'    => '立即验证',
-    'verificationEmailGreeting'  => '你好 :username',
-    'verificationEmailMessage'   => '您的帐户两步验证码为：',
-    'verificationEmailSentMsg'   => '验证电子邮件已发送！',
-    'verificationEmailSubject'   => '需要验证',
-    'verificationEmailSuccess'   => '成功！',
-    'verificationLockedMessage'  => '帐户被锁定！',
-    'verificationLockedTitle'    => '卫生署！',
-    'verificationModalConfBtn'   => '好',
-    'verificationWarningMessage' => '这是您的帐户锁定之前的最后一次尝试 :hours 小时。',
-    'verificationWarningTitle'   => '警告！',
-    'verifyButton'               => '校验',
+    /*
+    |--------------------------------------------------------------------------
+    | Default Authentication Language Lines
+    |--------------------------------------------------------------------------
+    */
+    'email'               => '邮箱',
+    'failed'              => '身份凭证与站点记录不符合。',
+    'login'               => '登陆',
+    'login-now-on'        => '立即登入',
+    'logout'              => '登出',
+    'lost-password'       => '忘了密码?',
+    'lost-username'       => '忘了用户名？',
+    'need-invite'         => '开放注册已关闭，请从已注册用户处获得邀请!',
+    'not-a-member'        => '尚未成员？30秒内便能完成注册。',
+    'password'            => '密码',
+    'recover-my-password' => '重设密码',
+    'remember-me'         => '记住我的登录',
+    'signup'              => '注册',
+    'throttle'            => '登录次数过多。请 :seconds 秒後再试。',
+    'unlock'              => '解锁',
+    'user-icon'           => '用户头像',
+    'username'            => '用户名',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Two Step Authentication Language Lines
+    |--------------------------------------------------------------------------
+    */
+    'subtitle'                      => '电邮制作中-需时数分钟-注意大小写',
+    'title'                         => '两步验证(电邮形式)',
+    'titleFailed'                   => '验证失败',
+    'titlePassed'                   => '各位，有好消息!',
+    
+    'inputAlt1'                     => 'Code Input 1',
+    'inputAlt2'                     => 'Code Input 2',
+    'inputAlt3'                     => 'Code Input 3',
+    'inputAlt4'                     => 'Code Input 4',
+    
+    'attemptsRemaining'             => '尝试次数剩馀|尝试次数剩馀',
+    'missingCode'                   => '没有收到验证码?',
+    'verifyButton'                  => '验证',
+    
+    'exceededTitle'                 => '验证尝试次数已达上限',
+    'lockedUntil'                   => '账户被锁定，直到:',
+    'returnButton'                  => '返回主页',
+    'tryAgainIn'                    => '稍後再尝试',
+    
+    'verificationEmailButton'       => '立即验证',
+    'verificationEmailGreeting'     => 'Hello :username',
+    'verificationEmailMessage'      => '账户的两步验证码为:',
+    'verificationEmailSubject'      => '要求验证',
+    
+    'verificationEmailSentMsg'      => '验证邮件已发送!',
+    'verificationEmailSuccess'      => '成功!',
+    
+    'verificationWarningMessage'    => '你的账户将被锁定 :hours 小时，这是你最後的一次尝试机会',
+    'verificationWarningTitle'      => '警告!',
+    
+    'verificationLockedMessage'     => '账户被锁定!',
+    'verificationLockedTitle'       => 'Doh!',
+    
+    'verificationModalConfBtn'      => 'Ok',
+    
 ];
--- resources/lang/cn/rss.php
+++ resources/lang/cn/rss.php
@@ -1,19 +1,22 @@
 <?php
-
-
+/*
+ * |--------------------------------------------------------------------------
+ * | RSS Language Lines
+ * |--------------------------------------------------------------------------
+ */
 return [
-    'create' => 'Create',
-    'create-private-feed' => 'Create Private RSS Feed',
-    'create-public-feed' => 'Create Public RSS Feed',
-    'delete' => 'Delete',
-    'edit' => 'Edit',
-    'edit-private-feed' => 'Edit Private RSS Feed',
-    'edit-public-feed' => 'Edit Public RSS Feed',
-    'feed' => 'Feed',
-    'feeds' => 'Feeds',
-    'name' => 'Name',
-    'public' => 'Public',
-    'private' => 'Private',
-    'rss' => 'RSS',
-    'type' => 'Type',
+    'create'        => '建立',
+    'create-private-feed'   => '建立私有 RSS 摘要',
+    'create-public-feed'   => '建立公开 RSS 摘要',
+    'delete'        => '删除',
+    'edit'          => '编辑',
+    'edit-private-feed'     => '编辑私有 RSS 摘要',
+    'edit-public-feed'     => '编辑私有 RSS 摘要',
+    'feed'          => '摘要',
+    'feeds'         => '摘要',
+    'name'          => '名称',
+    'public'        => '公开',
+    'private'       => '私有',
+    'rss'           => 'RSS',
+    'type'          => '规格',
 ];
