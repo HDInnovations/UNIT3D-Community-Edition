e179937ba4fccb185dadc9d89460815fc93b9bb1
Morgan Wong <pbodq2@yahoo.com.hk>
2019-04-14 21:07:28 +0800

    Differentiate the label for Upload button

--- resources/views/partials/side_nav.blade.php
+++ resources/views/partials/side_nav.blade.php
@@ -3,7 +3,7 @@
         <li class="nav-level">@lang('common.navigation')</li>
         <li>
             <a href="{{ route('home') }}">
-                <i class="{{ config('other.font-awesome') }} fa-home" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-home" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('common.home')</span>
                 <span class="selected"></span>
             </a>
@@ -16,98 +16,98 @@
             @else
                 <a href="{{ route('torrents') }}">
             @endif
-                <i class="{{ config('other.font-awesome') }} fa-magnet" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-magnet" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('torrent.torrents')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('forum_index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-comments" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-comments" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('forum.forums')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('upload') }}">
-                <i class="{{ config('other.font-awesome') }} fa-upload" style=" font-size: 18px; color: rgb(255,255,255);"></i>
-                <span class="menu-text">@lang('common.upload')</span>
+                <i class="{{ config('other.font-awesome') }} fa-upload" style=" font-size: 18px; color: #fff;"></i>
+                <span class="menu-text">@lang('common.publish')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('requests') }}">
-                <i class="{{ config('other.font-awesome') }} fa-copy" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-copy" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('request.requests')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('graveyard.index') }}">
-                <i class="fab fa-snapchat-ghost" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="fab fa-snapchat-ghost" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('graveyard.graveyard')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('gallery') }}">
-                <i class="{{ config('other.font-awesome') }} fa-images" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-images" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">Gallery</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('stats') }}">
-                <i class="{{ config('other.font-awesome') }} fa-chart-bar" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-chart-bar" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('common.extra-stats')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('polls') }}">
-                <i class="{{ config('other.font-awesome') }} fa-chart-pie" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-chart-pie" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('poll.polls')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('bonus_store') }}">
-                <i class="{{ config('other.font-awesome') }} fa-shopping-cart" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-shopping-cart" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('bon.bon') @lang('bon.store')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('inbox', ['username' => auth()->user()->username, 'id' => auth()->user()->id]) }}">
-                <i class="{{ config('other.font-awesome') }} fa-envelope" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-envelope" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('pm.inbox')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('bug') }}">
-                <i class="{{ config('other.font-awesome') }} fa-bug" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-bug" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('common.bug')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('home') }}/page/rules.1">
-                <i class="{{ config('other.font-awesome') }} fa-info-square" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-info-square" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('common.rules')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('home') }}/page/faq.3">
-                <i class="{{ config('other.font-awesome') }} fa-question-square" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-question-square" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('common.faq')</span>
                 <span class="selected"></span>
             </a>
         </li>
         <li>
             <a href="{{ route('rss.index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-rss" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                <i class="{{ config('other.font-awesome') }} fa-rss" style=" font-size: 18px; color: #fff;"></i>
                 <span class="menu-text">@lang('rss.rss')</span>
                 <span class="selected"></span>
             </a>
@@ -115,7 +115,7 @@
         @if (auth()->user()->group->is_modo)
             <li>
                 <a href="{{ route('staff_dashboard') }}">
-                    <i class="{{ config('other.font-awesome') }} fa-cogs" style=" font-size: 18px; color: rgb(255,255,255);"></i>
+                    <i class="{{ config('other.font-awesome') }} fa-cogs" style=" font-size: 18px; color: #fff;"></i>
                     <span class="menu-text">@lang('staff.staff-dashboard')</span>
                     <span class="selected"></span>
                 </a>
