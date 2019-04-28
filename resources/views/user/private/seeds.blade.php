e713d049efc58bb878e62fc85195d141138e4e17
Morgan Wong <pbodq2@yahoo.com.hk>
2019-04-28 22:02:23 +0800

    (Update) Align labels wordings
    minors.

--- resources/views/user/private/seeds.blade.php
+++ resources/views/user/private/seeds.blade.php
@@ -124,7 +124,7 @@
                         </div>
                     </div>
                     <div class="mx-0 mt-5 form-group fatten-me">
-                        <label for="qty" class="mt-5 col-sm-1 label label-default fatten-me">Sorting</label>
+                        <label for="qty" class="mt-5 col-sm-1 label label-default fatten-me">@lang('common.sort')</label>
                         <div class="col-sm-2">
                             <select id="sorting" name="sorting" trigger="change" class="form-control userFilter">
                                 <option value="hcreated_at">Created at</option>
@@ -138,7 +138,7 @@
                         </div>
                     </div>
                     <div class="mx-0 mt-5 form-group fatten-me">
-                        <label for="qty" class="mt-5 col-sm-1 label label-default fatten-me">Direction</label>
+                        <label for="qty" class="mt-5 col-sm-1 label label-default fatten-me">@lang('common.direction')</label>
                         <div class="col-sm-2">
                             <select id="direction" name="direction" trigger="change" class="form-control userFilter">
                                 <option value="desc">Descending</option>
