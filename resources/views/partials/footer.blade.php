919b035aa130f395c143d8d671a0bdddf665e630
Morgan Wong <pbodq2@yahoo.com.hk>
2019-04-29 00:42:31 +0800

    (Update) Minor update on hyperlinks

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -64,10 +64,12 @@
 
         <div class="col-md-2 l-footer-section">
             <h2 class="l-footer-section-title">@lang('common.other')</h2>
-            <a href="https://www.patreon.com/UNIT3D"
-               class="btn btn-xs btn-primary">@lang('common.patron')</a>
-            <a href="https://github.com/UNIT3D/UNIT3D"
-               class="btn btn-xs btn-primary">@lang('common.powered-by')</a>
+            <ul>
+            <li><a href="https://www.patreon.com/UNIT3D" target="_blank" 
+               class="btn btn-xs btn-primary">@lang('common.patron')</a></li>
+            <li><a href="https://github.com/UNIT3D/UNIT3D" target="_blank" 
+               class="btn btn-xs btn-primary">@lang('common.powered-by')</a></li>
+            </ul
         </div>
     </div>
 </div>
