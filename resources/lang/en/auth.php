e86257334ff89bffb83d5c59f60867e7a2f6bc6f
Morgan Wong <pbodq2@yahoo.com.hk>
2019-04-30 21:56:45 +0800

    (Update) Add multi-lang translation
    Added fields

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -6,8 +6,15 @@ return [
     | Default Authentication Language Lines
     |--------------------------------------------------------------------------
     */
+    'activation-error'    => 'Banned or Invalid Token Or Account Already Confirmed!',
+    'activation-success'  => 'Account Confirmed! You May Now Login!',
+    'allow-invite-appl'   => 'Open Reg Closed! You Must Be Invited To Register! However application signups are open. You Have Been Redirected To The Application Page!',
+    'allow-invite'        => 'Open Reg Closed! You Must Be Invited To Register! You Have Been Redirected To Login Page!',
+    'application-submitted'  => 'Your Application Has Been Submitted. You will receive a email soon!',
+	'banned' 			  => 'This account is Banned!',
     'email'               => 'E-mail',
     'failed'              => 'These credentials do not match our records.',
+    'invalid-key'         => 'Invalid or Expired Invite Key!',
     'login'               => 'Login',
     'login-now-on'        => 'Login now on',
     'logout'              => 'Logout',
@@ -15,14 +22,19 @@ return [
     'lost-username'       => 'Forgot Your Username?',
     'need-invite'         => 'Open Registration Is Disabled and You Will Need A Invite To Gain Access!',
     'not-a-member'        => 'Not yet member ? Signup in less than 30s.',
+	'not-activated'       => 'This account has not been activated and is still in validating group. Please check your email for activation link. If you did not receive the activation code, please click "forgot password" and complete the steps.',    
     'password'            => 'Password',
     'recover-my-password' => 'Recover My Password',
+    'register-thanks'     => 'Thanks for signing up! Please check your email to Validate your account',
     'remember-me'         => 'Remember Me',
+	'require-rules'    	  => 'Please Read And Accept Our Rules By Scrolling To Bottom Of Page.',    
     'signup'              => 'Signup',
     'throttle'            => 'Too many login attempts. Please try again in :seconds seconds.',
     'unlock'              => 'Unlock',
     'user-icon'           => 'User icon',
     'username'            => 'Username',
+    'welcome'             => 'Welcome Back!',
+	'welcome-restore'     => 'Welcome Back! Your Account Is No Longer Disabled!',
 
     /*
     |--------------------------------------------------------------------------
