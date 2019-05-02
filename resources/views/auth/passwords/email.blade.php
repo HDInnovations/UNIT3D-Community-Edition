84b38bbcdfae9646939abe951cd0534c9778bdbd
Morgan Wong <pbodq2@yahoo.com.hk>
2019-05-02 22:40:56 +0800

    (Update) Add captcha support

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -53,6 +53,18 @@
         <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
             @csrf
             <input type="email" id="email" class="fadeIn third" name="email" placeholder="@lang('auth.email')" required autofocus>
+            
+            @if (config('captcha.enabled') == true)				
+                <div class="text-center">
+                    <div class="g-recaptcha" data-sitekey="{{ config('captcha.sitekey') }}"></div>
+                    @if ($errors->has('g-recaptcha-response'))
+                        <span class="invalid-feedback" style="display: block;">
+                            <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                        </span>
+                    @endif
+                </div>                
+            @endif	
+            
             <button type="submit" class="fadeIn fourth">@lang('common.submit')</button>
         </form>
 
@@ -65,6 +77,11 @@
 </div>
 
 <script type="text/javascript" src="{{ mix('js/app.js') }}" integrity="{{ Sri::hash('js/app.js') }}" crossorigin="anonymous"></script>
+
+@if (config('captcha.enabled') == true)
+<script type="text/javascript" src="https://www.google.com/recaptcha/api.js"></script>
+@endif
+
 @foreach (['warning', 'success', 'info'] as $key)
     @if (Session::has($key))
         <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
