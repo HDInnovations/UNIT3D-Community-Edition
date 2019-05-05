8547382981dd0d554cff836907fc07747eb98a9d
Morgan Wong <pbodq2@yahoo.com.hk>
2019-05-05 17:32:55 +0800

    (Update) Add captcha support
    Overriding the function validateEmail with extra condition

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -16,6 +16,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -25,4 +27,17 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+    
+    protected function validateEmail(Request $request)
+    {
+        if (config('captcha.enabled') == false) {
+		$request->validate(['email' => 'required|email']);
+		}
+		else{
+			$request->validate([
+                'email' => 'required|email',
+                'g-recaptcha-response' => 'required|recaptcha',
+			]);
+		}
+    }	    
 }
