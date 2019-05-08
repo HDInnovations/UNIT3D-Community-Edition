c4341846671a8e153b65454bd5edc0601e3c1a7c
HDVinnie <unit3d@protonmail.com>
2019-05-08 12:25:40 -0400

    (Update) Bengali Language Support :earth_americas:

--- resources/lang/bn/articles.php
+++ resources/lang/bn/articles.php
@@ -1,8 +1,7 @@
-<?php
-
-return [
-    'articles'      => 'প্রবন্ধ',
-    'meta-articles' => 'ট্র্যাকার এবং সম্প্রদায়ের নিবন্ধ এবং খবর',
-    'published-at'  => 'প্রকাশিত এ',
-    'read-more'     => 'আরও পড়ুন',
-];
+<?php 
+return array (
+  'articles' => 'প্রবন্ধ',
+  'meta-articles' => 'ট্র্যাকার এবং সম্প্রদায়ের নিবন্ধ এবং খবর',
+  'published-at' => 'প্রকাশিত',
+  'read-more' => 'আরো পড়ুন',
+);
--- resources/lang/bn/blocks.php
+++ resources/lang/bn/blocks.php
@@ -1,19 +1,18 @@
-<?php
-
-return [
-    'active-in-last'          => 'শেষ পর্যন্ত সক্রিয়',
-    'chatbox'                 => 'চ্যাট বক্স',
-    'check-news'              => 'খবর (প্রতিদিন পরীক্ষা)',
-    'click'                   => 'ক্লিক',
-    'featured-by'             => 'দ্বারা বৈশিষ্ট্যযুক্ত',
-    'featured-torrents'       => 'বৈশিষ্ট্যযুক্ত টরেন্টস',
-    'featured-torrents-intro' => 'আপনি পেতে পারেন যখন তাদের পেতে!',
-    'featured-until'          => 'এই পর্যন্ত ট্রাড বৈশিষ্ট্যযুক্ত হয়',
-    'latest-posts'            => 'সর্বশেষ পোস্ট',
-    'latest-topics'           => 'সর্বশেষ বিষয়গুলি',
-    'latest-torrents'         => 'সর্বশেষ টরেন্টস',
-    'new-news'                => 'নতুন খবর',
-    'new-torrents'            => 'নতুন টরেন্টস',
-    'to-enable-editor'        => 'সম্পাদক সক্ষম করতে',
-    'users-online'            => 'ব্যবহারকারী অনলাইন',
-];
+<?php 
+return array (
+  'chatbox' => 'চ্যাট বক্স',
+  'click' => 'ক্লিক',
+  'to-enable-editor' => 'সম্পাদক সক্রিয় করতে',
+  'featured-by' => 'দ্বারা বৈশিষ্ট্যযুক্ত',
+  'featured-torrents' => 'বৈশিষ্ট্যযুক্ত টরেন্টস',
+  'featured-torrents-intro' => 'আপনি করতে পারেন যখন তাদের পেতে!',
+  'featured-until' => 'এই পর্যন্ত টরেন্ট বৈশিষ্ট্যযুক্ত করা হয়',
+  'top-torrents' => 'শীর্ষ টরেন্টস',
+  'latest-posts' => 'সর্বশেষ পোস্ট',
+  'latest-topics' => 'সর্বশেষ বিষয়',
+  'active-in-last' => 'শেষ সক্রিয়',
+  'users-online' => 'ব্যবহারকারীদের অনলাইন',
+  'check-news' => 'খবর (প্রতিদিন চেক করুন)',
+  'new-news' => 'নতুন খবর',
+  'new-torrents' => 'নতুন টরেন্টস',
+);
