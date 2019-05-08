92bae2d14f5b9b01bd5f74bb61d344244cdc0fa5
HDVinnie <unit3d@protonmail.com>
2019-05-08 16:47:07 -0400

    (Add) Thai Language Support :earth_americas:

---
+++ resources/lang/th/blocks.php
@@ -0,0 +1,18 @@
+<?php 
+return array (
+  'chatbox' => 'กล่องแชท',
+  'click' => 'คลิก',
+  'to-enable-editor' => 'เพื่อเปิดใช้งานเครื่องมือแก้ไข',
+  'featured-by' => 'แนะนำโดย',
+  'featured-torrents' => 'เพลงเด่น',
+  'featured-torrents-intro' => 'รับในขณะที่คุณสามารถ!',
+  'featured-until' => 'นี่คือคุณลักษณะฝนตกหนักจนถึง',
+  'top-torrents' => 'เพลงยอดนิยม',
+  'latest-posts' => 'โพสต์ล่าสุด',
+  'latest-topics' => 'หัวข้อล่าสุด',
+  'active-in-last' => 'ใช้งานล่าสุด',
+  'users-online' => 'ผู้ใช้งานออนไลน์',
+  'check-news' => 'ข่าว (ตรวจสอบรายวัน)',
+  'new-news' => 'ข่าวใหม่',
+  'new-torrents' => 'เพลงใหม่',
+);
---
+++ resources/lang/th/graveyard.php
@@ -0,0 +1,14 @@
+<?php 
+return array (
+  'dead' => 'ตาย',
+  'graveyard' => 'สุสาน',
+  'guidelines' => 'แนวทาง',
+  'guidelines-content' => '1) คุณไม่สามารถคืนค่าการอัปโหลดของคุณเองอีกครั้ง <br> 2) อย่ารื้อฟื้นสิ่งที่คุณไม่สามารถกระทำได้เช่นกัน',
+  'howto' => 'นี่คือกฎ',
+  'howto-desc1' => 'คุณต้องเพาะ <span class=\\"text-green\\"><em>: ชื่อเป็น</em></span> เวลา <span class=\\"text-red text-bold\\">30 วัน</span> สำหรับ ressurection ในกรณีนี้เมื่อ seedtime ปัจจุบันของคุณ',
+  'howto-desc2' => 'คุณจะได้รับรางวัล',
+  'howto-hits' => 'ฮิต',
+  'pending' => 'รอดำเนินการ',
+  'resurrect' => 'ชุบชีวิต',
+  'reward' => 'โทเค็นฟรี',
+);
