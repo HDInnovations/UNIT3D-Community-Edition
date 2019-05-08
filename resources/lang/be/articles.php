afd678ef780ec5c67a348b06f00ca6b2aac3a11c
HDVinnie <unit3d@protonmail.com>
2019-05-08 12:20:22 -0400

    (Add) Belarusian Language Support :earth_americas:

---
+++ resources/lang/be/articles.php
@@ -0,0 +1,7 @@
+<?php 
+return array (
+  'articles' => 'артыкулы',
+  'meta-articles' => 'Артыкулы і навіны на трэкеры і супольнасці',
+  'published-at' => 'Published On',
+  'read-more' => 'Чытаць далей',
+);
---
+++ resources/lang/be/rss.php
@@ -0,0 +1,18 @@
+<?php 
+return array (
+  'create' => 'стварыць',
+  'create-private-feed' => 'Стварэнне прыватнага RSS паток',
+  'create-public-feed' => 'Стварыць Грамадскі RSS паток',
+  'delete' => 'выдаляць',
+  'edit' => 'рэдагаваць',
+  'edit-private-feed' => 'Рэдагаваць Private RSS паток',
+  'edit-public-feed' => 'Рэдагаваць Адкрыты RSS паток',
+  'feed' => 'падача',
+  'feeds' => 'стужкі',
+  'name' => 'імя',
+  'public' => 'грамадскага',
+  'private' => 'прыватны',
+  'rss' => 'RSS',
+  'rss-feed' => 'RSS Падача',
+  'type' => 'тып',
+);
