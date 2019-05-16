a859cf45ec32749eb52a39fbc0cc51dd84cd867c
Morgan Wong <pbodq2@yahoo.com.hk>
2019-05-17 04:25:09 +0800

    (Update) fine tune translation :earth_americas:

--- resources/views/user/private/unsatisfieds.blade.php
+++ resources/views/user/private/unsatisfieds.blade.php
@@ -59,7 +59,7 @@
                         </div>
                     </div>
                     <div class="mx-0 mt-5 form-group fatten-me">
-                        <label for="name" class="mt-5 col-sm-1 label label-default fatten-me">Seeding</label>
+                        <label for="name" class="mt-5 col-sm-1 label label-default fatten-me">@lang('torrent.seeding')</label>
                         <div class="col-sm-10">
                         <span class="badge-user">
                     <label class="inline">
@@ -77,15 +77,15 @@
                         <label for="qty" class="mt-5 col-sm-1 label label-default fatten-me">@lang('common.sort')</label>
                         <div class="col-sm-2">
                             <select id="sorting" name="sorting" trigger="change" class="form-control userFilter">
-                                <option value="satisfied_at">Satisfied in</option>
-                                <option value="completed_at">Completed at</option>
-                                <option value="name">Name</option>
-                                <option value="seeder">Seeding</option>
-                                <option value="size">Size</option>
-                                <option value="seeders">Seeders</option>
-                                <option value="leechers">Leechers</option>
-                                <option value="times_completed">Times Completed</option>
-                                <option value="seedtime">Seedtime</option>
+                                <option value="satisfied_at">@lang('torrent.satisfied_in')</option>
+                                <option value="completed_at">@lang('torrent.completed_at')</option>
+                                <option value="name">@lang('torrent.name')</option>
+                                <option value="seeder">@lang('torrent.seeding')</option>
+                                <option value="size">@lang('torrent.size')</option>
+                                <option value="seeders">@lang('torrent.seeders')</option>
+                                <option value="leechers">@lang('torrent.leechers')</option>
+                                <option value="times_completed">@lang('torrent.completed-times')</option>
+                                <option value="seedtime">@lang('torrent.seedtime')</option>
                             </select>
                         </div>
                     </div>
