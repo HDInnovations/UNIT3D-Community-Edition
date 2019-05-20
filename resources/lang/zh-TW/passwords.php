6acac049ffe92cece8d267788c6e4cf938a747fc
Morgan Wong <pbodq2@yahoo.com.hk>
2019-05-21 03:53:09 +0800

    (Update) Fine tune translation :earth_americas:

---
+++ resources/lang/zh-TW/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密碼必須包含最少6字元及確認一致.',
+    'reset'    => '密碼已重置!',
+    'sent'     => '已發出重置密碼的電郵!',
+    'token'    => '連結無效.',
+    'user'     => "找不到正確的電郵地址.",
+];
