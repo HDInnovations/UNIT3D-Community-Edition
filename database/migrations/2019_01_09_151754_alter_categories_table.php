12052852a12cdc56b413bb305093730e54da101b
HDVinnie <unit3d@protonmail.com>
2019-07-22 12:49:07 -0400

    (Update) Alter Categories Migration :rocket:

--- database/migrations/2019_01_09_151754_alter_categories_table.php
+++ database/migrations/2019_01_09_151754_alter_categories_table.php
@@ -14,13 +14,13 @@ class AlterCategoriesTable extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->dropColumn('meta');
             $table->string('image')->nullable()->after('slug');
-            $table->boolean('movie_meta')->default(0);
-            $table->boolean('tv_meta')->default(0);
-            $table->boolean('game_meta')->default(0);
-            $table->boolean('music_meta')->default(0);
-            $table->boolean('no_meta')->default(0);
+            $table->boolean('movie_meta')->default(0)->after('meta');
+            $table->boolean('tv_meta')->default(0)->after('meta');
+            $table->boolean('game_meta')->default(0)->after('meta');
+            $table->boolean('music_meta')->default(0)->after('meta');
+            $table->boolean('no_meta')->default(0)->after('meta');
+            $table->dropColumn('meta');
         });
     }
 
