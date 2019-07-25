31871cadc2f64b92f331a8abfe08b31368ae2212
HDVinnie <unit3d@protonmail.com>
2019-07-25 14:19:05 -0400

    (Add) Command To Fill Torrent Years On Backlogged Torrents :rocket:

---
+++ app/Console/Commands/FetchReleaseYears.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie
+ */
+
+namespace App\Console\Commands;
+
+use App\Models\Torrent;
+use Illuminate\Console\Command;
+use MarcReichel\IGDBLaravel\Models\Game;
+
+class FetchReleaseYears extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:release_years';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch Release Years For Torrents In DB';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     * @throws \ErrorException
+     */
+    public function handle()
+    {
+        $client = new \App\Services\MovieScrapper(config('api-keys.tmdb'), config('api-keys.tvdb'), config('api-keys.omdb'));
+
+        $torrents = Torrent::withAnyStatus()
+            ->select(['id', 'category_id', 'imdb', 'tmdb'])
+            ->get();
+
+        foreach ($torrents as $torrent) {
+            $meta = null;
+
+            if ($torrent->category->tv_meta) {
+                if ($torrent->tmdb && $torrent->tmdb != 0) {
+                    $meta = $client->scrape('tv', null, $torrent->tmdb);
+                } else {
+                    $meta = $client->scrape('tv', 'tt'.$torrent->imdb);
+                }
+            }
+
+            if ($torrent->category->movie_meta) {
+                if ($torrent->tmdb && $torrent->tmdb != 0) {
+                    $meta = $client->scrape('movie', null, $torrent->tmdb);
+                } else {
+                    $meta = $client->scrape('movie', 'tt'.$torrent->imdb);
+                }
+            }
+
+            if ($torrent->category->game_meta) {
+                if ($torrent->igdb && $torrent->igdb != 0) {
+                    $meta = Game::find($torrent->igdb);
+                }
+            }
+
+            // Save Release Year If Exsist
+            if ($meta->releaseYear) {
+                $torrent->release_year = $meta->releaseYear;
+                $torrent->save();
+            }
+            if ($meta->first_release_date) {
+                $torrent->release_year = date('Y', strtotime( $meta->first_release_date));
+                $torrent->save();
+            }
+
+            // sleep for 2 seconds
+            sleep(2);
+        }
+    }
+}
