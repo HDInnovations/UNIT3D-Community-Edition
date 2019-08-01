ec6d066db6e7faea40efa4c2b1c1533cc85ac012
HDVinnie <unit3d@protonmail.com>
2019-08-01 17:36:55 -0400

    (Add) NIST Password Validation Lang :new:

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -126,8 +126,14 @@ return [
     |
     */
 
-    'email_list' => 'Sorry, this email domain is not allowed to be used on this site. Please see sites email whitelist.',
-    'recaptcha' => 'Please Complete The ReCaptcha.',
+    'email_list'                        => 'Sorry, this email domain is not allowed to be used on this site. Please see sites email whitelist.',
+    'recaptcha'                         => 'Please Complete The ReCaptcha.',
+    'can-not-contain-word'              => 'The :attribute can not contain the word \':word\'.',
+    'can-not-be-similar-to-word'        => 'The :attribute can not be similar to the word \':word\'.',
+    'found-in-data-breach'              => 'The :attribute was found in a third party data breach, and can not be used.',
+    'can-not-be-dictionary-word'        => 'The :attribute "\':word\'" can not be a dictionary word.',
+    'can-not-be-repetitive-characters'  => 'The :attribute can not have repetitive characters of 3 or more.',
+    'can-not-be-sequential-characters'  => 'The :attribute can not have sequential characters of 3 or more.',
 
     'custom' => [
         'attribute-name' => [
