f50612cad6620eca6f88c0498a60726ed062d1c3
HDVinnie <unit3d@protonmail.com>
2019-09-09 07:29:09 -0400

    (Update) custom.js :rocket:
    - closes #849

--- resources/js/unit3d/custom.js
+++ resources/js/unit3d/custom.js
@@ -44,3 +44,11 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
+$(document).ready(function() {
+    $(window).keydown(function(event){
+        if((event.keyCode == 13) && ($(event.target)[0]!=$("textarea")[0])) {
+            event.preventDefault();
+            return false;
+        }
+    });
+});
