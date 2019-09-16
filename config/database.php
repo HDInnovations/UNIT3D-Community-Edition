0164c43fda308706933c8d7013c1224bae44e954
HDVinnie <unit3d@protonmail.com>
2019-09-16 13:23:03 -0400

    (Uodate) Database Config :rocket:

--- config/database.php
+++ config/database.php
@@ -119,7 +119,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -159,6 +159,15 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_BROADCAST_DB', 3),
+            'read_write_timeout' => -1,
+        ],
+
+        'session' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_BROADCAST_DB', 4),
             'read_write_timeout' => -1,
         ],
 
