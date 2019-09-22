efac751c48a9ca614eebd0c4d36c3c1ca5f04d80
HDVinnie <unit3d@protonmail.com>
2019-09-22 17:06:53 -0400

    (Add) Models :new:

---
+++ app/Models/PlaylistTorrent.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ *
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlaylistTorrent extends Model
+{
+    /**
+     * Indicates If The Model Should Be Timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Has Many Torrents.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function torrents()
+    {
+        return $this->hasMany(Torrent::class);
+    }
+}
