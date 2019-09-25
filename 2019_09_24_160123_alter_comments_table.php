f283e9f8e6e7dcbd4b5ffbb27e9c31814f079f96
HDVinnie <unit3d@protonmail.com>
2019-09-25 10:52:31 -0400

    (Update) Comments Migration :rocket:

---
+++ 2019_09_24_160123_alter_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('playlist_id')->nullable()->index()->after('requests_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn('playlist_id');
+        });
+    }
+}
