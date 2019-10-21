cb898a145503ac2530f7a5febe06e4eb51f18f67
HDVinnie <unit3d@protonmail.com>
2019-10-20 22:29:59 -0400

    (Fix) Chatbox :bug:
    - closes #864
    - laravel-echo client does not support to set a string prefix for channel name. Thus setting a prefix in the database config file for REDIS was the issue.
    - Please note you MAY need to run `php artisan cache:clear && php artisan clear:all_cache`.

--- config/database.php
+++ config/database.php
@@ -127,11 +127,6 @@ return [
 
         'client' => env('REDIS_CLIENT', 'predis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-        ],
-
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
