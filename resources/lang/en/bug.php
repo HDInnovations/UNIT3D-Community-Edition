e339cd852ef2401c2bd64ae29ffeb4dec50b612d
Gizmo3D <34943376+HD4Free-Gizmo3D@users.noreply.github.com>
2019-11-18 06:34:33 -0800

    Cleanup

--- resources/lang/en/bug.php
+++ resources/lang/en/bug.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'bug-report'             => 'Bug report',
-    'bug-report-description' => 'Report a site bug',
+    'bug-report'             => 'Bug Report',
+    'bug-report-description' => 'Report a Site Bug',
     'enter-description'      => 'Describe the problem as best as possible',
     'enter-email'            => 'Please enter your e-mail',
     'enter-title'            => 'Please choose a proper title',
@@ -11,5 +11,5 @@ return [
     'low'                    => 'Low',
     'priority'               => 'Priority',
     'priority-description'   => 'Choose only very high if the bug really is a problem for using the site.',
-    'very-high'              => 'Very high',
+    'very-high'              => 'Very High',
 ];
