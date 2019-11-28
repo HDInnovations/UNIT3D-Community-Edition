a557e92fc478dbb25655a51c7afb714707844480
HDVinnie <unit3d@protonmail.com>
2019-11-28 17:21:30 -0500

    (Add) API BaseController :new:

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
