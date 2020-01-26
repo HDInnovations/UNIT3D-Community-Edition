1f4ec0014986d8817393a70e97a23db9148f0c85
Alex Silva <ialexsilva@users.noreply.github.com>
2020-01-26 15:03:04 -0300

    Update auth.php

--- resources/lang/pt/auth.php
+++ resources/lang/pt/auth.php
@@ -13,5 +13,5 @@ return [
     */
 
     'failed'   => 'As credenciais indicadas não coincidem com as registadas no sistema.',
-    'throttle' => 'O número limite de tentativas de login foi atingido. Por favor tente novamente dentro de :seconds segundos.',
+    'throttle' => 'O número limite de tentativas de login foi atingido. Por favor, tente novamente dentro de :seconds segundos.',
 ];
