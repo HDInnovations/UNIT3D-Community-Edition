e37109f7ee8b80d15e121eafe06b9cd6fb27791e
Alex Silva <ialexsilva@users.noreply.github.com>
2020-01-28 17:34:13 -0300

    Update passwords.php

--- resources/lang/pt/passwords.php
+++ resources/lang/pt/passwords.php
@@ -12,9 +12,9 @@ return [
     |
     */
 
-    'password' => 'A palavra-passe deverá conter pelo menos seis carateres e ser igual à confirmação.',
-    'reset'    => 'A palavra-passe foi redefinida!',
-    'sent'     => 'O lembrete para a palavra-passe foi enviado!',
-    'token'    => 'Este código de recuperação da palavra-passe é inválido.',
-    'user'     => 'Não existe nenhum utilizador com o endereço de correio eletrónico indicado.',
+    'password' => 'As senhas devem ter pelo menos seis caracteres e coincidir com a confirmação.',
+    'reset'    => 'A sua senha foi redefinida!',
+    'sent'     => 'Enviamos por e-mail o link para redefinir a sua senha!',
+    'token'    => 'Este código de redefinição de senha é inválido.',
+    'user'     => 'Não existe nenhum usuário com o endereço de e-mail indicado.',
 ];
