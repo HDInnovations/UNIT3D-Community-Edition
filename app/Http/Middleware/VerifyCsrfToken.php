80c842e39cf925b9fec04a60ff9fded39e03805f
HDVinnie <unit3d@protonmail.com>
2020-02-12 14:49:18 -0500

    chore: update license block
    - make styleci happy

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,7 +7,9 @@
  * The details is bundled with this project in the file LICENSE.txt.
  *
  * @project    UNIT3D Community Edition
+ *
  * @author     HDVinnie <hdinnovations@protonmail.com>
+ *
  * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
  */
 
--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -6,7 +6,9 @@
  * The details is bundled with this project in the file LICENSE.txt.
  *
  * @project    UNIT3D Community Edition
+ *
  * @author     HDVinnie <hdinnovations@protonmail.com>
+ *
  * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
  */
 define('LARAVEL_START', microtime(true));
