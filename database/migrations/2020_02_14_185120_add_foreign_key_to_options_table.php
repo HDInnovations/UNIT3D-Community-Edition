f5bf7b63914566fe8c23f5e6f9b660f071ce69f8
gyakkun <xiaowufeixia@gmail.com>
2020-02-15 14:40:12 +0800

    fix: Typo found by Travis.

--- database/migrations/2020_02_14_185120_add_foreign_key_to_options_table.php
+++ database/migrations/2020_02_14_185120_add_foreign_key_to_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddForeignKeysToOptionsTable extends Migration
+class AddForeignKeyToOptionsTable extends Migration
 {
     /**
      * Run the migrations.
