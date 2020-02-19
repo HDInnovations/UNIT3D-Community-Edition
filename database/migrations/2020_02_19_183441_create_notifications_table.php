53b8411e71ae4b92eb9118254256ae40d2c413dd
HDVinnie <unit3d@protonmail.com>
2020-02-19 13:51:20 -0500

    chore: styleci

--- database/migrations/2020_02_19_183441_create_notifications_table.php
+++ database/migrations/2020_02_19_183441_create_notifications_table.php
@@ -1,15 +1,26 @@
 <?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateNotificationsTable extends Migration
 {
     /**
      * Run the migrations.
      *
-     * @return  void
+     * @return void
      */
     public function up()
     {
@@ -26,11 +37,10 @@ class CreateNotificationsTable extends Migration
     /**
      * Reverse the migrations.
      *
-     * @return  void
+     * @return void
      */
     public function down()
     {
         Schema::dropIfExists('notifications');
     }
-
 }
