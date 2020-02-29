e80eef5ae13f5da17973b491404b30d111867726
Ben Johnson <ben@indiehd.com>
2020-02-28 20:31:14 -0500

    tests: Add tests for Bookmark and Category controllers

---
+++ tests/Feature/Http/Controllers/CategoryControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\User;
+use GroupsTableSeeder;
+use Tests\TestCase;
+
+/**
+ * @see \App\Http\Controllers\CategoryController
+ */
+class CategoryControllerTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(GroupsTableSeeder::class);
+    }
+
+    /** @test */
+    public function index_returns_an_ok_response()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('categories.index'));
+
+        $response->assertOk()
+            ->assertViewIs('category.index')
+            ->assertViewHas('categories');
+    }
+
+    /** @test */
+    public function show_returns_an_ok_response()
+    {
+        $category = factory(Category::class)->create();
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('categories.show', ['id' => $category->id]));
+
+        $response->assertOk()
+            ->assertViewIs('category.show')
+            ->assertViewHas('torrents')
+            ->assertViewHas('user')
+            ->assertViewHas('category')
+            ->assertViewHas('personal_freeleech');
+    }
+}
