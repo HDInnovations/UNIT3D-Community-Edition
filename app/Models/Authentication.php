6c33f984edb71676585b267f6c3409af68eb9b84
HDVinnie <unit3d@protonmail.com>
2020-03-04 18:41:37 -0500

    add: authentication model

---
+++ app/Models/Authentication.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Authentication extends Model
+{
+    /** @var string */
+    const TYPE_LOGIN = 'login';
+    const TYPE_FAILED = 'failed';
+    const TYPE_LOCKOUT = 'lockout';
+
+    /**
+     * The Attributes That Should Be Casted To Native Types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'user_id'    => 'integer',
+        'device_id'  => 'integer',
+        'ip_address' => 'string',
+    ];
+
+    /**
+     * Belongs To A User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault([
+            'username' => 'System',
+            'id'       => '1',
+        ]);
+    }
+
+    /**
+     * Belongs To A Device.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
+}
