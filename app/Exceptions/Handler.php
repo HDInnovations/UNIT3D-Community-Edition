c623f512ad30c84d02ad9dcee4c0c057231c10b9
Laravel Shift <shift@laravelshift.com>
2020-03-17 12:57:54 +0000

    Shift to Throwable

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,6 +13,7 @@
 
 namespace App\Exceptions;
 
+use Throwable;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
@@ -46,13 +47,13 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param \Exception $exception
+     * @param  \Throwable  $exception
      *
-     * @throws \Exception
+     * @throws \Throwable
      *
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -61,13 +62,13 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \Exception               $exception
+     * @param  \Throwable  $exception
      *
-     * @throws \Exception
+     * @throws \Throwable
      *
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
