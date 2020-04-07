10bce46772fe5138933ae45a961fae262409ff7c
HDVinnie <unit3d@protonmail.com>
2020-04-07 19:26:25 -0400

    add: award logic for future usage

--- app/Achievements/UserUploadedFirstSubtitle.php
+++ app/Achievements/UserUploadedFirstSubtitle.php
@@ -15,4 +15,21 @@ class UserUploadedFirstSubtitle extends Achievement
      * A small description for the achievement
      */
     public $description = 'Congratulations! You have made your first subtitle upload!';
+
+    /*
+    * A small description for the award
+    */
+    //public $award = "1000 Bonus Points";
+
+    /*
+    * Triggers whenever an Achiever unlocks this achievement
+    */
+    /*public function whenUnlocked($progress)
+    {
+        $achiever = $progress->achiever;
+
+        $user = User::findOrFail($achiever->id);
+        $user->seedbonus += 1000;
+        $user->save();
+    }*/
 }
