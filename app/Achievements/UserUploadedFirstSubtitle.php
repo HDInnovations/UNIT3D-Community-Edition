6f7759a9be61107182bddab7ffe3d2885d3eec8c
HDVinnie <unit3d@protonmail.com>
2020-04-07 17:26:35 -0400

    add: subtitle achievements

---
+++ app/Achievements/UserUploadedFirstSubtitle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Achievements;
+
+use Gstt\Achievements\Achievement;
+
+class UserUploadedFirstSubtitle extends Achievement
+{
+    /*
+     * The achievement name
+     */
+    public $name = "UserUploadedFirstSubtitle";
+
+    /*
+     * A small description for the achievement
+     */
+    public $description = "Congratulations! You have made your first subtitle upload!";
+}
