30a6d3a7feac545423cac7c1fccfb2ba0f1694d5
HDVinnie <unit3d@protonmail.com>
2020-04-14 20:45:38 -0400

    refactor: null coalescing operator

--- app/Http/Middleware/Http2ServerPush.php
+++ app/Http/Middleware/Http2ServerPush.php
@@ -97,7 +97,7 @@ class Http2ServerPush
             })
             ->take($limit);
 
-        $sizeLimit = $sizeLimit ?? max(1, intval($this->getConfig('size_limit', 32 * 1_024)));
+        $sizeLimit ??= max(1, intval($this->getConfig('size_limit', 32 * 1_024)));
         $headersText = trim($headers->implode(','));
         while (strlen($headersText) > $sizeLimit) {
             $headers->pop();
