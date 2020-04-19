7a119993fed752dc71052f85d45f219a4ff7783a
innocente205 <57386850+innocente205@users.noreply.github.com>
2020-04-19 12:58:49 +0700

    Update ChatRepository.php
    Lookup the 'command' instead of the 'slug' to allow changing the systembot name.

--- app/Repositories/ChatRepository.php
+++ app/Repositories/ChatRepository.php
@@ -313,7 +313,7 @@ class ChatRepository
         if ($bot) {
             $this->message($systemUserId, $this->systemChatroom(), $message, null, $bot);
         } else {
-            $systemBotId = Bot::where('slug', 'systembot')->first()->id;
+            $systemBotId = Bot::where('command', 'systembot')->first()->id;
 
             $this->message($systemUserId, $this->systemChatroom(), $message, null, $systemBotId);
         }
