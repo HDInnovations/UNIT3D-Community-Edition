946905bbb8a79ad257c2d520b7d206c238927e2f
Viktor Mazur <vmazu@softserveinc.com>
2020-04-24 01:01:27 +0300

    Apply fixes from StyleCI

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -13,12 +13,12 @@
 
 namespace App\Http\Controllers;
 
-use Image;
 use App\Album;
-use Carbon\Carbon;
-use Brian2694\Toastr\Toastr;
-use Illuminate\Http\Request;
 use App\Services\Clients\OmdbClient;
+use Brian2694\Toastr\Toastr;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Image;
 
 class AlbumController extends Controller
 {
