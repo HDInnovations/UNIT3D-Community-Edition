9e05b5b6e02868f312b7afeb83260bb7a0b8c093
Howard Lau <howardlau1999@hotmail.com>
2020-04-23 23:56:09 +0800

    add: translations for cookies consent zh-CN

---
+++ resources/lang/vendor/cookieConsent/zh-CN/texts.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Howard Lau <howardlau1999@hotmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'message' => '允许 Cookies 将有助于改善您在本站的体验',
+    'agree' => '允许 Cookies',
+];
