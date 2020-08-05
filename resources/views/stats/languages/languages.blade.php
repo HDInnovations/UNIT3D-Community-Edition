a335bd89c3aca204eb55fb246147cf48190c4bb3
HDVinnie <unit3d@protonmail.com>
2020-08-05 00:42:59 -0400

    update: language stats view

--- resources/views/stats/languages/languages.blade.php
+++ resources/views/stats/languages/languages.blade.php
@@ -22,18 +22,12 @@
         <div class="block">
             <h2>@lang('stat.languages')</h2>
             <hr>
-            <p class="text-red"><strong><i class="{{ config('other.font-awesome') }} fa-flag"></i>
-                    @lang('stat.languages')</strong></p>
             <div class="row col-md-offset-1">
                 @foreach ($languages as $code => $name)
-                    <div class="well col-md-2" style="margin: 10px;">
+                    <div class="well col-md-3" style="margin: 10px;">
                         <div class="text-center">
-                            <img src="{{ url('img/flags/' . $code . '.png') }}" alt="{{ $name }}" style=" border-radius: 5px"
-                                height="100px" width="150px" />
-                            <br>
-                            <br>
-                            <p class="text-success">{{ $name }}</p>
-                    @php $count = App\Models\User::where('locale', '=', $code)->count(); @endphp
+                            <h3 class="text-success">{{ $name }}</h3>
+                            @php $count = App\Models\User::where('locale', '=', $code)->count(); @endphp
                             <span class="badge-extra text-blue">Used By {{ $count }} Users</span>
                         </div>
                     </div>
