7756916925af9637d3a500851d281f636c616a58
HDVinnie <unit3d@protonmail.com>
2020-08-17 18:42:10 -0400

    update: announce controller
    - add credits for Rhilip

--- app/Http/Controllers/AnnounceController.php
+++ app/Http/Controllers/AnnounceController.php
@@ -9,6 +9,7 @@
  *
  * @author     HDVinnie <hdinnovations@protonmail.com>
  * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @credits    Rhilip
  */
 
 namespace App\Http\Controllers;
