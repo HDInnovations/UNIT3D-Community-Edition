524423bcd50a27758d1bcaec3f974bd4a4f0d8f3
HDVinnie <unit3d@protonmail.com>
2020-09-04 12:12:43 -0400

    update: increment unit3d version

--- config/unit3d.php
+++ config/unit3d.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'powered-by' => 'Powered By UNIT3D Community Edition v2.7.0',
+    'powered-by' => 'Powered By UNIT3D Community Edition v3.0.0',
 
     /*
     |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ return [
     |
     */
 
-    'version' => 'v2.7.0',
+    'version' => 'v3.0.0',
 
     /*
     |--------------------------------------------------------------------------
