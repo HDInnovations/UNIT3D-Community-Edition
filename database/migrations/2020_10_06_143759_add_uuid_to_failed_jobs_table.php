6ed7eac5692704796c29ec28da62fb5c77c1a9c1
HDVinnie <unit3d@protonmail.com>
2020-10-07 09:49:38 -0400

    fix: failed jobs migration

--- database/migrations/2020_10_06_143759_add_uuid_to_failed_jobs_table.php
+++ database/migrations/2020_10_06_143759_add_uuid_to_failed_jobs_table.php
@@ -14,7 +14,7 @@ class AddUuidToFailedJobsTable extends Migration
     public function up()
     {
         Schema::table('failed_jobs', function (Blueprint $table) {
-            //
+            $table->string('uuid')->after('id')->nullable()->unique();
         });
     }
 
