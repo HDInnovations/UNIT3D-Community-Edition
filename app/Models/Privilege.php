4d2d05baedf87f766b990bee590c13d306c3fca0
HDVinnie <hdinnovations@protonmail.com>
2020-10-31 01:08:08 -0400

    update: models

---
+++ app/Models/Privilege.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Privilege extends Model
+{
+    use HasFactory;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(RBACRoles::class, 'role_privileges', 'privilege_id', 'role_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_privileges', 'privilege_id', 'user_id');
+    }
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RBACRoles extends Model
+{
+    use HasFactory;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function privileges()
+    {
+        return $this->belongsToMany(Privilege::class, 'role_privilege', 'role_id', 'privilege_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_role', 'role_id', 'user_id');
+    }
+}
