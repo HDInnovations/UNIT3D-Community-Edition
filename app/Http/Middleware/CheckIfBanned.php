9bee593c21bf7a893abfe17a77bc30c7b915e6e5
HDVinnie <hdinnovations@protonmail.com>
2020-11-01 21:49:06 -0500

    update: banned middleware

--- app/Http/Middleware/CheckIfBanned.php
+++ app/Http/Middleware/CheckIfBanned.php
@@ -13,7 +13,7 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Group;
+use App\Models\Role;
 use Closure;
 
 class CheckIfBanned
@@ -32,9 +32,9 @@ class CheckIfBanned
     public function handle($request, Closure $next, $guard = null)
     {
         $user = $request->user();
-        $banned_group = \cache()->rememberForever('banned_group', fn () => Group::where('slug', '=', 'banned')->pluck('id'));
+        $banned_group = \cache()->rememberForever('banned_group', fn () => Role::where('slug', '=', 'banned')->pluck('id'));
 
-        if ($user && $user->group_id == $banned_group[0]) {
+        if ($user && $user->hasRole($banned_group[0])) {
             \auth()->logout();
             $request->session()->flush();
 
