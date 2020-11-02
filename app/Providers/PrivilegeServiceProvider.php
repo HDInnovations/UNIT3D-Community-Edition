10e4afbf9e745c7a5af900ef1e4747aa910a42b0
HDVinnie <hdinnovations@protonmail.com>
2020-11-01 23:46:21 -0500

    update: privilege service provider

--- app/Providers/PrivilegeServiceProvider.php
+++ app/Providers/PrivilegeServiceProvider.php
@@ -32,7 +32,7 @@ class PrivilegeServiceProvider extends ServiceProvider
         //
         // @role()
         Blade::directive('role', function ($role) {
-            return "<?php if(auth()->check() && auth()->user()->hasRole($role)) { ?>"; //return this if statement inside php tag
+            return "<?php if(auth()->check() && auth()->user()->hasRole([$role])) { ?>"; //return this if statement inside php tag
         });
         //@endrole
         Blade::directive('endrole', function ($role) {
