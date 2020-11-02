4a04088fb5337d144fe52b0593a81cb9589042de
HDVinnie <hdinnovations@protonmail.com>
2020-11-01 20:58:34 -0500

    update: users table
    - new migration to edit column name

---
+++ database/migrations/2020_11_02_015614_update_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('group_id', 'primary_role_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
