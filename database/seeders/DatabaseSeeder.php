6e35a3749c5f899626a78fbff166227bd8527b50
HDVinnie <hdinnovations@protonmail.com>
2020-11-01 20:48:41 -0500

    update: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RBACTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(BonExchangeTableSeeder::class);
         $this->call(AchievementDetailsTableSeeder::class);
