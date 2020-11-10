08a79655a8746002a66a2274ae3d3cb8cfd1fcd4
Obi-Wana <73784120+Obi-Wana@users.noreply.github.com>
2020-11-10 19:45:37 +0100

    Create PasswordSecurity.php

---
+++ app/PasswordSecurity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordSecurity extends Model
+{
+	protected $guarded = [];
+
+    	public function user()
+    	{
+        	return $this->belongsTo('App\User');
+    	}
+}
