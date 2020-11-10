ec77379f3866f7c584b2f9f3f58724e1888cd2dd
Obi-Wana <73784120+Obi-Wana@users.noreply.github.com>
2020-11-10 20:17:22 +0100

    fix styleCI

--- app/Http/Controllers/Auth/PasswordSecurityController.php
+++ app/Http/Controllers/Auth/PasswordSecurityController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\PasswordSecurity;
+use Illuminate\Http\Request;
 
 class PasswordSecurityController extends Controller
 {
-    public function show2faForm(Request $request){
+    public function show2faForm(Request $request)
+    {
         $user = \Auth::user();
 
         $google2fa_url = "";
@@ -21,46 +22,50 @@ class PasswordSecurityController extends Controller
                 $user->passwordSecurity->google2fa_secret
             );
         }
-        $data = array(
-            'user' => $user,
+        $data = [
+            'user' 	    => $user,
             'google2fa_url' => $google2fa_url
-        );
+        ];
         return view('auth.2fa', ['data' => $data, 'user' => $user]);
     }
 
-    public function generate2faSecret(Request $request){
+    public function generate2faSecret(Request $request)
+    {
     	$user = \Auth::user();
     	// Initialise the 2FA class
     	$google2fa = app('pragmarx.google2fa');
 
     	// Add the secret key to the registration data
     	PasswordSecurity::create([
-        	'user_id' => $user->id,
-        	'google2fa_enable' => 0,
-        	'google2fa_secret' => $google2fa->generateSecretKey(),
+            'user_id'          => $user->id,
+            'google2fa_enable' => 0,
+            'google2fa_secret' => $google2fa->generateSecretKey(),
     	]);
 
-    	return redirect('totp/2fa')->with('success',"Secret Key is generated, Please verify Code to Enable 2FA");
+        return redirect('totp/2fa')->with('success', 'Secret Key is generated, Please verify Code to Enable 2FA');
     }
 
-    public function enable2fa(Request $request){
+    public function enable2fa(Request $request)
+    {
         $user = \Auth::user();
         $google2fa = app('pragmarx.google2fa');
         $secret = $request->input('verify-code');
         $valid = $google2fa->verifyKey($user->passwordSecurity->google2fa_secret, $secret);
-        if($valid){
+        if ($valid) {
             $user->passwordSecurity->google2fa_enable = 1;
             $user->passwordSecurity->save();
-            return redirect('totp/2fa')->with('success',"2FA is Enabled Successfully.");
-        }else{
-            return redirect('totp/2fa')->with('error',"Invalid Verification Code, Please try again.");
+            return redirect('totp/2fa')->with('success', '2FA is Enabled Successfully.');
+        }
+	else {
+            return redirect('totp/2fa')->with('error', 'Invalid Verification Code, Please try again.');
         }
     }
     
-    public function disable2fa(Request $request){
-        if (!(\Hash::check($request->get('current-password'), \Auth::user()->password))) {
+    public function disable2fa(Request $request)
+    {
+        if (! (\Hash::check($request->get('current-password'), \Auth::user()->password))) {
             // The passwords matches
-            return redirect()->back()->with("error","Your  password does not matches with your account password. Please try again.");
+            return redirect()->back()->with('error', 'Your  password does not matches with your account password. Please try again.');
         }
 
         $validatedData = $request->validate([
@@ -69,10 +74,11 @@ class PasswordSecurityController extends Controller
         $user = \Auth::user();
         $user->passwordSecurity->google2fa_enable = 0;
         $user->passwordSecurity->save();
-        return redirect('totp/2fa')->with('success',"2FA is now Disabled.");
+        return redirect('totp/2fa')->with('success', '2FA is now Disabled.');
     }
 
-    public function faVerify(Request $request) {
+    public function faVerify(Request $request)
+    {
     	$this->middleware('2fa')->only('faVerify');
 	return redirect(URL()->previous());
     }
