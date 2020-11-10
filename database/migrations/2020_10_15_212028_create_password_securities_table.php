3a9f15248b48f0e5aade9f618664878ccaad87e5
Obi-Wana <73784120+Obi-Wana@users.noreply.github.com>
2020-11-10 19:50:50 +0100

    Create 2020_10_15_212028_create_password_securities_table.php

---
+++ database/migrations/2020_10_15_212028_create_password_securities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasswordSecuritiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password_securities', function (Blueprint $table) {
+		$table->increments('id');
+            	$table->integer('user_id');
+            	$table->boolean('google2fa_enable')->default(false);
+            	$table->string('google2fa_secret')->nullable();
+            	$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('password_securities');
+    }
+}
