1ff3857647daa8563b5c05463ae801f62b6473d3
Obi-Wana <73784120+Obi-Wana@users.noreply.github.com>
2020-11-10 20:43:20 +0100

    fix StyleCI

--- app/Http/Middleware/Google2FAMiddleware.php
+++ app/Http/Middleware/Google2FAMiddleware.php
@@ -12,6 +12,7 @@ class Google2FAMiddleware
      *
      * @param \Illuminate\Http\Request $request
      * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
