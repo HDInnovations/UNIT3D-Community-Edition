f370eac0fbde3079d6c89c47bff4da4d8df5db11
HDVinnie <HDVinnie@users.noreply.github.com>
2020-11-24 06:46:14 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/opcache.php
+++ config/opcache.php
@@ -12,10 +12,10 @@
  */
 
 return [
-    'url' => env('OPCACHE_URL', config('app.url')),
-    'prefix' => 'opcache-api',
-    'verify' => true,
-    'headers' => [],
+    'url'         => env('OPCACHE_URL', config('app.url')),
+    'prefix'      => 'opcache-api',
+    'verify'      => true,
+    'headers'     => [],
     'directories' => [
         base_path('app'),
         base_path('bootstrap'),
