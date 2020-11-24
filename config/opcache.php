5e105472a062bb888d71e6eb826a3e96f345b0ba
HDVinnie <hdinnovations@protonmail.com>
2020-11-24 01:46:03 -0500

    update: opcache package
    - removes deprecated appstract/lush-http

--- config/opcache.php
+++ config/opcache.php
@@ -12,18 +12,30 @@
  */
 
 return [
-    'url'         => env('OPCACHE_URL', config('app.url')),
-    'verify_ssl'  => true,
-    'headers'     => [],
+    'url' => env('OPCACHE_URL', config('app.url')),
+    'prefix' => 'opcache-api',
+    'verify' => true,
+    'headers' => [],
     'directories' => [
         base_path('app'),
         base_path('bootstrap'),
         base_path('public'),
-        base_path('resources/lang'),
+        base_path('resources'),
         base_path('routes'),
-        base_path('storage/framework/views'),
-        base_path('vendor/appstract'),
-        base_path('vendor/composer'),
-        base_path('vendor/laravel/framework'),
+        base_path('storage'),
+        base_path('vendor'),
+    ],
+    'exclude' => [
+        'test',
+        'Test',
+        'tests',
+        'Tests',
+        'stub',
+        'Stub',
+        'stubs',
+        'Stubs',
+        'dumper',
+        'Dumper',
+        'Autoload',
     ],
 ];
