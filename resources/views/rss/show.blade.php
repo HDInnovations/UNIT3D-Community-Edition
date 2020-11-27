0d62d3b42bc65b8b9f389fe910efdfbead51711b
HDVinnie <hdinnovations@protonmail.com>
2020-11-27 10:14:31 -0500

    update: rss view

--- resources/views/rss/show.blade.php
+++ resources/views/rss/show.blade.php
@@ -1,6 +1,3 @@
-@php
-    echo '<?xml version="1.0" encoding="UTF-8" ?>'
-@endphp
 <rss version="2.0"
      xmlns:atom="http://www.w3.org/2005/Atom"
      xmlns:content="http://purl.org/rss/1.0/modules/content/"
