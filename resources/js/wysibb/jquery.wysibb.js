5da2383ea3cef6e34c49c81480e1fb84a09a97ae
HDVinnie <hdinnovations@protonmail.com>
2020-12-06 01:28:58 -0500

    fix: jquery.wysibb.js

--- resources/js/wysibb/jquery.wysibb.js
+++ resources/js/wysibb/jquery.wysibb.js
@@ -893,7 +893,7 @@ wbbdebug = true;
                 let mheight = this.options.autoresize === true ? this.options.resize_maxheight : height;
                 this.$body = $(
                     this.strf(
-                        '<div class="wysibb-text-editor" style="max-height:{maxheight;}px;min-height:{height;}px;"></div>',
+                        '<div class="wysibb-text-editor" style="max-height:{maxheight}px;min-height:{height}px;"></div>',
                         { maxheight: mheight, height: height }
                     )
                 ).insertAfter(this.$txtArea);
