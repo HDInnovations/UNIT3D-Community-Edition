2fca96ea10d4d57a56ec7013155f0b8f9c909cc2
Globber <Globbered@users.noreply.github.com>
2020-12-23 13:31:46 +0100

    Update graveyard.php da language

--- resources/lang/da/graveyard.php
+++ resources/lang/da/graveyard.php
@@ -12,15 +12,21 @@
  */
 
 return [
-    'dead'               => 'Død',
+    'current-seedtime'   => 'Nuværende Seedtid',
+    'dead'               => 'Døde',
     'graveyard'          => 'Kirkegård',
     'guidelines'         => 'Retningslinier',
-    'guidelines-content' => '1) Du kan ikke genoplive dine egne uploads. <br> 2) Genoplive ikke noget, du ikke kan forpligte dig også.',
-    'howto'              => 'Heres Reglen',
-    'howto-desc1'        => 'Du skal frø <span class=\\"text-green\\"><em>: navn</em></span> i <span class="text-red text-bold">30 dage</span> for en vellykket ressurction. I hvilket tilfælde, når din nuværende frøtid på',
-    'howto-desc2'        => 'Du vil blive belønnet',
-    'howto-hits'         => 'Hits',
-    'pending'            => 'Verserende',
-    'resurrect'          => 'genoplive',
-    'reward'             => 'Freeleech-tokens',
+    'guidelines-content' => '1) Du kan ikke genoplive dine egne uploads.<br>2) Genopliv ikke noget, du ikke kan forpligtige dig til.',
+    'howto'              => 'Sådan gør du',
+    'howto-desc1'        => 'Du skal seede <span class="text-green"><em>:name</em></span> i <span class="text-red text-bold">30 dage</span> for at opnå en vellykket genoplivning. I hvilket tilfælde din nuværende seedtid på',
+    'howto-desc2'        => 'Du vil blive belønnet med',
+    'howto-hits'         => 'rammer',
+    'not-rewarded'       => 'Ikke belønnet',
+    'pending'            => 'Afventende',
+    'resurrect'          => 'Genopliv',
+    'resurrect-date'     => 'Genoplivelses-dato',
+    'reward'             => 'Freeleech tokens',
+    'rewarded'           => 'Belønnet',
+    'seedtime-goal'      => 'Seedtids-mål',
+
 ];
