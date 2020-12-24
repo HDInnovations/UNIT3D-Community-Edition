f5d2da9e92bfec4479f51e1c917829858c4b9aaf
HDVinnie <hdinnovations@protonmail.com>
2020-12-23 23:42:16 -0500

    fix: version controller

--- app/Http/Controllers/Staff/VersionController.php
+++ app/Http/Controllers/Staff/VersionController.php
@@ -21,7 +21,7 @@ use GuzzleHttp\Client;
  */
 class VersionController extends Controller
 {
-    private \App\Http\Controllers\Staff\VersionController $versionController;
+    private $versionController;
 
     public function __construct()
     {
