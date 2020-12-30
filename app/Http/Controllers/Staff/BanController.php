c25773a8543820d98beb83ecdfe285a1e8122e3e
HDVinnie <hdinnovations@protonmail.com>
2020-12-30 12:16:53 -0500

    refactor: sstaff ban controller

--- app/Http/Controllers/Staff/BanController.php
+++ app/Http/Controllers/Staff/BanController.php
@@ -54,11 +54,11 @@ class BanController extends Controller
     {
         $user = User::where('username', '=', $username)->firstOrFail();
         $staff = $request->user();
-        $banned_group = \cache()->rememberForever('banned_group', fn () => Group::where('slug', '=', 'banned')->pluck('id'));
+        $bannedGroup = \cache()->rememberForever('banned_group', fn () => Group::where('slug', '=', 'banned')->pluck('id'));
 
         \abort_if($user->group->is_modo || $request->user()->id == $user->id, 403);
 
-        $user->group_id = $banned_group[0];
+        $user->group_id = $bannedGroup[0];
         $user->can_upload = 0;
         $user->can_download = 0;
         $user->can_comment = 0;
