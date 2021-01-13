db6ff969511cfad6735da0088cad55ed846c1894
nikosuk <57614897+nikosuk@users.noreply.github.com>
2021-01-13 20:16:21 +0200

    Add files via upload

--- resources/lang/el/notification.php
+++ resources/lang/el/notification.php
@@ -12,14 +12,27 @@
  */
 
 return [
+	'bon-gifts'          => 'Δώρα Πονταλάκια',
+    'comment-tags'       => 'Σχόλια Tags',
     'date'             => 'Ημερομηνία',
     'delete'           => 'Διαγραφή',
     'delete-all'       => 'Διαγράψτε όλες τις ειδοποιήσεις',
+	'filter-by-type'     => 'Φιλτρο ανα τύπο ειδοποίησης',
     'mark-all-read'    => 'Σημειώστε όλα αυτά ως αναγνωσμένα',
     'mark-read'        => 'Σημείωσε ως διαβασμένο',
     'message'          => 'Μήνυμα',
     'no-notifications' => 'Δεν υπάρχουν ανακοινώσεις',
     'notifications'    => 'Ειδοποιήσεις',
+	'post-tags'          => 'Post Tags',
+    'post-tips'          => 'Post Tips',
     'read'             => 'Ανάγνωση',
+	'request-approvals'  => 'Request Approvals',
+    'request-bounties'   => 'Request Bounties',
+    'request-claims'     => 'Request Claim',
+    'request-fills'      => 'Request Fills',
+    'request-rejections' => 'Request Rejections',
+    'request-unclaims'   => 'Request Unclaims',
+    'reseed-requests'    => 'Reseed Request',
     'title'            => 'Τίτλος',
+	'unfollows'          => 'Ξε- ακολούθησε',
 ];
