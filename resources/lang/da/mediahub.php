8d44f21c7bd420955186de72769338b3005d8f36
Globber <Globbered@users.noreply.github.com>
2021-01-13 15:20:23 +0100

    Create mediahub.php for Danish translation

---
+++ resources/lang/da/mediahub.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Howard Lau <howardlau1999@hotmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'title'                 => 'MedieHub',
+    'shows'                 => 'TV Shows',
+    'movies'                => 'Film',
+    'collections'           => 'Samlinger',
+    'persons'               => 'Personer',
+    'genres'                => 'Genre',
+    'networks'              => 'Netværk',
+    'companies'             => 'Firmaer',
+    'terms'                 => 'Dette produkt bruger TMDb API, men er ikke godkendt eller certificeret af TMDb.',
+    'select-hub'            => 'Vælg venligst en Hub',
+];
