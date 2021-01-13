6d9024a38f096979177d195f314e0fd4fc9eb2f4
Globber <Globbered@users.noreply.github.com>
2021-01-13 17:41:15 +0100

    Update thank-button.blade.php
    Add translation ref

--- resources/views/livewire/thank-button.blade.php
+++ resources/views/livewire/thank-button.blade.php
@@ -1,6 +1,6 @@
 <div style="display: inline;">
 	<button wire:click="thank({{ $torrent->id }})" class="btn btn-sm btn-primary">
 		<i class="{{ config('other.font-awesome') }} fa-heart text-pink"></i> @lang('torrent.thank') @lang('torrent.uploader')
-		({{ $torrent->thanks()->count() }} Given)
+		({{ $torrent->thanks()->count() }} @lang('common.given'))
 	</button>
 </div>
