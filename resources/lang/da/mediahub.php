daa5542d696a650b0e273c457906aa96e4cca4e0
Globber <Globbered@users.noreply.github.com>
2021-01-14 15:37:21 +0100

    Update mediahub.php
    Add language strings

--- resources/lang/da/mediahub.php
+++ resources/lang/da/mediahub.php
@@ -12,17 +12,26 @@
  */
 
 return [
+    'born'                  => 'Født:',
     'collections'           => 'Samlinger',
     'companies'             => 'Firmaer',
     'disclaimer'            => 'Dette produkt bruger TMDb API, men er ikke godkendt eller certificeret af TMDb.',
     'episodes'              => 'Episoder',
+    'first-seen'            => 'Først set på:',
     'genres'                => 'Genre',
+    'latest-project'        => 'Seneste projekt:',
     'networks'              => 'Netværk',
+    'no-data'               => 'Ingen data fundet!',   
     'movies'                => 'Film',
+    'movie-credits'         => 'Film-kredits:',
     'persons'               => 'Personer',
+    'plot'                  => 'Handling:',
+    'release-date'          => 'Frigivelsesdato:',
     'seasons'               => 'Sæsoner',
     'select-hub'            => 'Vælg venligst en Hub',
     'show'                  => 'TV Show',
     'shows'                 => 'TV Shows',
     'title'                 => 'MedieHub',
+    'tv-credits'            => 'TV-kredits:',
+    'wiki-read'             => 'Læs hele bio på Wikipedia:',
 ];
