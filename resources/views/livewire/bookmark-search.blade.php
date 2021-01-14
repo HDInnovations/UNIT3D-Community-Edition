3474cdcbc1e292ea692ecfeb4bcfecc00c916533
Globber <Globbered@users.noreply.github.com>
2021-01-14 15:41:21 +0100

    Update bookmark-search.blade.php
    Add language ref

--- resources/views/livewire/bookmark-search.blade.php
+++ resources/views/livewire/bookmark-search.blade.php
@@ -10,7 +10,7 @@
 		</div>
 
 		<div class="form-group">
-			<input type="text" wire:model="searchTerm" class="form-control" style="width: 275px;" placeholder="Search By Torrent Name"/>
+			<input type="text" wire:model="searchTerm" class="form-control" style="width: 275px;" placeholder="@lang('torrent.search-by-name')"/>
 		</div>
 	</div>
 	<div class="box-body no-padding">
