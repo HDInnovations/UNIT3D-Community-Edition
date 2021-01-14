9b470cba88262ee50677608586fa62a6724616b0
Globber <Globbered@users.noreply.github.com>
2021-01-14 15:37:03 +0100

    Update mediahub.php
    Add language strings

--- resources/lang/en/mediahub.php
+++ resources/lang/en/mediahub.php
@@ -12,17 +12,26 @@
  */
 
 return [
+    'born'                  => 'Born:',
     'collections'           => 'Collections',
     'companies'             => 'Companies',
     'disclaimer'            => 'This product uses the TMDb API but is not endorsed or certified by TMDb.',
     'episodes'              => 'Episodes',
+    'first-seen'            => 'First Appeared:',
     'genres'                => 'Genres',
+    'latest-project'        => 'Latest Project:',
     'networks'              => 'Netvorks',
+    'no-data'               => 'No Data Found!', 
     'movies'                => 'Movies',
+    'movie-credits'         => 'Movie Credits:',
     'persons'               => 'Persons',
+    'plot'                  => 'Plot:',
+    'release-date'          => 'Release date:',
     'seasons'               => 'Seasons',
     'select-hub'            => 'Please Select A Hub',
     'show'                  => 'TV Show',
     'shows'                 => 'TV Shows',
     'title'                 => 'MediaHub',
+    'tv-credits'            => 'TV Credits:',
+    'wiki-read'             => 'Read Full Bio On Wikipedia:',
 ];
