7afd998968f1c5e0a6a3ac7235792d864bb87b72
HDVinnie <HDVinnie@users.noreply.github.com>
2021-01-27 00:00:26 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/igdb.php
+++ config/igdb.php
@@ -16,7 +16,7 @@ return [
      * These are the credentials you got from https://dev.twitch.tv/console/apps
      */
     'credentials' => [
-        'client_id' => env('TWITCH_CLIENT_ID', ''),
+        'client_id'     => env('TWITCH_CLIENT_ID', ''),
         'client_secret' => env('TWITCH_CLIENT_SECRET', ''),
     ],
 
