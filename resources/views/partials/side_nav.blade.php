3da997828875706da231be11e402ce8298a9f13f
Neko Chan <80861218+LewdNeko-chan@users.noreply.github.com>
2021-03-18 00:40:26 +0100

    An Option to cleanup the Menu a bit
    Because mobile devices cannot access some menu points, because its out of view.

--- resources/views/partials/side_nav.blade.php
+++ resources/views/partials/side_nav.blade.php
@@ -1,6 +1,9 @@
 <aside id="hoe-left-panel" hoe-position-type="fixed">
     <ul class="nav panel-list">
+
+
         <li class="nav-level">@lang('common.navigation')</li>
+
         <li>
             <a href="{{ route('home.index') }}">
                 <i class="{{ config('other.font-awesome') }} fa-home" style=" font-size: 18px; color: #ffffff;"></i>
@@ -8,19 +11,55 @@
                 <span class="selected"></span>
             </a>
         </li>
-        <li>
-            @if (auth()->user()->torrent_layout == 1)
-                <a href="{{ route('groupings') }}">
-            @elseif (auth()->user()->torrent_layout == 2)
-                <a href="{{ route('cards') }}">
-            @else
-                <a href="{{ route('torrents') }}">
-            @endif
-                <i class="{{ config('other.font-awesome') }} fa-tv-retro" style=" font-size: 18px; color: #ffffff;"></i>
+
+
+        <li class="hoe-has-menu">
+            <a href="javascript:void(0)">
+                <i class="{{ config('other.font-awesome') }} fa-download" style=" font-size: 18px; color: #ffffff;"></i>
                 <span class="menu-text">@lang('torrent.torrents')</span>
                 <span class="selected"></span>
             </a>
+            <ul class="hoe-sub-menu">
+                <li>
+                    @if (auth()->user()->torrent_layout == 1)
+                        <a href="{{ route('groupings') }}">
+                    @elseif (auth()->user()->torrent_layout == 2)
+                        <a href="{{ route('cards') }}">
+                    @else
+                        <a href="{{ route('torrents') }}">
+                    @endif
+                        <span class="menu-text">@lang('torrent.torrents')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('requests') }}">
+                        <span class="menu-text">@lang('request.requests')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('mediahub.index') }}">
+                        <span class="menu-text">MediaHub</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('subtitles.index') }}">
+                        <span class="menu-text">@lang('common.subtitles')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('graveyard.index') }}">
+                        <span class="menu-text">@lang('graveyard.graveyard')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+            </ul>
         </li>
+
+
         <li class="hoe-has-menu">
             <a href="javascript:void(0)">
                 <i class="{{ config('other.font-awesome') }} fa-upload" style=" font-size: 18px; color: #ffffff;"></i>
@@ -39,81 +78,64 @@
                 @endforeach
             </ul>
         </li>
-        <li>
-            <a href="{{ route('mediahub.index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-database"
-                   style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">MediaHub</span>
+
+
+        <li class="hoe-has-menu">
+            <a href="javascript:void(0)">
+                <i class="{{ config('other.font-awesome') }} fa-user" style=" font-size: 18px; color: #ffffff;"></i>
+                <span class="menu-text">@lang('common.other')</span>
                 <span class="selected"></span>
             </a>
+            <ul class="hoe-sub-menu">
+
+                <li>
+                    <a href="{{ route('playlists.index') }}">
+                        <span class="menu-text">Playlists</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('albums.index') }}">
+                        <span class="menu-text">@lang('common.gallery')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('stats') }}">
+                        <span class="menu-text">@lang('common.extra-stats')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('polls') }}">
+                        <span class="menu-text">@lang('poll.polls')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('bonus_store') }}">
+                        <span class="menu-text">@lang('bon.bon') @lang('bon.store')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('forums.index') }}">
+                        <span class="menu-text">@lang('forum.forums')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('rss.index') }}">
+                        <span class="menu-text">@lang('rss.rss')</span>
+                        <span class="selected"></span>
+                    </a>
+                </li>
+
+            </ul>
         </li>
-        <li>
-            <a href="{{ route('requests') }}">
-                <i class="{{ config('other.font-awesome') }} fa-hands-helping"
-                    style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('request.requests')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('playlists.index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-list-ol" style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">Playlists</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('subtitles.index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-closed-captioning" style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('common.subtitles')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('graveyard.index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-skull" style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('graveyard.graveyard')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('albums.index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-images" style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('common.gallery')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('stats') }}">
-                <i class="{{ config('other.font-awesome') }} fa-chart-bar"
-                    style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('common.extra-stats')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('polls') }}">
-                <i class="{{ config('other.font-awesome') }} fa-chart-pie"
-                    style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('poll.polls')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('bonus_store') }}">
-                <i class="{{ config('other.font-awesome') }} fa-shopping-cart"
-                    style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('bon.bon') @lang('bon.store')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('forums.index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-comments" style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('forum.forums')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
+
+
         <li>
             <a href="{{ config('other.rules_url') }}">
                 <i class="{{ config('other.font-awesome') }} fa-info-square"
@@ -127,13 +149,6 @@
                 <i class="{{ config('other.font-awesome') }} fa-question-square"
                     style=" font-size: 18px; color: #ffffff;"></i>
                 <span class="menu-text">@lang('common.faq')</span>
-                <span class="selected"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('rss.index') }}">
-                <i class="{{ config('other.font-awesome') }} fa-rss" style=" font-size: 18px; color: #ffffff;"></i>
-                <span class="menu-text">@lang('rss.rss')</span>
                 <span class="selected"></span>
             </a>
         </li>
