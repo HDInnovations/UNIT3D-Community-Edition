ef9e55bb521347c982264a535614e01ba2a80fc1
Neko Chan <80861218+LewdNeko-chan@users.noreply.github.com>
2021-03-20 00:46:42 +0100

    added regex support

--- config/client-blacklist.php
+++ config/client-blacklist.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'enabled' => false,
+    'enabled' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -31,9 +31,32 @@ return [
     | An array of clients to be blacklisted which will reject them from announcing
     | to the sites tracker.
     |
-    |
+    | clients -> Please use Regex. this is to blacklist multiple clients in a fast way.
+	| For example, to blacklist only the client "bittorrent" add this following: '/^bittorrent$/'
+	| this means, exactly match bittorrent first of all, / at the beginning and /[modifyer, normaly i for case unsensitive]
+	| then after this, in that example here, the ^ means that this is the first position of the string, so nothing before
+	| the letter "b...". the $ at the end of the string means, that nothing shoud follow in this string. so that the string must end
+	| on "...t". for example, we want to blacklist all instances of "uglyclient/123.413.221" we must add
+	| we can do this by adding following rule: "/^(uglyclient)\/[0-9\.]+$/i"
+	| the "\" before the "/" and "." is needed for escaping. so, that means "uglyclient/6564.342.2346.2234.222.52" will also be blocked.
+	| Best way to learn regex is, to go on https://regex101.com/
+	|
     */
+	// Regex: "/^(µ?u?x?(web)?Torrent)(Mac)?( ?\/? ?[0-9a-zA-Z\(\) \/\:\.]+)?/i"
     'clients' => [
-        'Transmission/2.93', 'Transmission/2.04',
+        "/^(u?x?(web)?Torrent)(Mac)?( ?\/? ?[0-9a-zA-Z\(\) \/\:\.]+)?/i",
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blacklist Clients
+    |--------------------------------------------------------------------------
+	|
+	| Don't forget to add the readable on this array for display on the page.
+	|
+	*/
+	'clientsreadable' => [
+		'uTorrent* (Also Mac)',
+		'webTorrent*'
+	]
 ];
