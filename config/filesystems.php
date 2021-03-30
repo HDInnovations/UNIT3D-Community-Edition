e629186570cc66eb861d13995f3d2d0f2575190b
HDVinnie <hdinnovations@protonmail.com>
2021-03-30 09:03:43 -0400

    add: ftp/sftp filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -65,6 +65,36 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'ftp' => [
+            'driver' => 'ftp',
+            'host' => 'ftp.example.com',
+            'username' => 'your-username',
+            'password' => 'your-password',
+
+            // Optional FTP Settings...
+            // 'port' => 21,
+            // 'root' => '',
+            // 'passive' => true,
+            // 'ssl' => true,
+            // 'timeout' => 30,
+        ],
+
+        'sftp' => [
+            'driver' => 'sftp',
+            'host' => 'example.com',
+            'username' => 'your-username',
+            'password' => 'your-password',
+
+            // Settings for SSH key based authentication...
+            'privateKey' => '/path/to/privateKey',
+            'password' => 'encryption-password',
+
+            // Optional SFTP Settings...
+            // 'port' => 22,
+            // 'root' => '',
+            // 'timeout' => 30,
+        ],
+
         'backups' => [
             'driver' => 'local',
             'root'   => storage_path('backups'),
