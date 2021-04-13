6630a27d758c31f0aab4e9e229a2cd9a9989177c
Globber <Globbered@users.noreply.github.com>
2021-04-13 13:29:05 +0200

    Create ticket.php
    Create a new en ticket.php for ticket language strings

---
+++ resources/lang/en/ticket.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'body'           => 'Body',
+    'body-enter'     => 'Enter body here...',
+    'category'       => 'Category',
+    'create-ticket'  => 'Create Ticket',
+    'fix-errors'     => 'Please fix the following error(s) and try again:',
+    'helpdesk'       => 'Helpdesk',
+    'priority'       => 'Priority',
+    'subject'        => 'Subject',
+    'subject-enter'  => 'Enter subject here...',
+    'submit-ticket'  => 'Submit Ticket',
+    'reset'          => 'Reset',
+];
