8e3b2d38894be2da4c8b29a615799382b58dee97
clandestine8 <clandestine8@protonmail.com>
2021-04-17 21:08:33 -0400

    Add: User Privilege Restrictions

---
+++ database/migrations/2021_04_17_223947_user_restricted_privileges.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserRestrictedPrivileges extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('user_restricted_privilege', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->foreignId('privilege_id');
+
+            //FOREIGN KEY CONSTRAINTS
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('privilege_id')->references('id')->on('privileges');
+
+            //SETTING THE PRIMARY KEYS
+            $table->primary(['user_id', 'privilege_id']);
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
