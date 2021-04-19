bfd17f709f14ac9d8d0d6cf8fc0f478dfec318f8
Globber <Globbered@users.noreply.github.com>
2021-04-19 15:11:36 +0200

    Create controllers.php
    Add new translation strings for controller translation

---
+++ resources/lang/da/controllers.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'account-updated'                 => 'Din konto blev opdateret med succes!',
+    'api-change'                      => 'Dit API-token blev ændret med succes!',
+    'avatar-large'                    => 'Din avatar er for stor, maks. Filstørrelse: ',
+    'email-updated'                   => 'Din e-mail blev opdateret med succes!',
+    'external-css'                    => 'URLen til det eksterne CSS-typografiark er ugyldig, prøv det igen med en gyldig URL.',
+    'gif-square'                      => 'Fordi du uploader en GIF, skal din avatar være firkantet!',
+    'gone-hidden'                     => 'Du er forsvundet som en ninja!',
+    'gone-private'                    => 'Du er gået Privat!',
+    'gone-public'                     => 'Du er gået Offentligt!',
+    'gone-visible'                    => 'Du har opgivet dine ninja-måder og blevet synlig!',
+    'notifications-disabled'          => 'Du har deaktiveret Notifikationer!',
+    'notifications-enabled'           => 'Du har deaktiveret Notifikationer!',
+    'password-incorrect'              => 'Dit adgangskode var forkert!',
+    'password-reset'                  => 'Din adgangskode er nulstillet!',
+    'password-weak'                   => 'Din nye adgangskode er for svag!',
+    'pid-change'                      => 'Dit PID blev ændret med succes!',
+    'profile-updated'                 => 'Din profil blev opdateret med succes!',
+    'rid-change'                      => 'Dit RID blev ændret med succes!',
+    'settings-account'                => 'Dine konto notifikationsindstillinger er gemt!',
+    'settings-bon'                    => 'Dine BON notifikationsindstillinger er gemt!',
+    'settings-achievement-privacy'    => 'Dine præstations privatlivsindstillinger er blevet gemt!',
+    'settings-followed'               => 'Dine følger notifikationsindstillinger er gemt!',
+    'settings-forum'                  => 'Dine forum  notifikationsindstillinger er gemt!',
+    'settings-follower-privacy'       => 'Dine følger privatlivsindstillinger er blevet gemt!',
+    'settings-forumhistory-privacy'   => 'Din forumhistoriks privatlivsindstillinger er blevet gemt!',
+    'settings-mention'                => 'Dine @nævn notifikationsindstillinger er gemt!',
+    'settings-other-privacy'          => 'Dine andre privatlivsindstillinger er blevet gemt!',
+    'settings-profile-privacy'        => 'Dine profil privatlivsindstillinger er blevet gemt!',
+    'settings-request'                => 'Dine request notifikationsindstillinger er gemt!',
+    'settings-request-privacy'        => 'Dine request privatlivsindstillinger er blevet gemt!',
+    'settings-subscription'           => 'Dine abonnement  notifikationsindstillinger er gemt!',
+    'settings-torrent'                => 'Dine torrent notifikationsindstillinger er gemt!',
+    'settings-torrenthistory-privacy' => 'Din torrenthistoriks privatlivsindstillinger er blevet gemt!',
+    'twostep-auth'                    => 'Du ændrede din TwoStep Auth-status!',
+    'went-wrong'                      => 'Noget gik galt!',
+  
+    'warning-deactivated'             => 'Advarslen blev deaktiveret',
+    'warning-deactivated-all'         => 'Alle advarsler blev deaktiveret med succes',
+    'warning-deleted'                 => 'Advarslen blev slettet',
+    'warning-deleted-all'             => 'Alle advarsler blev slettet',
+    'warning-restored'                => 'Advarslen blev gendannet',
+   
+    'tmdb-bad-request'                => 'TMDM dårlig anmodning!',
+    'tmdb-required'                   => 'TMDB ID kræves',
+    'wish-exists'                     => 'Ønsket eksisterer allerede!',
+    'wish-added'                      => 'Ønsket tilføjet med succes!',
+    'wish-removed'                    => 'Ønsket blev fjernet!',
+
+];
