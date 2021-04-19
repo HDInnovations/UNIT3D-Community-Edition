559d3ae5ac5c6f59baadc562b5664a5f8ec3c579
Globber <Globbered@users.noreply.github.com>
2021-04-19 14:43:09 +0200

    Create controllers.php
    New string listing to aid translation of controllers

---
+++ resources/lang/en/controllers.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'account-updated'                 => 'Your Account Was Updated Successfully!',
+    'api-change'                      => 'Your API Token Was Changed Successfully!',
+    'avatar-large'                    => 'Your avatar is too large, max file size: ',
+    'email-updated'                   => 'Your Email Was Updated Successfully!',
+    'external-css'                    => 'The URL for the external CSS stylesheet is invalid, try it again with a valid URL.',
+    'gif-square'                      => 'Because you are uploading a GIF, your avatar must be square!',
+    'gone-hidden'                     => 'You Have Disappeared Like A Ninja!',
+    'gone-private'                    => 'You Have Gone Private!',
+    'gone-public'                     => 'You Have Gone Public!',
+    'gone-visible'                    => 'You Have Given Up Your Ninja Ways And Become Visible!',
+    'notifications-disabled'          => 'You Have Disabled Notifications!',
+    'notifications-enabled'           => 'You Have Enabled Notifications!',
+    'password-incorrect'              => 'Your Password Was Incorrect!',
+    'password-reset'                  => 'Your Password Has Been Reset',
+    'password-weak'                   => 'Your New Password Is To Weak!rrrrr',
+    'pid-change'                      => 'Your PID Was Changed Successfully!',
+    'profile-updated'                 => 'Your Profile Was Updated Successfully!',
+    'rid-change'                      => 'Your RID Was Changed Successfully!',
+    'settings-account'                => 'Your Account Notification Settings Have Been Saved!',
+    'settings-bon'                    => 'Your BON Notification Settings Have Been Saved!',
+    'settings-achievement-privacy'    => 'Your Achievement Privacy Settings Have Been Saved!',
+    'settings-followed'               => 'Your Followed User Notification Settings Have Been Saved!',
+    'settings-forum'                  => 'Your Forum Notification Settings Have Been Saved!',
+    'settings-follower-privacy'       => 'Your Follower Privacy Settings Have Been Saved!',
+    'settings-forumhistory-privacy'   => 'Your Forum History Privacy Settings Have Been Saved!',
+    'settings-mention'                => 'Your @Mention Notification Settings Have Been Saved!',
+    'settings-other-privacy'          => 'Your Other Privacy Settings Have Been Saved!',
+    'settings-profile-privacy'        => 'Your Profile Privacy Settings Have Been Saved!',
+    'settings-request'                => 'Your Request Notification Settings Have Been Saved!',
+    'settings-request-privacy'        => 'Your Request Privacy Settings Have Been Saved!',
+    'settings-subscription'           => 'Your Subscription Notification Settings Have Been Saved!',
+    'settings-torrent'                => 'Your Torrent Notification Settings Have Been Saved!',
+    'settings-torrenthistory-privacy' => 'Your Torrent History Privacy Settings Have Been Saved!',
+    'twostep-auth'                    => 'You Changed Your TwoStep Auth Status!',
+    'went-wrong'                      => 'Something Went Wrong!',
+
+    'warning-deactivated'             => 'Warning Was Successfully Deactivated',
+    'warning-deactivated-all'         => 'All Warnings Were Successfully Deactivated',
+    'warning-deleted'                 => 'Warning Was Successfully Deleted',
+    'warning-deleted-all'             => 'All Warnings Were Successfully Deleted',
+    'warning-restored'                => 'Warning Was Successfully Restored',
+   
+    'tmdb-bad-request'                => 'TMDM Bad Request!',
+    'tmdb-required'                   => 'TMDB ID Required',
+    'wish-exists'                     => 'Wish already exists!',
+    'wish-added'                      => 'Wish Successfully Added!',
+    'wish-removed'                    => 'Wish Successfully Removed!',
+
+];
