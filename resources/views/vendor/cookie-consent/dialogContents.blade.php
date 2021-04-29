e5b4002a8680142d79e6a1e11c107cd3f24fe9ab
HDVinnie <hdinnovations@protonmail.com>
2021-04-29 19:52:24 -0400

    update: cookie consent

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,10 +1,10 @@
-<div class="alert alert-danger alert-dismissable" id="alert_system_notice">
+<div class="alert alert-danger alert-dismissable js-cookie-consent cookie-consent">
     <div class="text-center">
     <span class="cookie-consent__message">
         {!! trans('cookie-consent::texts.message') !!}
     </span>
 
-        <button class="btn btn-sm btn-primary">
+        <button class="btn btn-sm btn-primary js-cookie-consent-agree cookie-consent__agree">
             @lang('cookie-consent::texts.agree')
         </button>
     </div>
