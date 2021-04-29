5e4f5db157531015321fe32ad4e64ee584d47a4e
HDVinnie <HDVinnie@users.noreply.github.com>
2021-04-29 01:33:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -13,18 +13,19 @@
 
 namespace App\Http\Middleware;
 
+use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
-use App\Providers\RouteServiceProvider;
 
 class RedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  ...$guards
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @param string|null              ...$guards
+     *
      * @return mixed
      */
     public function handle(Request $request, Closure $next, ...$guards)
