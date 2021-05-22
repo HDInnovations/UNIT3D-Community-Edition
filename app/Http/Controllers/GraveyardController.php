c2799b02c424deca30f48f060e15068890e83397
HDVinnie <hdinnovations@protonmail.com>
2021-05-22 12:20:28 -0400

    refactor: graveyard to livewire

--- app/Http/Controllers/GraveyardController.php
+++ app/Http/Controllers/GraveyardController.php
@@ -33,98 +33,11 @@ class GraveyardController extends Controller
     }
 
     /**
-     * Show The Graveyard.
+     * Display a listing of the resource.
      */
-    public function index(Request $request): \Illuminate\Contracts\View\Factory | \Illuminate\View\View
+    public function index(): \Illuminate\Contracts\View\Factory | \Illuminate\View\View
     {
-        $current = Carbon::now();
-        $user = $request->user();
-        $torrents = Torrent::with('category', 'type')->where('created_at', '<', $current->copy()->subDays(30)->toDateTimeString())->paginate(25);
-        $repository = $this->torrentFacetedRepository;
-        $deadcount = Torrent::where('seeders', '=', 0)->where('created_at', '<', $current->copy()->subDays(30)->toDateTimeString())->count();
-
-        return \view('graveyard.index', [
-            'user'       => $user,
-            'torrents'   => $torrents,
-            'repository' => $repository,
-            'deadcount'  => $deadcount,
-        ]);
-    }
-
-    /**
-     * Uses Input's To Put Together A Search.
-     *
-     *
-     * @throws \Throwable
-     *
-     * @return array
-     */
-    public function faceted(Request $request, Torrent $torrent)
-    {
-        $current = Carbon::now();
-        $user = $request->user();
-        $search = $request->input('search');
-        $imdbId = Str::startsWith($request->get('imdb'), 'tt') ? $request->get('imdb') : 'tt'.$request->get('imdb');
-        $imdb = \str_replace('tt', '', $imdbId);
-        $tvdb = $request->input('tvdb');
-        $tmdb = $request->input('tmdb');
-        $mal = $request->input('mal');
-        $categories = $request->input('categories');
-        $types = $request->input('types');
-
-        $terms = \explode(' ', $search);
-        $search = '';
-        foreach ($terms as $term) {
-            $search .= '%'.$term.'%';
-        }
-
-        $torrent = $torrent->with('category', 'type')->where('seeders', '=', 0)->where('created_at', '<', $current->copy()->subDays(30)->toDateTimeString());
-
-        if ($request->has('search') && $request->input('search') != null) {
-            $torrent->where('name', 'like', $search);
-        }
-
-        if ($request->has('imdb') && $request->input('imdb') != null) {
-            $torrent->where('imdb', '=', \str_replace('tt', '', $imdb));
-        }
-
-        if ($request->has('tvdb') && $request->input('tvdb') != null) {
-            $torrent->where('tvdb', '=', $tvdb);
-        }
-
-        if ($request->has('tmdb') && $request->input('tmdb') != null) {
-            $torrent->where('tmdb', '=', $tmdb);
-        }
-
-        if ($request->has('mal') && $request->input('mal') != null) {
-            $torrent->where('mal', '=', $mal);
-        }
-
-        if ($request->has('categories') && $request->input('categories') != null) {
-            $torrent->whereIn('category_id', $categories);
-        }
-
-        if ($request->has('types') && $request->input('types') != null) {
-            $torrent->whereIn('type_id', $types);
-        }
-
-        if ($request->has('sorting') && $request->input('sorting') != null) {
-            $sorting = $request->input('sorting');
-            $order = $request->input('direction');
-            $torrent->orderBy($sorting, $order);
-        }
-
-        if ($request->has('qty')) {
-            $qty = $request->get('qty');
-            $torrents = $torrent->paginate($qty);
-        } else {
-            $torrents = $torrent->paginate(25);
-        }
-
-        return \view('graveyard.results', [
-            'user'     => $user,
-            'torrents' => $torrents,
-        ])->render();
+        return \view('graveyard.index');
     }
 
     /**
