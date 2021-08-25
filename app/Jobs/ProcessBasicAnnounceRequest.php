bc1f3d4176177e53fae6c814c417b303027a6b3d
Alkl58 <alexandrekaul@protonmail.com>
2021-08-25 12:11:31 +0200

    Config: Disable / Enable Client Connectable Check Fix Eloquent updating "updated_at" in Connectable Check

--- app/Jobs/ProcessBasicAnnounceRequest.php
+++ app/Jobs/ProcessBasicAnnounceRequest.php
@@ -125,7 +125,7 @@ class ProcessBasicAnnounceRequest implements ShouldQueue
         $peer->left = $this->queries['left'];
         $peer->torrent_id = $this->torrent->id;
         $peer->user_id = $this->user->id;
-        //$peer->updateConnectableStateIfNeeded();
+        $peer->updateConnectableStateIfNeeded();
         $peer->save();
         // End Peer Update
 
