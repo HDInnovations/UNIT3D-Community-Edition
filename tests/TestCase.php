514bc33ff7aa9fb9cdd016d80fbd10632c5d6b89
HDVinnie <hdinnovations@protonmail.com>
2021-10-20 14:13:00 -0400

    update: tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -26,6 +26,10 @@ abstract class TestCase extends BaseTestCase
         $this->artisan('route:clear');
 
         $this->artisan('cache:clear');
+
+        if (!defined('LARAVEL_START')) {
+            define('LARAVEL_START', microtime(true));
+        }
     }
 
     /**
