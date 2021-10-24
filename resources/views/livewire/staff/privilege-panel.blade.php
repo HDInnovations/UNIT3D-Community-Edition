8750ed53f76ea3ac47c2abf0289d17f36175734b
clandestine8 <clandestine8@protonmail.com>
2021-10-24 00:40:18 -0400

    update: Privilege and User Views

--- resources/views/livewire/staff/privilege-panel.blade.php
+++ resources/views/livewire/staff/privilege-panel.blade.php
@@ -2,9 +2,9 @@
      x-data="{ tab: 1, panel: 0, query: '', roleSlug: '',  activeUser: @entangle('activeUser') }">
     <div class="ppNav">
             <div :class="{ 'active': tab === 1 }" @click.prevent="tab = 1; panel = 0;"><i
-                        class="fas fa-user-tag"></i> Roles</div>
+                        class="fas fa-users-class"></i> Roles</div>
             <div :class="{ 'active': tab === 2 }"@click.prevent="tab = 2; panel = 0;"><i
-                            class="fas fa-lock"></i> Privileges</div>
+                            class="fas fa-key"></i> Privileges</div>
             <div :class="{ 'active': tab === 3 }" @click.prevent="tab = 3; panel = 0;"><i
                             class="fas fa-user-lock"></i> Users</div>
     </div>
@@ -37,86 +37,73 @@
             </div>
         </section>
         <section x-show="tab === 2 && panel == 0">
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>Privilege <small style="font-family: monospace; font-size: 10px;">[slug]</small></th>
-                    <th>Description</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
+            <div class="ppTable">
+                <div class="ppTInfo">
+                    <div class="ppTTitle"><strong>Configure Privileges</strong></div>
+                    <div class="ppTBack"><a href="{{route('staff.dashboard.index')}}"><i class="fas fa-arrow-circle-left"></i> Back to Dashboard</a></div>
+                </div>
+                <div class="ppTHead">
+                    <div class="ppTHeadings">Privilege <small style="font-family: monospace; font-size: 9px;">[slug]</small></div>
+                    <div class="ppTHeadings">Description</div>
+                    <div class="ppTHeadings">Actions</div>
+                </div>
+                <div class="ppTBody">
                 @foreach($privileges as $privilege)
-                    <tr>
-                        <td>{{$privilege->name}} <small
-                                    style="font-family: monospace; font-size: 10px;">[{{$privilege->slug}}]</small></td>
-                        <td>{{$privilege->description}}</td>
-                        <td><a class="btn btn-success" href="#">Edit Settings</a> <a class="btn btn-primary" href="#">Audit
-                                Usage</a></td>
-
-                    </tr>
+                        <div class="ppPrivilege">{{$privilege->name}} <small
+                                    style="font-family: monospace; font-size: 10px;">[{{$privilege->slug}}]</small></div>
+                        <div class="ppDescription">{{$privilege->description}}</div>
+                        <div class="ppActions">
+                            <a href="#">Edit Settings</a>
+                            <a href="#">Audit Usage</a>
+                        </div>
                 @endforeach
-                </tbody>
-            </table>
-
-        </section>
-        <section x-show="tab === 3 && panel == 0">
-
-            <div class="row">
-                <div class="col-xs-12">
-                    <div class="form-group">
-                        <input type="text" wire:model="userSearch" class="form-control float-right"
-                               style="width: 275px;"
-                               placeholder="@lang('user.search')"/>
-                    </div>
                 </div>
             </div>
+        </section>
+        <section x-show="tab === 3 && panel == 0">
+            <div class="ppTable">
+                <div class="ppTInfo">
+                    <div class="ppTTitle"><strong>Configure Users</strong></div>
+                    <div class="ppTInput">
+                        <input type="text" wire:model="userSearch" style="width: 200px;" placeholder="@lang('user.search')"/>
+                    </div>
+                    <div class="ppTBack"><a href="{{route('staff.dashboard.index')}}"><i class="fas fa-arrow-circle-left"></i> Back to Dashboard</a></div>
+                </div>
+                <div class="ppTHeadUsers">
+                    <div class="ppTHeadings" sortable wire:click="sortBy('username')"
+                        :direction="$wire.sortField === 'username' ? $wire.sortDirection : null" role="button">
+                        @lang('common.username')
+                        @include('livewire.includes._sort-icon', ['field' => 'username'])
+                    </div>
+                    <div class="ppTHeadings" sortable wire:click="sortBy('role_id')"
+                         :direction="$wire.sortField === 'role_id' ? $wire.sortDirection : null" role="button">
+                        Role
+                        @include('livewire.includes._sort-icon', ['field' => 'role_id'])</div>
+                    <div class="ppTHeadings" sortable wire:click="sortBy('email')"
+                         :direction="$wire.sortField === 'email' ? $wire.sortDirection : null"
+                         role="button">
+                        @lang('common.email')
+                        @include('livewire.includes._sort-icon', ['field' => 'email'])</div>
+                    <div class="ppTHeadings" sortable wire:click="sortBy('created_at')"
+                         :direction="$wire.sortField === 'created_at' ? $wire.sortDirection : null"
+                         role="button">
+                        @lang('user.registration-date')
+                        @include('livewire.includes._sort-icon', ['field' => 'created_at'])
+                    </div>
+                    <div class="ppTHeadings">
+                        Actions
+                    </div>
+                </div>
+                <div class="ppTBodyUsers">
 
-
-            <table class="table vertical-align table-hover">
-                <tbody>
-                <tr>
-
-                    <th>
-                        <div sortable wire:click="sortBy('username')"
-                             :direction="$wire.sortField === 'username' ? $wire.sortDirection : null" role="button">
-                            @lang('common.username')
-                            @include('livewire.includes._sort-icon', ['field' => 'username'])
-                        </div>
-                    </th>
-                    <th>
-                        <div sortable wire:click="sortBy('role_id')"
-                             :direction="$wire.sortField === 'role_id' ? $wire.sortDirection : null" role="button">
-                            Role
-                            @include('livewire.includes._sort-icon', ['field' => 'role_id'])
-                        </div>
-                    </th>
-                    <th class="hidden-sm hidden-xs">
-                        <div sortable wire:click="sortBy('email')"
-                             :direction="$wire.sortField === 'email' ? $wire.sortDirection : null"
-                             role="button">
-                            @lang('common.email')
-                            @include('livewire.includes._sort-icon', ['field' => 'email'])
-                        </div>
-                    </th>
-                    <th class="hidden-sm hidden-xs">
-                        <div sortable wire:click="sortBy('created_at')"
-                             :direction="$wire.sortField === 'created_at' ? $wire.sortDirection : null"
-                             role="button">
-                            @lang('user.registration-date')
-                            @include('livewire.includes._sort-icon', ['field' => 'created_at'])
-                        </div>
-                    </th>
-                    <th>@lang('common.action')</th>
-                </tr>
                 @foreach ($users as $user)
-                    <tr>
-                        <td>
+
+                        <div class="ppValue">
 						<span class="badge-user text-bold">
                             {{ $user->username }}
                         </span>
-                        </td>
-                        <td>
+                        </div>
+                        <div class="ppValue">
                             @foreach($user->roles as $role)
                                 @if($role->slug === $user->primaryRole->slug)
                                     <span class="badge text-bold"
@@ -126,20 +113,18 @@
                                     <span class="badge">{{$role->name}}</span>
                                 @endif
                             @endforeach
-                        </td>
-                        <td class="hidden-sm hidden-xs">{{ $user->email }}</td>
-                        <td class="hidden-sm hidden-xs">{{ \Illuminate\Support\Carbon::make($user->created_at)->toFormattedDateString() }}</td>
-                        <td>
-                            <a class="btn btn-success" href="#"
-                               @click.prevent="$wire.GetUser({{$user->id}}).then( ()=>{ tab = 3; panel = 2; } );">Edit
-                                Privileges & Restrictions</a>
-                            <a class="btn btn-primary" href="#">Change Primary Role</a>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+                        </div>
+                        <div class="ppValue">{{ $user->email }}</div>
+                        <div class="ppValue">{{ \Illuminate\Support\Carbon::make($user->created_at)->toFormattedDateString() }}</div>
+                        <div class="ppActions">
+                            <a href="#"
+                               @click.prevent="$wire.GetUser({{$user->id}}).then( ()=>{ tab = 3; panel = 2; } );"><i class="fas fa-key"></i> Privilges</a>
+                            <a href="#"><i class="fas fa-users-class"></i> Roles</a>
+                        </div>
 
+                @endforeach
+                </div>
+            </div>
         </section>
         <section x-show="tab === 1 && panel == 1">
             <div class="ppTable">
