1a59c710d3c1fa89b8ce51ceb49f11e81764077d
Roardom <78790963+Roardom@users.noreply.github.com>
2021-11-11 08:44:32 +0000

    update: peerlist generation

--- app/Http/Controllers/AnnounceController.php
+++ app/Http/Controllers/AnnounceController.php
@@ -451,7 +451,12 @@ class AnnounceController extends Controller
             $limit = ($queries['numwant'] <= 50 ? $queries['numwant'] : 50);
 
             // Get Torrents Peers
-            $peers = Peer::where('torrent_id', '=', $torrent->id)->where('user_id', '!=', $user->id)->take($limit)->get()->toArray();
+            if ($queries['left'] == 0) {
+                // Only include leechers in a seeder's peerlist
+                $peers = Peer::where('torrent_id', '=', $torrent->id)->where('seeder', '=', 0)->where('user_id', '!=', $user->id)->take($limit)->get()->toArray();
+            } else {
+                $peers = Peer::where('torrent_id', '=', $torrent->id)->where('user_id', '!=', $user->id)->take($limit)->get()->toArray();
+            }
 
             $repDict['peers'] = $this->givePeers($peers, $queries['compact'], $queries['no_peer_id']);
             $repDict['peers6'] = $this->givePeers($peers, $queries['compact'], $queries['no_peer_id'], FILTER_FLAG_IPV6);
