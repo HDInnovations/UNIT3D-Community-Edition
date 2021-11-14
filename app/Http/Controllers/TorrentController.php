4dc46444949c88cdaca34b64bdfd0e005d4f0e4c
HDVinnie <hdinnovations@protonmail.com>
2021-11-14 14:56:11 -0500

    fix: #2002
    - closes #2002 due to no trailer available.

--- app/Http/Controllers/TorrentController.php
+++ app/Http/Controllers/TorrentController.php
@@ -202,7 +202,11 @@ class TorrentController extends Controller
                 'platforms', ])
                 ->find($torrent->igdb);
             $link = collect($meta->videos)->take(1)->pluck('video_id');
-            $trailer = 'https://www.youtube.com/embed/'.$link[0];
+            if (isset($link[0])) {
+                $trailer = 'https://www.youtube.com/embed/'.isset($link[0]);
+            } else {
+                $trailer = '/img/no-video.png';
+            }
             $platforms = PlatformLogo::whereIn('id', collect($meta->platforms)->pluck('platform_logo')->toArray())->get();
         }
 
