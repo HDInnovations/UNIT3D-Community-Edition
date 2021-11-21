1c9dfdee96b29c2e78c85dd5dab904d3070e4375
HDVinnie <HDVinnie@users.noreply.github.com>
2021-11-21 23:15:37 +0000

    Apply fixes from StyleCI

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,7 +28,7 @@ $.ajaxSetup({
 window.axios = require('axios');
 window.axios.defaults.headers.common = {
     'X-Requested-With': 'XMLHttpRequest',
-    'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
 };
 
 /**
