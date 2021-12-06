a644a544c6af7c802fb8a43f625ebeec070ece77
Jay Sizzla <aithercc_heaven@protonmail.com>
2021-12-06 21:32:00 +0100

    fix StyleCI

--- database/migrations/2021_11_22_115517_add_more_torrent_promos.php
+++ database/migrations/2021_11_22_115517_add_more_torrent_promos.php
@@ -39,7 +39,7 @@ class AddMoreTorrentPromos extends Migration
         // Change all "free->100" torrents to "free->1" for now FL discounts
         $fl_torrents = DB::table('torrents')->select('id', 'free')->where('free', '>', 1)->get();
         $i = 0;
-        foreach ($fl_torrents as $torrent){
+        foreach ($fl_torrents as $torrent) {
             DB::table('torrents')
                 ->where('id', $torrent->id)
                 ->update([
