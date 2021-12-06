f22be050a74dde8011e3ded9b47adae70b2f53ae
alkl58 <alkl58@protonmail.com>
2021-12-06 16:16:37 +0100

    CommentController: German & French Translation

---
+++ resources/lang/fr/comment.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'added'                        => 'Ton commentaire a été ajouté!',
+    'deleted'                      => 'Le commentaire a été supprimé.',
+    'edited'                       => 'Le commentaire a été modifié.',
+    'rights-revoked'               => 'Ton droit de commenter a été révoqué!',
+    'slow-down'                    => 'Ralentissez - Trop de commentaires!',
+];
