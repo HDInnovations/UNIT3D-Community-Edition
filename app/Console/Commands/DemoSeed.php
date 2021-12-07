d6fee137be9b005990fbd232c0126e3f50395512
HDVinnie <hdinnovations@protonmail.com>
2021-12-07 16:40:14 -0500

    update: demo seed command

--- app/Console/Commands/DemoSeed.php
+++ app/Console/Commands/DemoSeed.php
@@ -290,19 +290,25 @@ Menu
                         $year = (int) \substr($tv['first_air_date'], 0, 4);
                     }
 
+                    $freeleech = ['0', '25', '50', '75', '100'];
+                    $selected = \random_int(0, \count($freeleech) - 1);
+
                     Torrent::factory()->create([
-                        'user_id'       => $uid,
-                        'tmdb'          => $id,
-                        'name'          => $tv['name'].' ('.$year.')',
-                        'slug'          => Str::slug($tv['name']),
-                        'description'   => $tv['overview'],
-                        'category_id'   => 2,
-                        'type_id'       => \rand(1, 6),
-                        'resolution_id' => \rand(1, 10),
-                        'featured'      => false,
-                        'sticky'        => 0,
-                        'release_year'  => $year,
-                        'mediainfo'     => '
+                        'user_id'        => $uid,
+                        'tmdb'           => $id,
+                        'name'           => $tv['name'].' ('.$year.')',
+                        'slug'           => Str::slug($tv['name']),
+                        'description'    => $tv['overview'],
+                        'category_id'    => 2,
+                        'type_id'        => \rand(1, 6),
+                        'resolution_id'  => \rand(1, 10),
+                        'region_id'      => \rand(1, 242),
+                        'distributor_id' => \rand(1, 965),
+                        'free'           => $freeleech[$selected],
+                        'featured'       => false,
+                        'sticky'         => 0,
+                        'release_year'   => $year,
+                        'mediainfo'      => '
 Complete name                            : Double.Impact.1991.1080p.BluRay.DD+5.1.x264-LoRD.mkv
 Format                                   : Matroska
 Format version                           : Version 4
