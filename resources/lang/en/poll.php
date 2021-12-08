a2da1e3480927461e122c1c5270b0e0e6cb80cd4
alkl58 <alkl58@protonmail.com>
2021-12-08 20:57:23 +0100

    PollController: German & French Translation

--- resources/lang/en/poll.php
+++ resources/lang/en/poll.php
@@ -13,6 +13,8 @@
 
 return [
     'add-option'           => 'Add Option',
+    'already-voted-error'  => 'Bro have already vote on this poll. Your vote has not been counted.',
+    'already-voted-result' => 'You have already vote on this poll. Here are the results.',
     'create-poll'          => 'Create Poll',
     'current'              => 'Current Poll(s)',
     'delete-option'        => 'Delete Option',
@@ -25,6 +27,7 @@ return [
     'title'                => 'Title',
     'total'                => 'Total Votes Ever',
     'vote'                 => 'Vote',
+    'vote-counted'         => 'Your vote has been counted.',
     'vote-now'             => 'Get Your Vote In Now!',
     'votes'                => 'Votes',
 ];
