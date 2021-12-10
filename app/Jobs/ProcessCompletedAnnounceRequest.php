34f1a18d54f49e2a10d065fc65d3452fafdda0d1
HDVinnie <hdinnovations@protonmail.com>
2021-12-10 00:28:17 -0500

    fix: typos in announce jobs

--- app/Jobs/ProcessCompletedAnnounceRequest.php
+++ app/Jobs/ProcessCompletedAnnounceRequest.php
@@ -104,7 +104,7 @@ class ProcessCompletedAnnounceRequest implements ShouldQueue
 
         if (\config('other.freeleech') == 1 || $personalFreeleech || $this->user->group->is_freeleech == 1 || $freeleechToken) {
             $modDownloaded = 0;
-        } elseif ($this->torrent->free => 1) {
+        } elseif ($this->torrent->free >= 1) {
             // FL value in DB are from 0% to 100%.
             // Divide it by 100 and multiply it with "downloaded" to get discount download.
             $fl_discount = $downloaded * $this->torrent->free / 100;
