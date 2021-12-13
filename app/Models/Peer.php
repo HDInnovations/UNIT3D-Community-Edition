57148f03b7f7058dbd0d458789c3828465f1c739
alkl58 <alkl58@protonmail.com>
2021-12-13 21:31:49 +0100

    Apply fixes from StyleCI

--- app/Models/Peer.php
+++ app/Models/Peer.php
@@ -15,7 +15,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use Kyslik\ColumnSortable\Sortable;
 
 /**
