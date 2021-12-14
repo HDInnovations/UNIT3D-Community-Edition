a2041b57183fc42bb7e89086f654cb4fbd532612
alkl58 <alkl58@protonmail.com>
2021-12-14 20:42:28 +0100

    TicketController: German & French Translation

---
+++ resources/lang/fr/ticket.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'assign'             => 'Assigner',
+    'assigned-staff'     => 'Personnel assigné',
+    'assigned-success'   => 'Le ticket du Helpdesk a été assigné avec succès!',
+    'attachments'        => 'Pièces jointes',
+    'attachments-save'   => 'Sauvegarder les pièces jointes',
+    'body'               => 'Sujet',
+    'body-enter'         => 'Entrez le sujet ici...',
+    'category'           => 'Catégorie',
+    'close'              => 'Fermer',
+    'closed'             => 'Fermé',
+    'closed-success'     => 'Le ticket du Helpdesk a été fermé avec succès!',
+    'create-ticket'      => 'Créer un ticket',
+    'created'            => 'Créé',
+    'created-success'    => 'Ton ticket du Helpdesk a été créé avec succès!',
+    'delete'             => 'Supprimer',
+    'deleted-success'    => 'Ton ticket du Helpdesk a été supprimé avec succès!',
+    'description'        => 'Description',
+    'download'           => 'Télécharger',
+    'fix-errors'         => 'Veuillez corriger les erreurs suivantes et réessayer:',
+    'helpdesk'           => 'Helpdesk',
+    'no-attach'          => 'Aucune pièce jointe trouvée',
+    'priority'           => 'Priorité',
+    'opened-by'          => 'Ouvert par:',
+    'reset'              => 'Réinitialiser',
+    'subject'            => 'Sujet',
+    'subject-enter'      => 'Entrez le sujet ici...',
+    'submit-ticket'      => 'Soumettre le ticket',
+    'ticket'             => 'Ticket',
+    'unassign'           => 'Désassigner',
+    'unassigned-success' => 'Le ticket du Helpdesk a été désassigné avec succès!',
+    'updated-success'    => 'Ton ticket Helpdesk a été actualisé avec succès!',
+];
