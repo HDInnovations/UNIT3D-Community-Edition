d00be4ba4de823c2601e897b1542dd95869cf868
HDVinnie <hdinnovations@protonmail.com>
2021-12-23 13:18:52 -0500

    add: base top 10 torrents feature

---
+++ app/Http/Controllers/Top10Controller.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+namespace App\Http\Controllers;
+
+class Top10Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
+        return \view('top10.index');
+    }
+}
