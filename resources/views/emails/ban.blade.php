73c3244c9687c68d177056cb7362325c457705c8
HDVinnie <hdinnovations@protonmail.com>
2022-01-10 18:43:32 -0500

    remove: old files

--- resources/views/emails/ban.blade.php
+++
@@ -1,5 +0,0 @@
-@component('mail::message')
-    # @lang('email.banned-header')!
-    **Reason:** {{ $ban->ban_reason }}
-    *@lang('email.banned-footer')*
-@endcomponent
--- resources/views/emails/unban.blade.php
+++
@@ -1,5 +0,0 @@
-@component('mail::message')
-    # @lang('email.unban-header')!
-    **Reason:** {{ $ban->unban_reason }}
-    *@lang('email.unban-footer')*
-@endcomponent
