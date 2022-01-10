5fdff946955db446825f5bdf1ec854ea02fe0bc0
HDVinnie <hdinnovations@protonmail.com>
2022-01-10 05:54:00 -0500

    add: new mail and site notifications

---
+++ app/Notifications/UserBan.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserBan extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $discordUrl = 'https://discord.gg/kUtC3WynUU';
+
+        return (new MailMessage)
+            ->greeting('You have been banned 😭')
+            ->line('You have been banned from '.config('other.title').' for '.$this->ban->ban_reason)
+            ->action('Need Support?', $discordUrl)
+            ->line('Thank you for using 🚀'.config('other.title'));
+    }
+}
---
+++ app/Notifications/UserMaxWarningsReached.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserMaxWarningsReached extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $profileUrl = \href_profile($this->user);
+
+        return (new MailMessage)
+            ->greeting('Max Hit and Run Warnings Reached!')
+            ->line('You have hit the limit on active Hit and Run Warnings! Your download privilliges have been revoked!')
+            ->action('View Unsatfied Torrents to seed off your warnings or wait until they expire!', $profileUrl)
+            ->line('Thank you for using 🚀'.config('other.title'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'title' => 'Max Hit and Run Warnings Reached!',
+            'body'  => 'You have hit the limit on active Hit and Run Warnings! Your download privilliges have been revoked!',
+            'url'   => \sprintf('/users/%s', $this->user->username),
+        ];
+    }
+}
