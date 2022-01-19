d36e318d233c040378e552c907283ae7df7d7788
Shift <shift@laravelshift.com>
2022-01-19 19:51:20 +0000

    Convert `optional()` to nullsafe operator

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -56,6 +56,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting(): void
     {
-        RateLimiter::for('api', fn (Request $request) => Limit::perMinute(60)->by(\optional($request->user())->id ?: $request->ip()));
+        RateLimiter::for('api', fn (Request $request) => Limit::perMinute(60)->by(\$request->user()?->id ?: $request->ip()));
     }
 }
