5e214ebbe5f0a26f17ff83254b0e28618de3cd22
HDVinnie <hdinnovations@protonmail.com>
2022-01-24 23:51:28 -0500

    update: drop foreign first

--- database/migrations/2022_01_23_232931_update_comments_table.php
+++ database/migrations/2022_01_23_232931_update_comments_table.php
@@ -15,13 +15,13 @@ return new class() extends Migration {
         Schema::table('comments', function (Blueprint $table) {
             DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
+            $table->dropForeign('article_id');
+
             $table->dropIndex('fk_comments_torrents_1');
             $table->dropIndex('fk_comments_articles_1');
             $table->dropIndex('comments_playlist_id_index');
             $table->dropIndex('comments_collection_id_index');
             $table->dropIndex('comments_ticket_id_index');
-
-            $table->dropForeign('article_id');
 
             $table->bigIncrements('id')->change();
             $table->foreignId('parent_id')->after('user_id')->nullable()->constrained('comments')->onDelete('cascade');
