2e7f7b5e912f526c41d91379b6632ce32dfc5269
HDVinnie <hdinnovations@protonmail.com>
2022-02-13 21:01:33 -0500

    update: user groups enums
    - add license

--- app/Enums/UserGroups.php
+++ app/Enums/UserGroups.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @credits    LokiThor2021 <https://github.com/LokiThor2021>
+ */
 
 declare(strict_types=1);
 
