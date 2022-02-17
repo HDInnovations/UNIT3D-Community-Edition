c5258a73f0b0cef9567cbe3aab9a8e87a3a792a8
HDVinnie <hdinnovations@protonmail.com>
2022-02-17 01:00:18 -0500

    update: configs

--- config/other.php
+++ config/other.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'codebase' => 'UNIT3D Community Edition (Nex-Gen Torrent Tracker) v6.0.0',
+    'codebase' => 'UNIT3D Community Edition (Nex-Gen Torrent Tracker) v6.0.1',
 
     /*
     |--------------------------------------------------------------------------
