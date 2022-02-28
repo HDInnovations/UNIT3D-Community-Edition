59cb757daf27d43b25b6d0751d7c72ab210232e9
HDVinnie <hdinnovations@protonmail.com>
2022-02-28 04:57:56 -0500

    update: commands

--- app/Console/Commands/SyncTorrentSeasonEpisode.php
+++ app/Console/Commands/SyncTorrentSeasonEpisode.php
@@ -43,17 +43,17 @@ class SyncTorrentSeasonEpisode extends Command
                 continue;
             }
 
-            if (preg_match('~\.{0,1}S(?<season>\d+)\.{0,1}( ){0,1}E(?<episode>\d+)~', $torrent->name, $match)) {
+            if (preg_match('~\.{0,1}S(?<season>\d+)\.{0,1}( ){0,1}E(?<episode>\d+)~', (string) $torrent->name, $match)) {
                 // Match SxxExx, Sxx.Exx, Sxx Exx (Single Episodes)
                 $torrent->season_number = (int) $match['season'];
                 $torrent->episode_number = (int) $match['episode'];
                 $torrent->save();
-            } elseif (preg_match('~\.{0,1}S(?<season>\d+)\.{0,1}( )~', $torrent->name, $match)) {
+            } elseif (preg_match('~\.{0,1}S(?<season>\d+)\.{0,1}( )~', (string) $torrent->name, $match)) {
                 // Match Sxx (Complete Seasons)
                 $torrent->season_number = (int) $match['season'];
                 $torrent->episode_number = 0;
                 $torrent->save();
-            } elseif (preg_match('~\.{0,1}( ){0,1}E(?<episode>\d+)~', $torrent->name, $match)) {
+            } elseif (preg_match('~\.{0,1}( ){0,1}E(?<episode>\d+)~', (string) $torrent->name, $match)) {
                 // Match Exx (Single Episode without Season Number)
                 $torrent->season_number = 1;
                 $torrent->episode_number = (int) $match['episode'];
