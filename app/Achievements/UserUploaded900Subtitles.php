bda4c721ba5f13fbc89f49035a8b473371039e2b
HDVinnie <hdinnovations@protonmail.com>
2022-02-28 04:52:22 -0500

    fix: achievements doc blocks

--- app/Achievements/UserUploaded900Subtitles.php
+++ app/Achievements/UserUploaded900Subtitles.php
@@ -17,7 +17,7 @@ class UserUploaded900Subtitles extends Achievement
     public $description = 'You have made 900 subtitle uploads!';
 
     /*
-    * The amount of "points" this user need to obtain in order to complete this achievement
-    */
+     * The amount of "points" this user need to obtain in order to complete this achievement
+     */
     public $points = 900;
 }
