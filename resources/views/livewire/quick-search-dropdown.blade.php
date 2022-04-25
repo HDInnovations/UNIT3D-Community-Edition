a50f1c64802e1ce40fa8c32ad21d3231df1be1bd
Roardom <roardom@protonmail.com>
2022-04-25 04:15:25 -0500

    refactor: quick search - unify, keyboard navigation

--- resources/views/livewire/quick-search-dropdown.blade.php
+++ resources/views/livewire/quick-search-dropdown.blade.php
@@ -1,106 +1,187 @@
-<div style="width: 460px; min-height: 50px; display: block;" x-data="{ lock: false }">
-    <div class="text-center form-inline"
-         @focusout="if (!lock) { $wire.set('movie', ''); $wire.set('series', ''); $wire.set('person', ''); }">
-        <div class="form-group" style="position: relative; vertical-align: top;">
-            <input wire:model.debounce.250ms="movie" type="text" class="form-control" placeholder="Movie"
-                   autocomplete="off" style="width: 150px;"
-                   @focusin="$wire.set('series', ''); $wire.set('person', '');">
+<div
+    class="quick-search"
+    x-data="{ search: 'movie', ...quickSearchKeyboardNavigation() }"
+    x-on:keydown.escape.window="$refs.movieSearch.blur(); $refs.seriesSearch.blur(); $refs.personSearch.blur()"
+>
+    <div class="quick-search__inputs">
+        <div class="quick-search__radios">
+            <label class="quick-search__radio-label">
+                <input
+                    type="radio"
+                    class="quick-search__radio"
+                    x-on:click="
+                        search = 'movie';
+                        $wire.set('series', '');
+                        $wire.set('person', '');
+                        $nextTick(() => $refs.movieSearch.focus());
+                    "
+                />
+                <i
+                    class="quick-search__radio-icon {{ \config('other.font-awesome') }} fa-camera-movie"
+                    title="{{ __('mediahub.movies') }}"
+                ></i>
+            </label>
+            <label class="quick-search__radio-label">
+                <input
+                    type="radio"
+                    class="quick-search__radio"
+                    x-on:click="
+                        search = 'series';
+                        $wire.set('movie', '');
+                        $wire.set('person', '');
+                        $nextTick(() => $refs.seriesSearch.focus());
+                    "
+                />
+                <i
+                    class="quick-search__radio-icon {{ \config('other.font-awesome') }} fa-tv-retro"
+                    title="{{ __('mediahub.shows') }}"
+                ></i>
+            </label>
+            <label class="quick-search__radio-label">
+                <input
+                    type="radio"
+                    class="quick-search__radio"
+                    x-on:click="
+                        search = 'person';
+                        $wire.set('movie', '');
+                        $wire.set('series', '');
+                        $nextTick(() => $refs.personSearch.focus());
+                    "
+                />
+                <i
+                    class="quick-search__radio-icon {{ \config('other.font-awesome') }} fa-user"
+                    title="{{ __('mediahub.persons') }}"
+                ></i>
+            </label>
         </div>
-        <div class="form-group" style="position: relative; vertical-align: top;">
-            <input wire:model.debounce.250ms="series" type="text" class="form-control" placeholder="Series"
-                   autocomplete="off" style="width: 150px;" @focusin="$wire.set('movie', ''); $wire.set('person', '');">
-        </div>
-        <div class="form-group" style="position: relative; vertical-align: top;">
-            <input wire:model.debounce.250ms="person" type="text" class="form-control" placeholder="Person"
-                   autocomplete="off" style="width: 150px;" @focusin="$wire.set('movie', ''); $wire.set('series', '');">
-        </div>
-    </div>
-    @if( strlen($movie) > 2  || strlen($series) > 2  || strlen($person) > 2)
-        <div style="width: 100%; min-height: 60px; margin: 0; padding: 3px; display: block; background-color: #2b2b2b;"
-             @mouseenter="lock = true;" @mouseleave="lock = false">
-            @forelse ($search_results as $search_result)
-                @if (strlen($movie) > 2 )
-                    <div id="movie.{{$search_result->id}}" class="row" style="cursor: pointer; margin-bottom: 5px;"
-                         @click="window.location.href = '{{ route('torrents.similar', ['category_id' => '1', 'tmdb' => $search_result->id]) }}'"
-                         @contextmenu.prevent="window.open('{{ route('torrents.similar', ['category_id' => '1', 'tmdb' => $search_result->id]) }}', '_blank').focus()"
-
-
+        <input
+            class="quick-search__input"
+            wire:model.debounce.250ms="movie"
+            type="text"
+            placeholder="Movie"
+            x-show="search == 'movie'"
+            x-ref="movieSearch"
+            x-on:keydown.down.prevent="$refs.searchResults.firstElementChild?.firstElementChild?.focus()"
+            x-on:keydown.up.prevent="$refs.searchResults.lastElementChild?.firstElementChild?.focus()"
+        />
+        <input
+            wire:model.debounce.250ms="series"
+            class="quick-search__input"
+            type="text"
+            placeholder="Series"
+            x-show="search == 'series'"
+            x-ref="seriesSearch"
+            x-cloak
+            x-on:keydown.down.prevent="$refs.searchResults.firstElementChild?.firstElementChild?.focus()"
+            x-on:keydown.up.prevent="$refs.searchResults.lastElementChild?.firstElementChild?.focus()"
+        />
+        <input
+            wire:model.debounce.250ms="person"
+            class="quick-search__input"
+            type="text"
+            placeholder="Person"
+            x-show="search == 'person'"
+            x-ref="personSearch"
+            x-cloak
+            x-on:keydown.down.prevent="$refs.searchResults.firstElementChild?.lastElementChild?.focus()"
+            x-on:keydown.up.prevent="$refs.searchResults.lastElementChild?.firstElementChild?.focus()"
+        />
+        @if (strlen($movie) >= 3  || strlen($series) >= 3  || strlen($person) >= 3)
+            <div class="quick-search__results" x-ref="searchResults">
+                @forelse ($search_results as $search_result)
+                    <article
+                        class="quick-search__result"
+                        x-on:keydown.down.prevent="quickSearchArrowDown($el)"
+                        x-on:keydown.up.prevent="quickSearchArrowUp($el)"
                     >
-                        <div class="col-xs-3 text-center">
-                            <img src="{{ $search_result->poster }}" style="height: 60px; padding: 0; margin: 0">
-                        </div>
-                        <div class="col-xs-9 text-left">
-                            <div style="height: 60px;">
-                                <p style="line-height: initial; height: 60px; display: table-cell; vertical-align: middle;">
-                                    {{ $search_result->title }} ({{ substr($search_result->release_date, 0, 4) }})
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                @elseif (strlen($series) > 2 )
-                    <div id="series.{{$search_result->id}}" class="row" style="cursor: pointer; margin-bottom: 5px;"
-                         @click="window.location.href = '{{ route('torrents.similar', ['category_id' => '2', 'tmdb' => $search_result->id]) }}'"
-                         @contextmenu.prevent="window.open('{{ route('torrents.similar', ['category_id' => '2', 'tmdb' => $search_result->id]) }}', '_blank').focus()"
-                    >
-                        <div class="col-xs-3 text-center">
-                            <img src="{{ $search_result->poster }}" style="height: 60px; padding: 0; margin: 0">
-                        </div>
-                        <div class="col-xs-9 text-left">
-                            <div style="height: 60px;">
-                                <p style="line-height: initial; height: 60px; display: table-cell; vertical-align: middle;">{{ $search_result->name }}
-                                    ({{ substr($search_result->first_air_date, 0, 4) }})
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                @elseif (strlen($person) > 2 )
-                    <div id="person.{{$search_result->id}}" class="row" style="cursor: pointer; margin-bottom: 5px;"
-                         @click="window.location.href = '{{ route('mediahub.persons.show', ['id' => $search_result->id]) }}'"
-                         @contextmenu.prevent="window.open('{{ route('mediahub.persons.show', ['id' => $search_result->id]) }}', '_blank').focus()"
-                    >
-                        <div class="col-xs-3 text-center">
-                            <img src="{{ $search_result->still }}" style="height: 60px; padding: 0; margin: 0">
-                        </div>
-                        <div class="col-xs-9 text-left">
-                            <div style="height: 60px;">
-                                <p style="line-height: initial; height: 60px; display: table-cell; vertical-align: middle;">
+                        @if (strlen($movie) >= 3 )
+                            <a
+                                class="quick-search__result-link"
+                                href="{{ route('torrents.similar', ['category_id' => '1', 'tmdb' => $search_result->id]) }}"
+                            >
+                                <img
+                                    class="quick-search__image"
+                                    src="{{ $search_result->poster }}"
+                                    alt="{{ __('torrent.poster') }}"
+                                />
+                                <h2 class="quick-search__result-text">
+                                    {{ $search_result->title }}
+                                    <time
+                                        class="quick-search__result-year"
+                                        datetime="{{ $search_result->release_date }}"
+                                    >
+                                        {{ substr($search_result->release_date, 0, 4) }}
+                                    </time>
+                                </h2>
+                            </a>
+                        @elseif (strlen($series) >= 3 )
+                            <a
+                                class="quick-search__result-link"
+                                href="{{ route('torrents.similar', ['category_id' => '2', 'tmdb' => $search_result->id]) }}"
+                            >
+                                <img
+                                    class="quick-search__image"
+                                    src="{{ $search_result->poster }}"
+                                    alt="{{ __('torrent.poster') }}"
+                                />
+                                <h2 class="quick-search__result-text">
                                     {{ $search_result->name }}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                @endif
-            @empty
-                <div class="px-3 py-3">No results found</div>
-            @endforelse
-        </div>
-    @elseif( $movie || $series || $person)
-        <div style="width: 100%; min-height: 60px; margin: 0; padding: 3px; display: block; background-color: #2b2b2b;"
-             @mouseenter="lock = true;" @mouseleave="lock = false">
-            <div class="px-3 my-3">Keep typing to get results</div>
-        </div>
-    @endif
+                                    <time
+                                        class="quick-search__result-year"
+                                        datetime="{{ $search_result->first_air_date }}"
+                                    >
+                                        {{ substr($search_result->first_air_date, 0, 4) }}
+                                    </time>
+                                </h2>
+                            </a>
+                        @elseif (strlen($person) >= 3 )
+                            <a
+                                class="quick-search__result-link"
+                                href="{{ route('mediahub.persons.show', ['id' => $search_result->id]) }}"
+                            >
+                                <img
+                                    class="quick-search__image"
+                                    src="{{ $search_result->still }}"
+                                    alt="{{ __('torrent.poster') }}"
+                                />
+                                <h2 class="quick-search__result-text">
+                                    {{ $search_result->name }}
+                                </h2>
+                            </a>
+                        @endif
+                    </article>
+                @empty
+                    <article class="quick-search__result--empty">
+                        <p class="quick-search__result-text">No results found</p>
+                    </article>
+                @endforelse
+            </div>
+        @else
+            <div class="quick-search__results">
+                <article class="quick-search__result--keep-typing">
+                    <p class="quick-search__result-text">Keep typing to get results</p>
+                </article>
+            </div>
+        @endif
+    </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<script nonce="{{ HDVinnie\SecureHeaders\SecureHeaders::nonce('script') }}">
+    function quickSearchKeyboardNavigation() {
+        return {
+            quickSearchArrowDown(el) {
+                if (el.nextElementSibling == null) {
+                    el.parentNode?.firstElementChild?.firstElementChild?.focus()
+                } else {
+                    el.nextElementSibling?.firstElementChild?.focus()
+                }
+            },
+            quickSearchArrowUp(el) {
+                if (el.previousElementSibling == null) {
+                    document.querySelector(`.quick-search__input:not([style='display: none;'])`)?.focus()
+                } else {
+                    el.previousElementSibling?.firstElementChild?.focus()
+                }        
+            }
+        }
+    }
+</script>
