644b74c22a2c649836f6332b6764b1b80958ee10
HDVinnie <hdinnovations@protonmail.com>
2022-05-01 12:18:00 -0400

    update: unit3d config

--- config/unit3d.php
+++ config/unit3d.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'powered-by' => 'Powered By UNIT3D Community Edition v6.0.8',
+    'powered-by' => 'Powered By UNIT3D Community Edition v6.1.0',
 
     /*
     |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ return [
     |
     */
 
-    'version' => 'v6.0.8',
+    'version' => 'v6.1.0',
 
     /*
     |--------------------------------------------------------------------------
