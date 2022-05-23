63bdaba3489e1d3a40f1fcb5812042038335bcf3
HDVinnie <hdinnovations@protonmail.com>
2022-05-22 20:50:45 -0400

    update: tmdb api
    - new config for meta locale

--- app/Services/Tmdb/Client/Genre.php
+++ app/Services/Tmdb/Client/Genre.php
@@ -34,7 +34,7 @@ class Genre
                 ],
                 'query' => [
                     'api_key'  => \config('api-keys.tmdb'),
-                    'language' => \config('app.locale'),
+                    'language' => \config('app.meta_locale'),
                 ],
             ]
         );
