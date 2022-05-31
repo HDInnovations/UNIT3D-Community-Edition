c1ebe8ea575ccc2a22de94a590977dc792d45257
Shift <shift@laravelshift.com>
2022-05-31 21:28:21 +0000

    Remove redundant DocBlock tags

--- app/Traits/GroupedLastScope.php
+++ app/Traits/GroupedLastScope.php
@@ -26,8 +26,6 @@ trait GroupedLastScope
      *
      * @param \Illuminate\Database\Eloquent\Builder $query
      * @param array|null                            $fields A list of fields that's considered as a unique entry by the query.
-     *
-     * @return \Illuminate\Database\Eloquent\Builder
      */
     public function scopeLastPerGroup(Builder $query, ?array $fields = null): Builder
     {
