92fb549ec61035c4170c9721613abe80bd78c03d
HDVinnie <hdinnovations@protonmail.com>
2022-06-08 23:46:56 -0400

    fix: featured torrents slider query
    - use eager loading on user relation to avoid N+1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -109,7 +109,7 @@ class HomeController extends Controller
         $groups = \cache()->remember('user-groups', $expiresAt, fn () => Group::select(['name', 'color', 'effect', 'icon'])->oldest('position')->get());
 
         // Featured Torrents Block
-        $featured = \cache()->remember('latest_featured', $expiresAt, fn () => FeaturedTorrent::with('torrent')->get());
+        $featured = \cache()->remember('latest_featured', $expiresAt, fn () => FeaturedTorrent::with('torrent', 'user')->get());
 
         // Latest Poll Block
         $poll = \cache()->remember('latest_poll', $expiresAt, fn () => Poll::latest()->first());
