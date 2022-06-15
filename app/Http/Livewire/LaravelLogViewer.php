f5d1438872b85b153fc85096ff54f61a26244b88
Roardom <roardom@protonmail.com>
2022-06-15 01:14:11 -0500

    update: simple laravel log viewer

--- app/Http/Livewire/LaravelLogViewer.php
+++ app/Http/Livewire/LaravelLogViewer.php
@@ -13,25 +13,40 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\File;
 use Livewire\Component;
+use Livewire\WithPagination;
 use SplFileInfo;
 
 class LaravelLogViewer extends Component
 {
-    public $file = 0;
+    use WithPagination;
+
+    public $logs = [0];
 
     public $page = 1;
 
-    public $total;
-
-    public $perPage = 500;
-
-    public $paginator;
+    public $perPage = 5;
 
     protected $queryString = ['page'];
 
-    final protected function getLogFiles()
+    final public function paginationView(): string
+    {
+        return 'vendor.pagination.livewire-pagination';
+    }
+
+    final public function updatedPage(): void
+    {
+        $this->emit('paginationChanged');
+    }
+
+    final public function updatingLogs(): void
+    {
+        $this->page = 1;
+    }
+
+    final public function getLogFilesProperty()
     {
         $directory = \storage_path('logs');
 
@@ -39,29 +54,57 @@ class LaravelLogViewer extends Component
             ->sortByDesc(fn (SplFileInfo $file) => $file->getMTime())->values();
     }
 
-    final public function goto($page): void
+    final public function getEntriesProperty(): \Illuminate\Contracts\Pagination\LengthAwarePaginator
     {
-        $this->page = $page;
-    }
+        $files = $this->logFiles;
+        $logString = '';
+        
+        foreach ($this->logs as $log) {
+            $logString .= file_get_contents($files[$log]->getPathname());
+        }
 
-    final public function updatingFile(): void
-    {
-        $this->page = 1;
+        $entryPattern = '/^\[(?<date>.*)\]\s(?<env>\w+)\.(?<level>\w+)\:\s/m';
+        $contextPattern = '/^(?<message>[^\{]*)?(?:\{"exception"\:"\[object\]\s\((?<exception>[^\s\(]+))?.*\s(?:in|at)\s(?<in>.*)\:(?<line>\d+)\)?/ms';
+
+        $entries = \collect();
+
+        if (\preg_match_all($entryPattern, $logString, $entryMatches, PREG_SET_ORDER) !== false) {
+            $stacktraces = \preg_split($entryPattern, $logString);
+            // Delete the empty first entry
+            \array_shift($stacktraces);
+            $numEntries = \count($entryMatches);
+
+            for ($i = 0; $i < $numEntries; $i++) {
+                // The context is the portion before the first stack trace
+                $context = \preg_split('/^\[stacktrace\]|Stack trace\:/ms', $stacktraces[$i])[0];
+                // The `context` consists of a message, an exception, a filename, and a linecount
+                \preg_match($contextPattern, $context, $contextMatches);
+                
+                $entries->push([
+                    'date'        => $entryMatches[$i]['date'],
+                    'env'         => $entryMatches[$i]['env'],
+                    'level'       => $entryMatches[$i]['level'],
+                    'message'     => $contextMatches['message'] ?? '',
+                    'exception'   => $contextMatches['exception'] ?? '',
+                    'in'          => $contextMatches['in'] ?? '',
+                    'line'        => $contextMatches['line'] ?? '',
+                    'stacktrace'  => $stacktraces[$i],
+                ]);
+            }
+        }
+
+        $currentEntries = $entries->forPage($this->page, $this->perPage);
+
+        return new LengthAwarePaginator($currentEntries, $entries->count(), $this->perPage, $this->page);
     }
 
     final public function render(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
     {
-        $files = $this->getLogFiles();
-
-        $log = \collect(file($files[$this->file]->getPathname(), FILE_IGNORE_NEW_LINES));
-
-        $this->total = (int) \floor($log->count() / $this->perPage) + 1;
-
-        $log = $log->slice(($this->page - 1) * $this->perPage, $this->perPage)->values();
-
-        return \view('livewire.laravel-log-viewer')
-            ->withFiles($files)
-            ->withLog($log)
-            ->extends('layout.default');
+        return \view('livewire.laravel-log-viewer',[
+                'files' => $this->logFiles,
+                'entries' => $this->entries,
+            ])
+            ->extends('layout.default')
+            ->section('content');
     }
 }
--- resources/views/livewire/laravel-log-viewer.blade.php
+++ resources/views/livewire/laravel-log-viewer.blade.php
@@ -1,51 +1,100 @@
-<div>
-    @section('breadcrumbs')
-        <li class="breadcrumbV2">
-            <a href="{{ route('staff.dashboard.index') }}" class="breadcrumb__link">
-                {{ __('staff.staff-dashboard') }}
-            </a>
-        </li>
-        <li class="breadcrumb--active">
-            Laravel Log Viewer
-        </li>
-    @endsection
+@section('title')
+    <title>Laravel Log Viewer - {{ __('staff.staff-dashboard') }} - {{ config('other.title') }}</title>
+@endsection
 
-    <div class="jumbotron">
-        <div class="pull-right">
-            <select wire:model="file" class="form-control">
+@section('meta')
+    <meta name="description" content="Laravel Log Viewer - {{ __('staff.staff-dashboard') }}">
+@endsection
+
+@section('breadcrumbs')
+    <li class="breadcrumbV2">
+        <a href="{{ route('staff.dashboard.index') }}" class="breadcrumb__link">
+            {{ __('staff.staff-dashboard') }}
+        </a>
+    </li>
+    <li class="breadcrumb--active">
+        Laravel Log Viewer
+    </li>
+@endsection
+
+<div class="container-fluid">
+    <div class="block">
+        <h2><i class="{{ config('other.font-awesome') }} fa-list"></i> Laravel Log Viewer</h2>
+        <hr>
+        <div class="text-center">
+            {{ $entries->links() }}
+        </div>
+        <div style="display: grid; grid-template-columns: 1fr min-content; gap: 12px; align-items: flex-start">
+            <table class="table table-condensed table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Environment</th>
+                        <th>Level</th>
+                        <th>Message</th>
+                        <th>Exception</th>
+                        <th>In</th>
+                        <th>Line</th>
+                        <th>Stacktrace</th>
+                    </tr>
+                </thead>
+                @forelse($entries as $entry)
+                    <tbody x-data="{ expanded: false }" style="border-top: 0;">
+                        <tr x-on:click="expanded = !expanded" style="cursor: pointer;">
+                            <td>{{ $entry['date'] }}</td>
+                            <td>{{ $entry['env'] }}</td>
+                            <td>
+                                @switch ($entry['level'])
+                                    @case ('CRITICAL')
+                                        <span class="text-danger">{{ $entry['level'] }}</span>
+                                        @break
+                                    @case ('ERROR')
+                                        <span class="text-warning">{{ $entry['level'] }}</span>
+                                        @break
+                                    @case ('INFO')
+                                        <span class="text-info">{{ $entry['level'] }}</span>
+                                        @break
+                                    @case ('WARNING')
+                                        <span class="text-info">{{ $entry['level'] }}</span>
+                                        @break
+                                    @default
+                                        {{ $entry['level'] }}
+                                @endswitch
+                            </td>
+                            <td>{{ $entry['message'] }}</td>
+                            <td>{{ $entry['exception'] }}</td>
+                            <td>{{ $entry['in'] }}</td>
+                            <td>{{ $entry['line'] }}</td>
+                            <td>
+                                <button class="btn btn-primary" x-on:click.stop="navigator.clipboard.writeText($refs.stacktrace.textContent)">
+                                    Copy
+                                </button>
+                            </td>
+                        </tr>
+                        <tr x-cloak x-show="expanded">
+                            <td colspan="8">
+                                <code><pre x-ref="stacktrace">{{ $entry['stacktrace'] }}</pre></code>
+                            </td>
+                        </tr>
+                    </tbody>
+                @empty
+                    <tbody>
+                        <tr>
+                            <td colspan="8">No logs have been created yet.</td>
+                        </tr>
+                    </tbody>
+                @endforelse
+            </table>
+            <select multiple wire:model="logs" style="height: 320px; padding: 8px; border-radius: 4px;">
                 @foreach($files as $file)
-                    <option value="{{ $loop->index }}">{{ $file->getFilename() }}</option>
+                    <option value="{{ $loop->index }}" style="padding: 6px; border-radius: 4px; cursor: pointer;">
+                        {{ $file->getFilename() }}
+                    </option>
                 @endforeach
             </select>
         </div>
-
-        @include('vendor.pagination.laravel-log-paginator')
-
-        @if($log->count() > 0)
-            <ul class='text-monospace'>
-                @for($i=0; $i < $log->count(); $i++)
-                    @if(\str_starts_with($log[$i],'[stacktrace]') || \str_starts_with($log[$i],'#'))
-                        <li x-data="{expanded:false}" x-on:click="expanded = !expanded">[stacktrace]
-                            <ul class="ml-8" x-show="expanded" x-cloak >
-                                @while($i < $log->count())
-                                    <li wire:key="{{ $page }}-line-{{ $i }}">{{ $log[$i] }}</li>
-                                    @break(\str_starts_with($log[$i++],'"}'))
-                                @endwhile
-                            </ul>
-                        </li>
-                    @endif
-                    @break($i>=$log->count())
-
-                    <li wire:key="{{ $page }}-line-{{ $i }}" class="font-mono text-xs leading-5
-                        {{ \str_contains($log[$i], '.CRITICAL:') ? 'text-danger':''}}
-                        {{ \str_contains($log[$i], '.ERROR:') ? 'text-warning':'' }}
-                        {{ \str_contains($log[$i], '.INFO:') ? 'text-info':'' }}
-                        {{ \str_contains($log[$i], '.WARNING:') ? 'text-info':'' }}
-                        ">{{ $log[$i] }}
-                    </li>
-                @endfor
-            </ul>
-        @endif
+        <div class="text-center">
+            {{ $entries->links() }}
+        </div>
     </div>
 </div>
-
