fbc17f2e20544ede8d93a9666403f47b68e3512a
HDVinnie <hdinnovations@protonmail.com>
2022-06-22 22:24:01 -0400

    update: comments component

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -13,6 +13,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Achievements\UserMade100Comments;
+use App\Achievements\UserMade200Comments;
+use App\Achievements\UserMade300Comments;
+use App\Achievements\UserMade400Comments;
+use App\Achievements\UserMade500Comments;
+use App\Achievements\UserMade50Comments;
+use App\Achievements\UserMade600Comments;
+use App\Achievements\UserMade700Comments;
+use App\Achievements\UserMade800Comments;
+use App\Achievements\UserMade900Comments;
+use App\Achievements\UserMadeComment;
+use App\Achievements\UserMadeTenComments;
+use App\Models\User;
+use App\Notifications\NewComment;
+use App\Repositories\ChatRepository;
+use App\Repositories\TaggedUserRepository;
 use Livewire\Component;
 use Livewire\WithPagination;
 use voku\helper\AntiXSS;
@@ -20,6 +36,12 @@ use voku\helper\AntiXSS;
 class Comments extends Component
 {
     use WithPagination;
+
+    private TaggedUserRepository $taggedUserRepository;
+
+    private ChatRepository $chatRepository;
+
+    private ?\Illuminate\Contracts\Auth\Authenticatable $user;
 
     public $model;
 
@@ -37,9 +59,16 @@ class Comments extends Component
         'newCommentState.content' => 'comment',
     ];
 
+    final public function mount(TaggedUserRepository $taggedUserRepository, ChatRepository $chatRepository): void
+    {
+        $this->taggedUserRepository = $taggedUserRepository;
+        $this->chatRepository = $chatRepository;
+        $this->user = \auth()->user();
+    }
+
     final public function postComment(): void
     {
-        if (\auth()->user()->can_comment === 0) {
+        if ($this->user->can_comment === 0) {
             $this->dispatchBrowserEvent('error', ['type' => 'error',  'message' => \trans('comment.rights-revoked')]);
 
             return;
@@ -50,7 +79,7 @@ class Comments extends Component
         ]);
 
         $comment = $this->model->comments()->make((new AntiXSS())->xss_clean($this->newCommentState));
-        $comment->user()->associate(\auth()->user());
+        $comment->user()->associate($this->user);
         $comment->anon = $this->anon;
         $comment->save();
 
@@ -58,20 +87,84 @@ class Comments extends Component
             'content' => '',
         ];
 
+        // Achievements
+        if ($comment->anon === 0) {
+            $this->user->unlock(new UserMadeComment(), 1);
+            $this->user->addProgress(new UserMadeTenComments(), 1);
+            $this->user->addProgress(new UserMade50Comments(), 1);
+            $this->user->addProgress(new UserMade100Comments(), 1);
+            $this->user->addProgress(new UserMade200Comments(), 1);
+            $this->user->addProgress(new UserMade300Comments(), 1);
+            $this->user->addProgress(new UserMade400Comments(), 1);
+            $this->user->addProgress(new UserMade500Comments(), 1);
+            $this->user->addProgress(new UserMade600Comments(), 1);
+            $this->user->addProgress(new UserMade700Comments(), 1);
+            $this->user->addProgress(new UserMade800Comments(), 1);
+            $this->user->addProgress(new UserMade900Comments(), 1);
+        }
+
+        //Notification
+        if ($this->user->id !== $this->model->user_id) {
+            User::find($this->model->user_id)->notify(new NewComment($this->model, $comment));
+        }
+
+        // Auto Shout
+        $profileUrl = \href_profile($this->user);
+
+        if ($comment->anon === 0) {
+            $this->chatRepository->systemMessage(
+                \sprintf('[url=%s]%s[/url] has left a comment on Torrent [url=%s]%s[/url]', $profileUrl, $this->user->username, $torrentUrl, $torrent->name)
+            );
+        } else {
+            $this->chatRepository->systemMessage(
+                \sprintf('An anonymous user has left a comment on torrent [url=%s]%s[/url]', $torrentUrl, $torrent->name)
+            );
+        }
+
+        // Tagging
+        if ($this->taggedUserRepository->hasTags($request->input('content'))) {
+            if ($this->taggedUserRepository->contains($request->input('content'), '@here') && $this->user->group->is_modo) {
+                $users = \collect([]);
+
+                $torrent->comments()->get()->each(function ($c) use ($users) {
+                    $users->push($c->user);
+                });
+                $this->taggedUserRepository->messageCommentUsers(
+                    'torrent',
+                    $users,
+                    $this->user,
+                    'Staff',
+                    $comment
+                );
+            } else {
+                $sender = $comment->anon !== 0 ? $user->username : 'Anonymous';
+                $this->taggedUserRepository->messageTaggedCommentUsers(
+                    'torrent',
+                    $request->input('content'),
+                    $this->user,
+                    $sender,
+                    $comment
+                );
+            }
+        }
+
         $this->goToPage(1);
     }
 
-    final public function render(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    final public function getCommentsProperty()
     {
-        $comments = $this->model
+        return $this->model
             ->comments()
             ->with('user', 'children.user', 'children.children')
             ->parent()
             ->latest()
             ->paginate(10);
+    }
 
+    final public function render(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
         return \view('livewire.comments', [
-            'comments' => $comments,
+            'comments' => $this->comments,
         ]);
     }
 }
