808f50ca8debd59809f8abf6516ae2c3940cd64e
HDVinnie <hdinnovations@protonmail.com>
2022-06-29 22:09:47 -0400

    update: vite.config.js
    Co-Authored-By: Loki <85806054+LokiThor2021@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,56 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import babel from '@rollup/plugin-babel';
+import copy from 'rollup-plugin-copy';
+import { createVuePlugin } from 'vite-plugin-vue2';
+import ViteComponents from 'vite-plugin-components';
 
 export default defineConfig({
     plugins: [
         laravel([
+            /*
+            * Sourced asset dependencies via node_modules and JS bootstrapping
+            */
             'resources/js/app.js',
-            'resources/js/unit3d/chat.js',
-            'resources/js/unit3d/imgbb.js',
-            'resources/js/vendor/alpine.js',
-            'resources/js/vendor/virtual-select.js',
+            'resources/sass/app.scss',
+
+            /*
+             * Themes
+             *
+             * Note: Default wysibb theme is compiled into public/css/app.css from resources/sass/app.scss
+             */
+            'resources/sass/themes/galactic.scss',
+            'resources/sass/themes/dark-blue.scss',
+            'resources/sass/themes/dark-green.scss',
+            'resources/sass/themes/dark-pink.scss',
+            'resources/sass/themes/dark-purple.scss',
+            'resources/sass/themes/dark-red.scss',
+            'resources/sass/themes/dark-teal.scss',
+            'resources/sass/themes/dark-yellow.scss',
+            'resources/sass/themes/cosmic-void.scss',
+
+            /*
+             * Login and TwoStep Auth styles
+             *
+             * We compile each of these separately since they should only be loaded with the certain views
+             */
+            'resources/sass/main/login.scss',
+            'resources/sass/main/twostep.scss',
+
+            /* Babel File  */
+            'resources/js/unit3d.js'
+
         ]),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
+        babel({ babelHelpers: 'bundled' }),
+        copy({
+            targets: [
+                { src: 'resources/sass/vendor/webfonts/wysibb', dest: 'public/fonts/wysibb' },
+                { src: 'resources/sass/vendor/webfonts/font-awesome', dest: 'public/fonts/font-awesome' },
+                { src: 'resources/sass/vendor/webfonts/bootstrap', dest: 'public/fonts/bootstrap' }
+            ]
         }),
+        createVuePlugin(),
+        ViteComponents({ transformer: 'vue2' })
     ],
+
 });
