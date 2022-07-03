25a9766ea00dfcd8768fc48d3fb881ca5e2d3b4f
Roardom <roardom@protonmail.com>
2022-07-02 19:36:30 -0500

    update: achievements

--- resources/views/achievement/show.blade.php
+++ resources/views/achievement/show.blade.php
@@ -19,75 +19,49 @@
     @include('user.buttons.user')
 @endsection
 
-@section('content')
-    <div class="container-fluid">
-        @if (!auth()->user()->isAllowed($user,'achievement','show_achievement'))
-            <div class="container pl-0 text-center">
-                <div class="jumbotron shadowed">
-                    <div class="container">
-                        <h1 class="mt-5 text-center">
-                            <i class="{{ config('other.font-awesome') }} fa-times text-danger"></i>{{ __('user.private-profile') }}
-                        </h1>
-                        <div class="separator"></div>
-                        <p class="text-center">{{ __('user.not-authorized') }}</p>
-                    </div>
-                </div>
-            </div>
-        @else
-            <div class="block">
-                <div class="some-padding">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">{{ __('user.unlocked-achievements') }}</div>
-                                    <div class="panel-body">
-                                        <br/>
-                                        <div class="table-responsive">
-                                            <table class="table table-borderless">
-                                                <thead>
-                                                <tr>
-                                                    <th>{{ __('common.name') }}</th>
-                                                    <th>{{ __('common.description') }}</th>
-                                                    <th>{{ __('common.progress') }}</th>
-                                                </tr>
-                                                </thead>
-                                                <tbody>
-                                                @foreach ($achievements as $item)
-                                                    <tr>
-                                                        <td><img src="/img/badges/{{ $item->details->name }}.png"
-                                                                 alt="{{ $item->details->name }}" data-toggle="tooltip"
-                                                                 data-original-title="{{ $item->details->name }}"></td>
-                                                        <td>{{ $item->details->description }}</td>
-                                                        @if ($item->isUnlocked())
-                                                            <td>
-                                                                <span class="label label-success">{{ __('user.unlocked') }}</span>
-                                                            </td>
-                                                        @else
-                                                            <td><span class="label label-warning">{{ __('common.progress') }}:
-                                                                        {{ $item->points }}/{{ $item->details->points }}</span>
-                                                            </td>
-                                                        @endif
-                                                    </tr>
-                                                @endforeach
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-4 text-center">
-                                <div class="text-green well well-sm">
-                                    <h3>
-                                        <strong>{{ __('user.unlocked-achievements') }}: </strong>
-                                        {{ $user->unlockedAchievements()->count() }}
-                                    </h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endif
-    </div>
-@endsection
+@section('page', 'page__achievements--show')
+
+@if (auth()->user()->isAllowed($user,'achievement','show_achievement'))
+    @section('main')
+        <section class="panelV2 achievements__unlocked">
+            <h2 class="panel__heading">{{ __('user.unlocked-achievements') }}</h2>
+            @foreach($achievements as $achievement)
+                <article class="achievement" title="{{ $achievement->points }}/{{ $achievement->details->points }}">
+                    <figure class="achievement__badge">
+                        <img
+                            src="/img/badges/{{ $achievement->details->name }}.png"
+                            alt="{{ $achievement->details->name }}"
+                            title="{{ $achievement->details->name }}"
+                        >
+                        <figcaption class="achievement__description">
+                            {{ $achievement->details->description }}
+                        </figcaption>
+                    </figure>
+                    <progress
+                        class="achievement__progress"
+                        max="{{ $achievement->details->points }}"
+                        value="{{ $achievement->points }}"
+                    >
+                    </progress>
+                </article>
+            @endforeach
+        </section>
+    @endsection
+
+    @section('sidebar')
+        <section class="panelV2 achievement__statistics">
+            <h2 class="panel__heading">{{ __('user.statistics') }}</h2>
+            <dl class="key-value">
+                <dt>{{ __('user.unlocked-achievements') }}:</dt>
+                <dd>{{ auth()->user()->unlockedAchievements()->count() }}</dd>
+            </dl>
+        </section>
+    @endsection
+@else
+    @section('main')
+        <section class="panelV2">
+            <h2 class="panel__heading">{{ __('user.private-profile') }}</h2>
+            <div class="panel__body">{{ __('user.not-authorized') }}</div>
+        </section>
+    @endsection
+@endif
