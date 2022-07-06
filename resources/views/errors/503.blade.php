33cadfee35cd02e6e383958872464f8bfbbb48c2
Roardom <roardom@protonmail.com>
2022-07-06 16:58:58 -0500

    update: error views

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,18 +1,5 @@
 @extends('errors.layout')
 
-@section('title')
-    Error 503: Service Unavailable!
-@stop
+@section('title', 'Error 503: Service Unavailable!')
 
-@section('container')
-    <h1 class="mt-5 text-center">
-        <i class="{{ config('other.font-awesome') }} fa-exclamation-circle text-warning"></i> Error 503: Service
-        Unavailable!
-    </h1>
-
-    <div class="separator"></div>
-
-    <p class="text-center">
-        {{ $exception->getMessage() ?: 'Sorry, we are doing some maintenance. Please check back soon.' }}
-    </p>
-@stop
+@section('description', $exception->getMessage() ?: 'Sorry, we are doing some maintenance. Please check back soon.')
