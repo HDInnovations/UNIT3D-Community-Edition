c0cf86da9b858e97dce5ba6b87c4cbece68b7300
Roardom <roardom@protonmail.com>
2022-07-06 09:53:39 -0500

    update: bonus views

--- resources/views/bonus/tips.blade.php
+++ resources/views/bonus/tips.blade.php
@@ -24,77 +24,64 @@
     @include('user.buttons.user')
 @endsection
 
-@section('content')
-    <div class="container">
-        <div class="block">
-            <div class="some-padding">
-                <div class="row">
-                    <div class="col-sm-8">
-                        <div class="well">
-                            <h3>{{ __('bon.tips') }}</h3>
-                            <table class="table table-condensed table-striped">
-                                <thead>
-                                <tr>
-                                    <th>{{ __('bon.sender') }}</th>
-                                    <th>{{ __('bon.receiver') }}</th>
-                                    <th>{{ __('bon.points') }}</th>
-                                    <th>{{ __('bon.date') }}</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($bontransactions as $b)
-                                    <tr>
-                                        <td>
-                                            <a href="{{ route('users.show', ['username' => $b->senderObj->username]) }}">
-                                                <span class="badge-user text-bold">{{ $b->senderObj->username }}</span>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            @if($b->whereNotNull('torrent_id'))
-                                                @php $torrent = App\Models\Torrent::select(['anon'])->find($b->torrent_id) @endphp
-                                            @endif
-                                            @if(isset($torrent) && $torrent->anon === 1 && $b->receiver !== $user->id)
-                                                <span class="badge-user text-bold">{{ __('common.anonymous') }}</span>
-                                            @else
-                                                <a href="{{ route('users.show', ['username' => $b->receiverObj->username]) }}">
-                                                    <span class="badge-user text-bold">{{ $b->receiverObj->username }}</span>
-                                                </a>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            {{ $b->cost }}
-                                        </td>
-                                        <td>
-                                            {{ $b->date_actioned }}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            <div class="some-padding text-center">
-                                {{ $bontransactions->links() }}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4">
+@section('page', 'page__bonus--tips')
 
-                        <div class="text-blue well well-sm text-center">
-                            <h2><strong>{{ __('bon.your-points') }}: {{ $userbon }}<br></strong></h2>
-                        </div>
-
-                        <div class="text-orange well well-sm text-center">
-                            <div>
-                                <h3>{{ __('bon.you-have-received-tips') }}: <strong>{{ $tips_received }}</strong>
-                                    {{ __('bon.total-tips') }}</h3>
-                            </div>
-                            <div>
-                                <h3>{{ __('bon.you-have-sent-tips') }}: <strong>{{ $tips_sent }}</strong>
-                                    {{ __('bon.total-tips') }}</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+@section('main')
+    <section class="panelV2">
+        <h2 class="panel__heading">{{ __('bon.tips') }}</h2>
+        <table class="table table-condensed table-striped">
+            <thead>
+            <tr>
+                <th>{{ __('bon.sender') }}</th>
+                <th>{{ __('bon.receiver') }}</th>
+                <th>{{ __('bon.points') }}</th>
+                <th>{{ __('bon.date') }}</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($bontransactions as $b)
+                <tr>
+                    <td>
+                        <a href="{{ route('users.show', ['username' => $b->senderObj->username]) }}">
+                            <span class="badge-user text-bold">{{ $b->senderObj->username }}</span>
+                        </a>
+                    </td>
+                    <td>
+                        @if($b->whereNotNull('torrent_id'))
+                            @php $torrent = App\Models\Torrent::select(['anon'])->find($b->torrent_id) @endphp
+                        @endif
+                        @if(isset($torrent) && $torrent->anon === 1 && $b->receiver !== $user->id)
+                            <span class="badge-user text-bold">{{ __('common.anonymous') }}</span>
+                        @else
+                            <a href="{{ route('users.show', ['username' => $b->receiverObj->username]) }}">
+                                <span class="badge-user text-bold">{{ $b->receiverObj->username }}</span>
+                            </a>
+                        @endif
+                    </td>
+                    <td>{{ $b->cost }}</td>
+                    <td>{{ $b->date_actioned }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <div class="some-padding text-center">
+            {{ $bontransactions->links() }}
         </div>
-    </div>
+    </section>
+@endsection
+
+@section('sidebar')
+    <section class="panelV2">
+        <h2 class="panel__heading">{{ __('bon.your-points') }}</h2>
+        <div class="panel__body">{{ $userbon }}</div>
+    </section>
+    <section class="panelV2">
+        <h2 class="panel__heading">{{ __('bon.total-tips') }}</h2>
+        <dl class="key-value">
+            <dt>{{ __('bon.you-have-received-tips') }}</dt>
+            <dd>{{ $tips_received }}</dd>
+            <dt>{{ __('bon.you-have-sent-tips') }}</dt>
+            <dd>{{ $tips_sent }}</dd>
+        </div>
+    </section>
 @endsection
