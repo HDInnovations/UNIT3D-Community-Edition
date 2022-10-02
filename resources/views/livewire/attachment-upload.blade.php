eb3e3f8d38db10e3e74adaf7f513286cae3d8e4f
Roardom <roardom@protonmail.com>
2022-10-02 02:33:04 -0500

    update: helpdesk views

--- resources/views/livewire/attachment-upload.blade.php
+++ resources/views/livewire/attachment-upload.blade.php
@@ -1,7 +1,59 @@
-<div>
-    <input type="file" wire:model="attachment">
-
-    @error('attachment') <span class="error">{{ $message }}</span> @enderror
-
-    <button wire:click="upload">{{ __('ticket.attachments-save') }}</button>
-</div>
+<section class="panelV2">
+    <header class="panel__header">
+        <h2 class="panel__heading">{{ __('ticket.attachments') }}</h2>
+        <div class="panel__actions">
+            <div class="panel__action">
+                <div class="form__group">
+                    <input
+                        id="attachment"
+                        class="form__file"
+                        type="file"
+                        wire:model="attachment"
+                        wire:change="upload"
+                        style="display: none"
+                    >
+                    <label class="form__button form__button--text" for="attachment">
+                        {{ __('common.add') }}
+                    </label>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="data-table-wrapper">
+        <table class="data-table">
+            <thead>
+                <tr>
+                    <th>{{ __('common.name') }}</th>
+                    <th>{{ __('common.actions') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($attachments as $attachment)
+                    <tr>
+                        <td>{{ $attachment->file_name }}</td>
+                        <td>
+                            <menu class="data-table__actions">
+                                <li class="data-table__action">
+                                    <form
+                                        action="{{ route('tickets.attachment.download', $attachment) }}"
+                                        method="POST"
+                                    >
+                                        @csrf
+                                        <button class="form__button form__button--text">{{ __('ticket.download') }}</button>
+                                    </form>
+                                </li>
+                            </menu>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="2">{{ __('ticket.no-attach') }}</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+    @error('attachment')
+        {{ $message }}
+    @enderror
+</section>
--- resources/views/ticket/create.blade.php
+++ resources/views/ticket/create.blade.php
@@ -15,70 +15,90 @@
     </li>
 @endsection
 
+@section('page', 'page__ticket--create')
+
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10 col-sm-10 col-md-offset-1">
-                <div class="panel panel-chat shoutbox">
-                    <div class="panel-heading"><i class="fas fa-plus"></i> {{ __('ticket.create-ticket') }}</div>
-                    <div class="panel-body">
-                        @if(session('errors'))
-                            <div class="alert alert-danger">
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                                <h6><b>{{ __('ticket.fix-errors') }}</b></h6>
-                                <ul class="mb-0">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                        <form action="{{ route('tickets.store') }}" method="POST">
-                            @csrf
-                            <div class="form-row">
-                                <div class="form-group col-6">
-                                    <label for="category">{{ __('ticket.category') }} <span
-                                                class="text-danger small">*</span></label>
-                                    <select name="category" id="category" class="form-control">
-                                        @foreach($categories as $category)
-                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="form-group col-6">
-                                    <label for="priority">{{ __('ticket.priority') }} <span
-                                                class="text-danger small">*</span></label>
-                                    <select name="priority" id="priority" class="form-control">
-                                        @foreach($priorities as $priority)
-                                            <option value="{{ $priority->id }}">{{ $priority->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="form-group col-12">
-                                    <label for="subject">{{ __('ticket.subject') }} <span
-                                                class="text-danger small">*</span></label>
-                                    <input type="text" class="form-control" name="subject" id="subject"
-                                           placeholder="{{ __('ticket.subject-enter') }}" autocomplete="off">
-                                </div>
-                                <div class="form-group col-12">
-                                    <label for="body">{{ __('ticket.body') }} <span
-                                                class="text-danger small">*</span></label>
-                                    <textarea name="body" id="body" cols="30" rows="3" class="form-control"
-                                              placeholder="{{ __('ticket.body-enter') }}"></textarea>
-                                </div>
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-success btn-md"><i
-                                                class="fas fa-save"></i> {{ __('ticket.submit-ticket') }}</button>
-                                    <button type="reset" class="btn btn-danger btn-md"><i
-                                                class="fas fa-eraser"></i> {{ __('ticket.reset') }}</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+    <section class="panelV2">
+        <h2 class="panel__heading">
+            <i class="fas fa-plus"></i>
+            {{ __('ticket.create-ticket') }}
+        </h2>
+        <div class="panel__body">
+            @if(session('errors'))
+                <div class="alert alert-danger">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h6><b>{{ __('ticket.fix-errors') }}</b></h6>
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
-            </div>
+            @endif
+            <form class="form" action="{{ route('tickets.store') }}" method="POST">
+                @csrf
+                <p class="form__group">
+                    <select
+                        id="category"
+                        class="form__text"
+                        name="category"
+                        required
+                    >
+                        <option hidden disabled selected value=""></option>
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                    <label for="category" class="form__label form__label--floating">
+                        {{ __('ticket.category') }}
+                    </label>
+                </p>
+                <p class="form__group">
+                    <select
+                        name="priority"
+                        id="priority"
+                        class="form__select"
+                        required
+                    >
+                        <option hidden disabled selected value=""></option>
+                        @foreach($priorities as $priority)
+                            <option value="{{ $priority->id }}">{{ $priority->name }}</option>
+                        @endforeach
+                    </select>
+                    <label for="priority" class="form__label form__label--floating">
+                        {{ __('ticket.priority') }}
+                    </label>
+                </p>
+                <p class="form__group">
+                    <input
+                        id="ticket_subject"
+                        class="form__text"
+                        name="subject"
+                        required
+                    >
+                    <label for="ticket_subject" class="form__label form__label--floating">
+                        {{ __('ticket.subject') }}
+                    </label>
+                </p>
+                <p class="form__group">
+                    <textarea
+                        id="body"
+                        class="form__textarea"
+                        name="body"
+                        required
+                    ></textarea>
+                    <label for="body" class="form__label form__label--floating">
+                        {{ __('ticket.body') }}
+                    </label>
+                </p>
+                <p class="form__group">
+                    <button class="form__button form__button--filled">
+                        {{ __('common.submit') }}
+                    </button>
+                </p>
+            </form>
         </div>
     </div>
 @endsection
