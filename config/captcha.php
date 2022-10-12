c728c38ecb7d2335156aa9d710423cb16a984ee2
alkl58 <alkl58@protonmail.com>
2022-10-12 15:14:43 +0200

    add: 2fa configs

--- config/captcha.php
+++ config/captcha.php
@@ -22,4 +22,33 @@ return [
     */
 
     'enabled' => true,
+
+    /*
+    |
+    | Captcha Image Settings
+    |
+    */
+    'characters' => ['2', '3', '4', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
+    
+    'default'   => [
+        'length'    => 4,
+        'width'     => 200,
+        'height'    => 50,
+        'quality'   => 90,
+        'math'      => false, //Enable Math Captcha
+        'expire'    => 60,   //Stateless/API captcha expiration
+    ],
+
+    'inverse' => [
+        'length' => 4,
+        'width' => 120,
+        'height' => 36,
+        'quality' => 90,
+        'sensitive' => true,
+        'angle' => 12,
+        'sharpen' => 10,
+        'blur' => 1,
+        'invert' => true,
+        'contrast' => 0,
+    ]
 ];
