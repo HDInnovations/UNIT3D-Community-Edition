035e674c83d8e8712f2f5ff08d45f8720e2b987a
HDVinnie <hdinnovations@protonmail.com>
2022-11-27 18:21:02 -0500

    update: 2022_11_27_062458_drop_old_tables.php

--- database/migrations/2022_11_27_062458_drop_old_tables.php
+++ database/migrations/2022_11_27_062458_drop_old_tables.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
      */
     public function up()
     {
-        Schema::dropIfExists('albums');
         Schema::dropIfExists('images');
+        Schema::dropIfExists('albums');
     }
 };
