3a2b5a4374a19cde9220c5996502148bff8542ae
HDVinnie <HDVinnie@users.noreply.github.com>
2022-12-25 21:55:20 +0000

    PSR12 Linting

--- app/Http/Controllers/User/TorrentZipController.php
+++ app/Http/Controllers/User/TorrentZipController.php
@@ -16,7 +16,6 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class TorrentZipController extends Controller
 {
