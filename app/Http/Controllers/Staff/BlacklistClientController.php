05b7764edc5d4c35078ac0d0c5c347b966650b02
Roardom <roardom@protonmail.com>
2023-01-08 04:40:56 -0600

    refactor: move blacklist client requests to form requests

--- app/Http/Controllers/Staff/BlacklistClientController.php
+++ app/Http/Controllers/Staff/BlacklistClientController.php
@@ -14,8 +14,9 @@
 namespace App\Http\Controllers\Staff;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Staff\StoreBlacklistClientRequest;
+use App\Http\Requests\Staff\UpdateBlacklistClientRequest;
 use App\Models\BlacklistClient;
-use Illuminate\Http\Request;
 
 /**
  * @see \Tests\Feature\Http\Controllers\Staff\GroupControllerTest
@@ -45,23 +46,9 @@ class BlacklistClientController extends Controller
     /**
      * Edit A Blacklisted Client.
      */
-    public function update(Request $request, int $id): \Illuminate\Http\RedirectResponse
+    public function update(UpdateBlacklistClientRequest $request, int $id): \Illuminate\Http\RedirectResponse
     {
-        $client = BlacklistClient::findOrFail($id);
-        $client->name = $request->input('name');
-        $client->reason = $request->input('reason');
-
-        $v = \validator($client->toArray(), [
-            'name'   => 'required|string',
-            'reason' => 'sometimes|string',
-        ]);
-
-        if ($v->fails()) {
-            return \to_route('staff.blacklists.clients.index')
-                ->withErrors($v->errors());
-        }
-
-        $client->save();
+        BlacklistClient::where('id', '=', $id)->update($request->validated());
 
         \cache()->forget('client_blacklist');
 
@@ -80,23 +67,9 @@ class BlacklistClientController extends Controller
     /**
      * Store A New Blacklisted Client.
      */
-    public function store(Request $request): \Illuminate\Http\RedirectResponse
+    public function store(StoreBlacklistClientRequest $request): \Illuminate\Http\RedirectResponse
     {
-        $client = new BlacklistClient();
-        $client->name = $request->input('name');
-        $client->reason = $request->input('reason');
-
-        $v = \validator($client->toArray(), [
-            'name'   => 'required|string|unique:blacklist_clients',
-            'reason' => 'sometimes|string',
-        ]);
-
-        if ($v->fails()) {
-            return \to_route('staff.blacklists.clients.index')
-                ->withErrors($v->errors());
-        }
-
-        $client->save();
+        BlacklistClient::create($request->validated());
 
         \cache()->forget('client_blacklist');
 
