84dacf051dbc98ffdca33dc7ea83815682213fe8
HDVinnie <hdinnovations@protonmail.com>
2023-01-09 00:48:25 -0500

    update: movie client

--- app/Services/Tmdb/Client/Movie.php
+++ app/Services/Tmdb/Client/Movie.php
@@ -198,11 +198,11 @@ class Movie
 
     public function get_cast()
     {
-        return $this->data['credits']['cast'];
+        return $this->data['credits']['cast'] ?? null;
     }
 
     public function get_crew()
     {
-        return $this->data['credits']['crew'];
+        return $this->data['credits']['crew'] ?? null;
     }
 }
