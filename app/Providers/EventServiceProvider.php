054453dffa856f31a05bb085c10eb3cd85cb8779
Shift <shift@laravelshift.com>
2023-02-01 14:16:07 +0000

    Shift core files

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -91,4 +91,15 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
     }
+
+    /**
+     * Determine if events and listeners should be automatically discovered.
+     *
+     * @return bool
+     */
+    public function shouldDiscoverEvents()
+    {
+        return false;
+    }
 }
+
