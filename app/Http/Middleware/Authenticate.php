af180307a6c80ca79ee79b7b7f89bed0546c71eb
Shift <shift@laravelshift.com>
2023-02-15 00:30:18 +0000

    Add PHP type hints for Laravel 10

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -20,7 +20,7 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): ?string
     {
         if (! $request->expectsJson()) {
             return route('login');
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -97,7 +97,7 @@ class EventServiceProvider extends ServiceProvider
      *
      * @return bool
      */
-    public function shouldDiscoverEvents()
+    public function shouldDiscoverEvents(): bool
     {
         return false;
     }
