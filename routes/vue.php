f14bfd16eaea4f24ccde867249e6a676a05bfbb4
HDVinnie <hdinnovations@protonmail.com>
2023-02-22 13:19:46 -0500

    Revert "Convert route options to fluent methods"
    This reverts commit ca666143fd06f1503f06e91b7591fa855629c694.

--- routes/vue.php
+++ routes/vue.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::middleware('auth', 'twostep', 'banned')->group(function (): void {
+Route::group(['middleware' => ['auth', 'twostep', 'banned']], function (): void {
     Route::prefix('chat')->group(function (): void {
         Route::get('/config', [App\Http\Controllers\API\ChatController::class, 'config']);
 
