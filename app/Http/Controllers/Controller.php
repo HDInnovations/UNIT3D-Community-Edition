3e8567376b3d8a545d5e555a848f52d90c34fcd6
HDVinnie <hdinnovations@protonmail.com>
2023-02-22 13:28:31 -0500

    update: core laravel files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,6 +20,5 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests;
-    use DispatchesJobs;
     use ValidatesRequests;
 }
