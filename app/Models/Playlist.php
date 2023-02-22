cc7ae6976fdb2256868d284d04a06a7ce567a7cc
HDVinnie <hdinnovations@protonmail.com>
2023-02-21 21:34:02 -0500

    add: bbcode support for playlists

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -13,9 +13,12 @@
 
 namespace App\Models;
 
+use App\Helpers\Bbcode;
+use App\Helpers\Linkify;
 use App\Traits\Auditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use voku\helper\AntiXSS;
 
 class Playlist extends Model
 {
@@ -44,5 +47,23 @@ class Playlist extends Model
     public function comments(): \Illuminate\Database\Eloquent\Relations\MorphMany
     {
         return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    /**
+     * Set The Playlists Description After It's Been Purified.
+     */
+    public function setDescriptionAttribute(?string $value): void
+    {
+        $this->attributes['description'] = htmlspecialchars((new AntiXSS())->xss_clean($value), ENT_NOQUOTES);
+    }
+
+    /**
+     * Parse Description And Return Valid HTML.
+     */
+    public function getDescriptionHtml(): string
+    {
+        $bbcode = new Bbcode();
+
+        return (new Linkify())->linky($bbcode->parse(htmlspecialchars_decode($this->description)));
     }
 }
