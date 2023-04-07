7620838f3cbf4a450dbbd52289c9ef16b5c360bc
Roardom <roardom@protonmail.com>
2023-04-07 05:46:55 +0000

    update: user resurrections view

--- resources/views/livewire/user-resurrections.blade.php
+++ resources/views/livewire/user-resurrections.blade.php
@@ -1,147 +1,160 @@
-<div class="container-fluid">
-    <div class="block">
-        <div class="container well search mt-5">
-            <div class="form-horizontal form-condensed form-torrent-search form-bordered">
-                <div class="mx-0 mt-5 form-group fatten-me">
-                    <label for="name"
-                            class="mt-5 col-sm-1 label label-default fatten-me">{{ __('torrent.name') }}</label>
-                    <div class="col-sm-9 fatten-me">
-                        <label for="search"></label><input type="text" class="form-control userFilter"
-                                                            trigger="keyup"
-                                                            id="search" placeholder="{{ __('torrent.name') }}">
-                    </div>
+<div style="display: flex; flex-direction: column; gap: 1rem;">
+    <section class="panelV2 user-resurrections__filters">
+        <h2 class="panel__heading">{{ __('common.search') }}</h2>
+        <div class="panel__body">
+            <form class="form">
+                <div class="form__group">
+                    <p class="form__group">
+                        <input wire:model="name" class="form__text" placeholder="" autofocus="">
+                        <label class="form__label form__label--floating">{{ __('torrent.name') }}</label>
+                    </p>
                 </div>
-                <div class="mx-0 mt-5 form-group fatten-me">
-                    <div class="mt-5 col-sm-1 label label-default fatten-me">
-                        {{ __('torrent.filters') }}
-                    </div>
-                    <div class="col-sm-10">
-                        <span class="badge-user">
-                            <label style="user-select: none" class="inline" x-data="{ state: @entangle('rewarded'), ...ternaryCheckbox() }">
-                                <input
-                                    type="checkbox"
-                                    class="user-torrents__checkbox"
-                                    x-init="updateTernaryCheckboxProperties($el, state)"
-                                    x-on:click="state = getNextTernaryCheckboxState(state); updateTernaryCheckboxProperties($el, state)"
-                                    x-bind:checked="state === 'include'"
-                                >
-                                {{ __('graveyard.rewarded') }}
-                            </label>
-                        </span>
-                    </div>
-                </div>
-            </div>
+                <p class="form__group">
+                    <label style="user-select: none" class="form__label" x-data="{ state: @entangle('rewarded'), ...ternaryCheckbox() }">
+                        <input
+                            type="checkbox"
+                            class="user-resurrections__checkbox"
+                            x-init="updateTernaryCheckboxProperties($el, state)"
+                            x-on:click="state = getNextTernaryCheckboxState(state); updateTernaryCheckboxProperties($el, state)"
+                            x-bind:checked="state === 'include'"
+                        >
+                        {{ __('graveyard.rewarded') }}
+                    </label>
+                </p>
+            </form>
         </div>
-        <span id="filterHeader"></span>
-        <div id="userFilter" userName="{{ $user->username }}" userId="{{ $user->id }}" view="resurrections">
-            <div class="table-responsive">
-                <table class="table table-condensed table-striped table-bordered">
-                    <thead>
-                        <th class="user-resurrections__name-header" wire:click="sortBy('name')" role="columnheader button">
-                            {{ __('torrent.name') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'name'])
-                        </th>
-                        <th class="user-resurrections__size-header" wire:click="sortBy('size')" role="columnheader button">
-                            {{ __('torrent.size') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'size'])
-                        </th>
-                        <th class="user-resurrections__seeders-header" wire:click="sortBy('seeders')" role="columnheader button">
-                            {{ __('torrent.seeders') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'seeders'])
-                        </th>
-                        <th class="user-resurrections__leechers-header" wire:click="sortBy('leechers')" role="columnheader button">
-                            {{ __('torrent.leechers') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'leechers'])
-                        </th>
-                        <th class="user-resurrections__times-completed-header" wire:click="sortBy('times_completed')" role="columnheader button">
-                            {{ __('torrent.completed') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'times_completed'])
-                        </th>
-                        <th class="user-resurrections__created-at-header" wire:click="sortBy('created_at')" role="columnheader button">
-                            {{ __('graveyard.resurrect-date') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'created_at'])
-                        </th>
-                        <th class="user-resurrections__current-seedtime-header">
-                            {{ __('graveyard.current-seedtime') }}
-                        </th>
-                        <th class="user-resurrections__seedtime-header" wire:click="sortBy('seedtime')" role="columnheader button">
-                            {{ __('graveyard.seedtime-goal') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'seedtime'])
-                        </th>
-                        <th class="user-resurrections__rewarded-header" wire:click="sortBy('rewarded')" role="columnheader button">
-                            {{ __('graveyard.rewarded') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'rewarded'])
-                        </th>
-                        <th class="user-resurrections__cancel-header">
-                            {{ __('common.cancel') }}
-                        </th>
-                    </thead>
-                    <tbody>
+    </section>
+    <section class="panelV2">
+        <h2 class="panel__heading">{{ __('user.resurrections') }}</h2>
+        <div class="data-table">
+            <table class="data-table">
+                <thead>
+                    <th class="user-resurrections__name-header" wire:click="sortBy('name')" role="columnheader button">
+                        {{ __('torrent.name') }}
+                        @include('livewire.includes._sort-icon', ['field' => 'name'])
+                    </th>
+                    <th class="user-resurrections__size-header" wire:click="sortBy('size')" role="columnheader button">
+                        {{ __('torrent.size') }}
+                        @include('livewire.includes._sort-icon', ['field' => 'size'])
+                    </th>
+                    <th class="user-resurrections__seeders-header" wire:click="sortBy('seeders')" role="columnheader button">
+                        {{ __('torrent.seeders') }}
+                        @include('livewire.includes._sort-icon', ['field' => 'seeders'])
+                    </th>
+                    <th class="user-resurrections__leechers-header" wire:click="sortBy('leechers')" role="columnheader button">
+                        {{ __('torrent.leechers') }}
+                        @include('livewire.includes._sort-icon', ['field' => 'leechers'])
+                    </th>
+                    <th class="user-resurrections__times-completed-header" wire:click="sortBy('times_completed')" role="columnheader button">
+                        {{ __('torrent.completed') }}
+                        @include('livewire.includes._sort-icon', ['field' => 'times_completed'])
+                    </th>
+                    <th class="user-resurrections__created-at-header" wire:click="sortBy('created_at')" role="columnheader button">
+                        {{ __('graveyard.resurrect-date') }}
+                        @include('livewire.includes._sort-icon', ['field' => 'created_at'])
+                    </th>
+                    <th class="user-resurrections__current-seedtime-header">
+                        {{ __('graveyard.current-seedtime') }}
+                    </th>
+                    <th class="user-resurrections__seedtime-header" wire:click="sortBy('seedtime')" role="columnheader button">
+                        {{ __('graveyard.seedtime-goal') }}
+                        @include('livewire.includes._sort-icon', ['field' => 'seedtime'])
+                    </th>
+                    <th class="user-resurrections__rewarded-header" wire:click="sortBy('rewarded')" role="columnheader button">
+                        {{ __('graveyard.rewarded') }}
+                        @include('livewire.includes._sort-icon', ['field' => 'rewarded'])
+                    </th>
+                    <th class="user-resurrections__actions-header">
+                        {{ __('common.actions') }}
+                    </th>
+                </thead>
+                <tbody>
                     @foreach ($resurrections as $resurrection)
                         <tr>
-                            <td>
-                                <div class="torrent-file">
-                                    <div>
-                                        <a class="view-torrent"
-                                            href="{{ route('torrent', ['id' => $resurrection->torrent->id]) }}">
-                                            {{ $resurrection->torrent->name }}
-                                        </a>
-                                    </div>
-                                </div>
+                            <td class="user-resurrections__name">
+                                <a href="{{ route('torrent', ['id' => $resurrection->torrent->id]) }}">
+                                    {{ $resurrection->torrent->name }}
+                                </a>
                             </td>
-                            <td>
-                                    <span class="badge-extra text-blue text-bold">
-                                        {{ $resurrection->torrent->getSize() }}</span>
+                            <td class="user-resurrections__size">
+                                {{ App\Helpers\StringHelper::formatBytes($resurrection->torrent->size) }}
                             </td>
-                            <td>
-                                    <span class="badge-extra text-green text-bold">
-                                        {{ $resurrection->torrent->seeders }}</span>
+                            <td class="user-resurrections__seeders">
+                                <a href="{{ route('peers', ['id' => $resurrection->torrent->id]) }}">
+                                    <span class='text-green'>
+                                        {{ $resurrection->torrent->seeders }}
+                                    </span>
+                                </a>
                             </td>
-                            <td>
-                                    <span class="badge-extra text-red text-bold">
-                                        {{ $resurrection->torrent->leechers }}</span>
+                            <td class="user-resurrections__leechers">
+                                <a href="{{ route('peers', ['id' => $resurrection->torrent->id]) }}">
+                                    <span class='text-red'>
+                                        {{ $resurrection->torrent->leechers }}
+                                    </span>
+                                </a>
                             </td>
-                            <td>
-                                    <span class="badge-extra text-orange text-bold">
-                                        {{ $resurrection->torrent->times_completed }} {{ __('common.times') }}</span>
+                            <td class="user-resurrections__times_completed">
+                                <a href="{{ route('history', ['id' => $resurrection->torrent->id]) }}">
+                                    <span class='text-orange'>
+                                        {{ $resurrection->torrent->times_completed }}
+                                    </span>
+                                </a>
                             </td>
-                            <td>
+                            <td class="user-resurrections__created-at">
                                 {{ $resurrection->created_at->diffForHumans() }}
                             </td>
-                            <td>
+                            <td class="user-resurrections__current-seedtime">
                                 @php $history = App\Models\History::select(['seedtime'])->where('user_id', '=',
                                     $user->id)->where('torrent_id', '=', $resurrection->torrent_id)->first() @endphp
                                 {{ empty($history) ? '0' : App\Helpers\StringHelper::timeElapsed($history->seedtime) }}
                             </td>
-                            <td>
+                            <td class="user-resurrections__seedtime">
                                 {{ App\Helpers\StringHelper::timeElapsed($resurrection->seedtime) }}
                             </td>
-                            <td>
-                                @if ($resurrection->rewarded == 0)
-                                    <i class="{{ config('other.font-awesome') }} fa-times text-red"></i>
-                                @else
+                            <td class="user-resurrections__rewarded">
+                                @if ($resurrection->rewarded)
                                     <i class="{{ config('other.font-awesome') }} fa-check text-green"></i>
+                                @else
+                                    <i class="{{ config('other.font-awesome') }} fa-times text-red"></i>
                                 @endif
                             </td>
-                            <td>
-                                <form action="{{ route('graveyard.destroy', ['id' => $resurrection->id]) }}"
-                                        method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-sm btn-danger" @if ($resurrection->rewarded ==
-                                            1) disabled @endif>
-                                        <i class="{{ config('other.font-awesome') }} fa-trash"></i>
-                                    </button>
-                                </form>
+                            <td class="user-resurrections__actions">
+                                <menu class="data-table__actions">
+                                    <li class="data-table__action">
+                                        <form
+                                            action="{{ route('graveyard.destroy', ['id' => $resurrection->id]) }}"
+                                            method="POST"
+                                            x-data
+                                        >
+                                            @csrf
+                                            @method('DELETE')
+                                            <button
+                                                x-on:click.prevent="Swal.fire({
+                                                    title: 'Are you sure?',
+                                                    text: 'Are you sure you want to cancel this resurrection: {{ $resurrection->torrent->name }}?',
+                                                    icon: 'warning',
+                                                    showConfirmButton: true,
+                                                    showCancelButton: true,
+                                                }).then((result) => {
+                                                    if (result.isConfirmed) {
+                                                        $root.submit();
+                                                    }
+                                                })"
+                                                class="form__button form__button--text"
+                                            >
+                                                {{ __('common.cancel') }}
+                                            </button>
+                                        </form>
+                                    </li>
+                                </menu>
                             </td>
                         </tr>
                     @endforeach
-                    </tbody>
-                </table>
-                {{ $resurrections->links('partials.pagination') }}
-            </div>
+                </tbody>
+            </table>
         </div>
-    </div>
+        {{ $resurrections->links('partials.pagination') }}
+    </section>
     <script nonce="{{ HDVinnie\SecureHeaders\SecureHeaders::nonce('script') }}">
         function ternaryCheckbox() {
             return {
