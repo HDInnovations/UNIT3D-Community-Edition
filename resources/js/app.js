cbb4670b3dbb36f04b09f564bd5cc7da716b69d3
HDVinnie <hdinnovations@protonmail.com>
2023-04-12 08:52:51 -0400

    update: dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,50 @@
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
  */
 
-require('./bootstrap');
+try {
+    // Note: Eventually we will end up 100% jQuery free.
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap-sass');
+} catch (e) {}
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content,
+    },
+});
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-import Vue from 'vue';
-import version from './components/Version';
-import chatbox from './components/chat/Chatbox';
 
-if (document.getElementById('vue')) {
-    new Vue({
-        el: '#vue',
-        components: { version: version, chatbox: chatbox },
-    });
+window.axios = require('axios');
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+};
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+
+/*
+ * NPM Packages
+ */
+// Sweet Alert
+window.Swal = require('sweetalert2');
--- resources/js/unit3d/chat.js
+++ resources/js/unit3d/chat.js
@@ -1,4 +1,19 @@
 /**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+import Vue from 'vue';
+import chatbox from '../components/chat/Chatbox';
+
+if (document.getElementById('vue')) {
+    new Vue({
+        el: '#vue',
+        components: { chatbox: chatbox },
+    });
+}
+
+/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
