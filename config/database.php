45dbf0849bd49cbc8bfdea5d01776eebea46697e
HDVinnie <hdinnovations@protonmail.com>
2023-04-21 21:28:18 -0400

    fix: config/database.php
    - predis is shipped by default in UNIT3D

--- config/database.php
+++ config/database.php
@@ -127,7 +127,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
