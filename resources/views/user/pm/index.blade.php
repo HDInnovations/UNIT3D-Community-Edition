670348918842a26315001e2d8071501240a4c720
Roardom <roardom@protonmail.com>
2023-05-09 00:30:49 +0000

    fix: browsing to page 2 of inbox search results
    The paginate() function only works with GET requests. POST is only required for database changes, and these functions only read the database, so they can be converted to GET. Partially reverts #427.

--- resources/views/user/pm/index.blade.php
+++ resources/views/user/pm/index.blade.php
@@ -25,9 +25,7 @@
                 <form
                     class="panel__action"
                     action="{{ route('searchPMInbox') }}"
-                    method="POST"
                 >
-                    @csrf
                     <p class="form__group">
                         <input
                             id="search"
