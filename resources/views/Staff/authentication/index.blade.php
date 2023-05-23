3e38619fb8e4c1709956ba2796fce26f85f17084
HDVinnie <hdinnovations@protonmail.com>
2023-05-22 23:35:11 -0400

    add: failed login searching

--- resources/views/Staff/authentication/index.blade.php
+++ resources/views/Staff/authentication/index.blade.php
@@ -22,49 +22,5 @@
 @section('page', 'page__failed-logins-log--index')
 
 @section('main')
-    <section class="panelV2">
-        <h2 class="panel__heading">{{ __('staff.failed-login-log') }}</h2>
-        <div class="data-table-wrapper">
-            <table class="data-table">
-                <thead>
-                    <tr>
-                        <th>{{ __('common.no') }}</th>
-                        <th>{{ __('user.user-id') }}</th>
-                        <th>{{ __('common.username') }}</th>
-                        <th>{{ __('common.ip') }}</th>
-                        <th>{{ __('user.created-on') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($attempts as $attempt)
-                        <tr>
-                            <td>{{ $attempt->id }}</td>
-                            <td>{{ $attempt->user_id ?? 'Not Found' }}</td>
-                            <td>
-                                @if ($attempt->user_id == null)
-                                    {{ $attempt->username }}
-                                @else
-                                    <a class="text-bold"
-                                    href="{{ route('users.show', ['username' => $attempt->username]) }}">
-                                        {{ $attempt->username }}
-                                    </a>
-                                @endif
-                            </td>
-                            <td>{{ $attempt->ip_address }}</td>
-                            <td>
-                                <time datetime="{{ $attempt->created_at }}">
-                                    {{ $attempt->created_at }}
-                                </time>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="5">No failed logins</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
-        {{ $attempts->links('partials.pagination') }}
-    </section>
+    @livewire('failed-login-search')
 @endsection
