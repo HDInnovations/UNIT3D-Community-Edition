312bcae3c3e9a464846cb4e0cfa2d9299ba5bc52
HDVinnie <hdinnovations@protonmail.com>
2023-06-17 07:22:30 +0000

    PHP Style Change (Laravel Pint CI)

--- app/Actions/Fortify/UpdateUserPassword.php
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -14,13 +14,13 @@ class UpdateUserPassword implements UpdatesUserPasswords
     /**
      * Validate and update the user's password.
      *
-     * @param  array<string, string>  $input
+     * @param array<string, string> $input
      */
     public function update(User $user, array $input): void
     {
         Validator::make($input, [
             'current_password' => ['required', 'string', 'current_password:web'],
-            'password' => $this->passwordRules(),
+            'password'         => $this->passwordRules(),
         ], [
             'current_password.current_password' => __('The provided password does not match your current password.'),
         ])->validateWithBag('updatePassword');
