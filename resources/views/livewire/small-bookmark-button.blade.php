6482a64c1fb01ad94b3807bb62d6f3d461797057
HDVinnie <hdinnovations@protonmail.com>
2023-07-04 13:35:08 -0400

    update: font awesome

--- resources/views/livewire/small-bookmark-button.blade.php
+++ resources/views/livewire/small-bookmark-button.blade.php
@@ -1,6 +1,6 @@
 @if($this->isBookmarked)
     <button wire:click="destroy({{ $torrent->id }})" class="form__standard-icon-button" title="Unbookmark">
-        <i class="{{ config('other.font-awesome') }} fa-bookmark"></i>
+        <i class="{{ config('other.font-awesome') }} fa-bookmark-slash"></i>
     </button>
 @else
     <button wire:click="store({{ $torrent->id }})" class="form__standard-icon-button" title="Bookmark">
--- resources/views/livewire/torrent-search.blade.php
+++ resources/views/livewire/torrent-search.blade.php
@@ -440,6 +440,45 @@
             </form>
         </div>
     </section>
+    @if(auth()->user()-username === 'HDVinnie')
+        @php
+            $trending = (new App\Services\Tmdb\Client\Trending());
+        @endphp
+        <section class="trending" style="max-height: 330px !important;">
+            <div class="scroller" style="padding-bottom: 10px;">
+                @forelse($trending as $trend)
+                    <div class="item mini backdrop mini_card">
+                        <div class="image_content">
+                            <a href="{{ route('torrents.similar', ['category_id' => $torrent->category_id, 'tmdb' => $recommendation->recommendation_movie_id ?? $recommendation->recommendation_tv_id]) }}">
+                                @isset($recommendation->poster)
+                                    <img
+                                            class="backdrop"
+                                            src="{{ tmdb_image('poster_big', $recommendation->poster) }}"
+                                    >
+                                @else
+                                    <div class="no_image_holder w300_and_h450 backdrop"></div>
+                                @endif
+                                <div style="display: flex; justify-content: space-between; margin-top: 8px; font-size: 12px;">
+                            <span>
+                                @isset($recommendation->release_date)
+                                    {{ substr($recommendation->release_date, 0, 4) }}
+                                @else($recommendation->first_air_date)
+                                    {{ substr($recommendation->first_air_date, 0, 4) }}
+                                @endif
+                            </span>
+                                    <span class="{{ rating_color($recommendation->vote_average) ?? 'text-white' }}">
+                                {{ ($recommendation->vote_average ?? 0) * 10 }}%
+                            </span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                @empty
+                    No Recommendations Found!
+                @endforelse
+            </div>
+        </section>
+    @endif
     <section class="panelV2 torrent-search__results">
         <header class="panel__header">
             <h2 class="panel__heading">{{ __('torrent.torrents') }}</h2>
