1b31fc4bac0eaf0c701424fcfb96b2d10d57f8cd
Roardom <roardom@protonmail.com>
2023-07-09 08:36:17 +0000

    add: buttons to copy torrent description/mediainfo/bdinfo

--- resources/views/torrent/partials/description.blade.php
+++ resources/views/torrent/partials/description.blade.php
@@ -1,8 +1,21 @@
 <section class="panelV2">
-    <h2 class="panel__heading">
-        <i class="{{ config("other.font-awesome") }} fa-sticky-note"></i>
-        {{ __('common.description') }}
-    </h2>
+    <header class="panel__header">
+        <h2 class="panel__heading">
+            <i class="{{ config("other.font-awesome") }} fa-sticky-note"></i>
+            {{ __('common.description') }}
+        </h2>
+        <div class="panel__actions">
+            <div class="panel__action">
+                <button
+                    class="form__button form__button--text"
+                    x-data
+                    x-on:click.stop="navigator.clipboard.writeText(atob('{{ base64_encode($torrent->description) }}'))"
+                >
+                    Copy
+                </button>
+            </div>
+        </div>
+    </header>
     <div class="panel__body bbcode-rendered">
         @joypixels($torrent->getDescriptionHtml())
     </div>
