e364fb6a5f955f0562a1123cbdb8532745bfde51
HDVinnie <hdinnovations@protonmail.com>
2023-07-11 18:28:32 +0000

    PHP Style Change (Laravel Pint CI)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,7 +10,8 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, AdditionalAssertions;
+    use AdditionalAssertions;
+    use CreatesApplication;
     use RefreshDatabase;
 
     /**
