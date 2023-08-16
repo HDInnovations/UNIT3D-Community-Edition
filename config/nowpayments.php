ec28a9dacbb7f2c9e6398cbc453efe2b1f947153
Jay Sizzla <aithercc_heaven@protonmail.com>
2023-08-16 15:05:37 +0200

    Fix sandboxurl

---
+++ config/nowpayments.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the Laravel NOWPayments package.
+ *
+ * (c) Prevail Ejimadu <prevailexcellent@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+return [
+
+    /**
+     * API Key From NOWPayments Dashboard
+     *
+     */
+    'apiKey' => getenv('NOWPAYMENTS_API_KEY'),
+
+    /**
+     * You enviroment can either be live or sandbox.
+     * Make sure to add the appropriate API key after changing the enviroment in .env
+     *
+     */
+    'env' => getenv('NOWPAYMENTS_ENV', 'sandbox'),
+
+    /**
+     * NOWPayments Live URL
+     *
+     */
+    'liveUrl' => "https://api.nowpayments.io/v1",
+
+    /**
+     * NOWPayments Sandbox URL
+     *
+     */
+    'sandboxUrl' => "https://api-sandbox.nowpayments.io/v1",
+
+    /**
+     * Your callback URL
+     *
+     */
+    'callbackUrl' => getenv('NOWPAYMENTS_CALLBACK_URL'),
+
+    /**
+     * Your URL Path
+     *
+     */
+    'path' => 'laravel-nowpayments',
+
+    /**
+     * You can add your custom middleware to access the dashboard here
+     *
+     */
+    'middleware' => null, // "Authorise::class",
+
+    /**
+     * Your Nowpayment email here
+     *
+     */
+    'email' => getenv('NOWPAYMENTS_EMAIL'),
+    
+    /**
+     * Your Nowpayment password here
+     *
+     */
+    'password' =>  getenv('NOWPAYMENTS_PASSWORD'),
+
+];
