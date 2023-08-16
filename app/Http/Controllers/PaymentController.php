057ec3cdfed25c26ad84261854861a0fb50b9057
HDVinnie <hdinnovations@protonmail.com>
2023-08-16 20:30:25 +0000

    PHP Style Change (Laravel Pint CI)

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -15,7 +15,6 @@ namespace App\Http\Controllers;
 
 use App\Models\DonationItem;
 use App\Models\DonationTransaction;
-use App\Http\Controllers\Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use PrevailExcel\Nowpayments\Facades\Nowpayments;
