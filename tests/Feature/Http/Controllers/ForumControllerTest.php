b2cccb10c1717743c89342a814bc71ba07ca6cfb
HDVinnie <hdinnovations@protonmail.com>
2023-08-19 11:44:32 -0400

    update: ForumControllerTest

--- tests/Feature/Http/Controllers/ForumControllerTest.php
+++ tests/Feature/Http/Controllers/ForumControllerTest.php
@@ -36,7 +36,7 @@ test('show returns an ok response', function (): void {
     $user = User::factory()->create();
 
     $forum = Forum::factory()->create([
-        'parent_id'               => 0, // This Forum does not have a parent, which makes it a proper Forum and not a "Forum Category".
+        'parent_id'               => null, // This Forum does not have a parent, which makes it a proper Forum and not a "Forum Category".
         'last_post_user_id'       => $user->id,
         'last_post_user_username' => $user->username,
     ]);
