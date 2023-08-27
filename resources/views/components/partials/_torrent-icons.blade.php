4c81724e2e235b9f545ecc7ae0d142d3aa710e77
Jay <73784120+Obi-Wana@users.noreply.github.com>
2023-08-27 19:45:04 +0200

    Add Special Freeleech Icon for donors

--- resources/views/components/partials/_torrent-icons.blade.php
+++ resources/views/components/partials/_torrent-icons.blade.php
@@ -72,7 +72,7 @@
     @endif
 
     @php
-        $alwaysFreeleech = $personalFreeleech || $torrent->freeleech_tokens_exists || auth()->user()->group->is_freeleech || config('other.freeleech')
+        $alwaysFreeleech = $personalFreeleech || $torrent->freeleech_tokens_exists || auth()->user()->group->is_freeleech || auth()->user()->is_donor || config('other.freeleech')
     @endphp
 
     @if ($alwaysFreeleech || $torrent->free)
@@ -90,6 +90,7 @@
                         [
                             __('torrent.personal-freeleech') => $personalFreeleech,
                             __('torrent.freeleech-token') => $torrent->freeleech_tokens_exists,
+                            __('torrent.special-freeleech') => auth()->user()->is_donor,
                             __('torrent.special-freeleech') => auth()->user()->group->is_freeleech,
                             __('torrent.global-freeleech') => config('other.freeleech'),
                             $torrent->free . '% ' . __('common.free') . ($torrent->fl_until !== null ? ' (expires ' . $torrent->fl_until->diffForHumans() . ')' : '') => $torrent->free > 0,
