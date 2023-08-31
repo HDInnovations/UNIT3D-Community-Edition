b0b8cbf8a0812e8a34d958d1f0be81813646af4d
HDVinnie <hdinnovations@protonmail.com>
2023-08-31 09:06:21 +0000

    PHP Style Change (Laravel Pint CI)

--- app/Http/Controllers/TorrentDownloadController.php
+++ app/Http/Controllers/TorrentDownloadController.php
@@ -45,6 +45,7 @@ class TorrentDownloadController extends Controller
         }
         $torrent = Torrent::withoutGlobalScope(ApprovedScope::class)->findOrFail($id);
         $hasHistory = $user->history()->where([['torrent_id', '=', $torrent->id], ['seeder', '=', 1]])->exists();
+
         // User's ratio is too low
         if ($user->ratio < config('other.ratio') && ! ($torrent->user_id === $user->id || $hasHistory)) {
             return to_route('torrents.show', ['id' => $torrent->id])
