8a1ab38b9daa48e004e31327236cda6baf933575
Jay Sizzla <aithercc_heaven@protonmail.com>
2023-09-15 15:06:58 +0200

    Fix StyleCI

--- app/Http/Livewire/FailedLoginSearch.php
+++ app/Http/Livewire/FailedLoginSearch.php
@@ -47,7 +47,7 @@ class FailedLoginSearch extends Component
         $this->emit('paginationChanged');
     }
 
-    final public function getFailedLoginsTop10IpProperty(): \Illuminate\Database\Eloquent\Collection
+    final public function getFailedLoginsTop10IpProperty(): \Illuminate\Support\Collection
     {
         $current = Carbon::now();
 
