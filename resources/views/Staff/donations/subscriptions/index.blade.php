2fb122ab2a6c031a99571a88b8f7601f6a6bd7b6
Jay Sizzla <aithercc_heaven@protonmail.com>
2023-10-12 14:57:11 +0200

    Remove bootstrap style

--- resources/views/Staff/donations/subscriptions/index.blade.php
+++ resources/views/Staff/donations/subscriptions/index.blade.php
@@ -22,202 +22,184 @@
 @section('page', 'page__donations-subscriptions--index')
 
 @section('main')
+    <!-- Upcoming Users -->
     <section class="panelV2">
-        <header class="panel__header">
-            <h2 class="panel__heading">Donors</h2>
-            <div class="panel__actions">
-                <div class="panel__action">
-                    <a href="{{ route('staff.donation_subscriptions.create') }}" class="form__button form__button--text">
-                        {{ __('common.add') }} VIP
-                    </a>
-                </div>
-            </div>
-        </header>
-        <div class="panel__body">
-            <!-- Upcoming Users -->
-            <h3>Upcoming</h3>
-            <div class="table-responsive">
-                <table class="table table-condensed table-striped table-bordered">
-                    <thead>
+        <h2 class="panel__heading">Upcoming</h2>
+        <div class="data-table-wrapper">
+            <table class="data-table">
+                <thead>
+                    <tr>
+                        <th>{{ __('common.user') }}</th>
+                        <th>{{ __('common.group') }}</th>
+                        <th>VIP start</th>
+                        <th>VIP end</th>
+                        <th width="10%">{{ __('common.action') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($subscriptions_upcoming as $subscription)
                         <tr>
-                            <th>{{ __('common.user') }}</th>
-                            <th>{{ __('common.group') }}</th>
-                            <th>VIP start</th>
-                            <th>VIP end</th>
-                            <th width="10%">{{ __('common.action') }}</th>
+                            <td>
+                                <a href="{{ route('users.show', ['user' => $subscription->user->username]) }}">
+                                    {{ $subscription->user->username }}
+                                </a>
+                            </td>
+
+                            <td>
+                                <span class="badge-user text-bold"
+                                    style="color:{{ $subscription->user->group->color }}; background-image:{{ $subscription->user->group->effect }};">
+                                    <i class="{{ $subscription->user->group->icon }}" data-toggle="tooltip"
+                                        data-original-title="{{ $subscription->user->group->name }}"></i>
+                                    {{ $subscription->user->group->name }}
+                                </span>
+                            </td>
+
+                            <td>
+                                {{ $subscription->start_at->format('Y-m-d') }}
+                            </td>
+
+                            <td>
+                                {{ $subscription->end_at->format('Y-m-d') }}
+                            </td>
+
+                            <td>
+                                <a
+                                    class="form__button form__button--text"
+                                    href="{{ route('staff.donation_subscriptions.edit', ['donationSubscription' => $subscription->id]) }}"
+                                >
+                                    {{ __('common.edit') }}
+                                </a>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($subscriptions_upcoming as $subscription)
-                            <tr>
-                                <td>
-                                    <a href="{{ route('users.show', ['user' => $subscription->user->username]) }}">
-                                        {{ $subscription->user->username }}
-                                    </a>
-                                </td>
-
-                                <td>
-                                    <span class="badge-user text-bold"
-                                        style="color:{{ $subscription->user->group->color }}; background-image:{{ $subscription->user->group->effect }};">
-                                        <i class="{{ $subscription->user->group->icon }}" data-toggle="tooltip"
-                                            data-original-title="{{ $subscription->user->group->name }}"></i>
-                                        {{ $subscription->user->group->name }}
-                                    </span>
-                                </td>
-
-                                <td>
-                                    {{ $subscription->start_at->format('Y-m-d') }}
-                                </td>
-
-                                <td>
-                                    {{ $subscription->end_at->format('Y-m-d') }}
-                                </td>
-
-                                <td>
-                                    <a
-                                        class="form__button form__button--text"
-                                        href="{{ route('staff.donation_subscriptions.edit', ['donationSubscription' => $subscription->id]) }}"
-                                    >
-                                        {{ __('common.edit') }}
-                                    </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                @if (! $subscriptions_upcoming->count())
-                    <div class="margin-10">
-                        {{ __('common.no-result') }}
-                    </div>
-                @endif
-                <br>
-                <div class="text-center">
-                    {{ $subscriptions_upcoming->links() }}
+                    @endforeach
+                </tbody>
+            </table>
+            @if (! $subscriptions_upcoming->count())
+                <div class="margin-10">
+                    {{ __('common.no-result') }}
                 </div>
-            </div>
+            @endif
+            {{ $subscriptions_inactive->links() }}
+        </div>
+    </section>
 
-            <!-- Active Users -->
-            <h3>Active</h3>
-            <div class="table-responsive">
-                <table class="table table-condensed table-striped table-bordered">
-                    <thead>
+    <!-- Active Users -->
+    <section class="panelV2">
+        <h2 class="panel__heading">Active</h2>
+        <div class="data-table-wrapper">
+            <table class="data-table">
+                <thead>
+                    <tr>
+                        <th>{{ __('common.user') }}</th>
+                        <th>{{ __('common.group') }}</th>
+                        <th>VIP Start</th>
+                        <th>VIP End</th>
+                        <th width="10%">{{ __('common.action') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($subscriptions_active as $subscription)
                         <tr>
-                            <th>{{ __('common.user') }}</th>
-                            <th>{{ __('common.group') }}</th>
-                            <th>VIP Start</th>
-                            <th>VIP End</th>
-                            <th width="10%">{{ __('common.action') }}</th>
+                            <td>
+                                <a href="{{ route('users.show', ['user' => $subscription->user->username]) }}">
+                                    {{ $subscription->user->username }}
+                                </a>
+                                @if ($subscription->user->is_donor && $subscription->end_at == $curdate->toDateString())
+                                    <i class="fas fa-exclamation-circle text-orange" aria-hidden="true" data-toggle="tooltip" title="" data-original-title="Demoted today!"></i>
+                                @elseif ($subscription->user->is_donor === false)
+                                    <i class="fas fa-exclamation-circle text-orange" aria-hidden="true" data-toggle="tooltip" title="" data-original-title="Shouln't this User be a VIP?"></i>
+                                @endif
+                            </td>
+
+                            <td>
+                                <span class="badge-user text-bold"
+                                    style="color:{{ $subscription->user->group->color }}; background-image:{{ $subscription->user->group->effect }};">
+                                    <i class="{{ $subscription->user->group->icon }}" data-toggle="tooltip"
+                                        data-original-title="{{ $subscription->user->group->name }}"></i>
+                                    {{ $subscription->user->group->name }}
+                                </span>
+                            </td>
+
+                            <td>
+                                {{ $subscription->start_at->format('Y-m-d') }}
+                            </td>
+
+                            <td>
+                                {{ $subscription->end_at->format('Y-m-d') }}
+                            </td>
+
+                            <td>
+                                <a
+                                    class="form__button form__button--text"
+                                    href="{{ route('staff.donations.subscriptions.edit', ['id' => $subscription->id]) }}"
+                                >
+                                    {{ __('common.edit') }}
+                                </a>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($subscriptions_active as $subscription)
-                            <tr>
-                                <td>
-                                    <a href="{{ route('users.show', ['user' => $subscription->user->username]) }}">
-                                        {{ $subscription->user->username }}
-                                    </a>
-                                    @if ($subscription->user->is_donor && $subscription->end_at == $curdate->toDateString())
-                                        <i class="fas fa-exclamation-circle text-orange" aria-hidden="true" data-toggle="tooltip" title="" data-original-title="Demoted today!"></i>
-                                    @elseif ($subscription->user->is_donor === false)
-                                        <i class="fas fa-exclamation-circle text-orange" aria-hidden="true" data-toggle="tooltip" title="" data-original-title="Shouln't this User be a VIP?"></i>
-                                    @endif
-                                </td>
-
-                                <td>
-                                    <span class="badge-user text-bold"
-                                        style="color:{{ $subscription->user->group->color }}; background-image:{{ $subscription->user->group->effect }};">
-                                        <i class="{{ $subscription->user->group->icon }}" data-toggle="tooltip"
-                                            data-original-title="{{ $subscription->user->group->name }}"></i>
-                                        {{ $subscription->user->group->name }}
-                                    </span>
-                                </td>
-
-                                <td>
-                                    {{ $subscription->start_at->format('Y-m-d') }}
-                                </td>
-
-                                <td>
-                                    {{ $subscription->end_at->format('Y-m-d') }}
-                                </td>
-
-                                <td>
-                                    <a
-                                        class="form__button form__button--text"
-                                        href="{{ route('staff.donations.subscriptions.edit', ['id' => $subscription->id]) }}"
-                                    >
-                                        {{ __('common.edit') }}
-                                    </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                @if (! $subscriptions_active->count())
-                    <div class="margin-10">
-                        {{ __('common.no-result') }}
-                    </div>
-                @endif
-                <br>
-                <div class="text-center">
-                    {{ $subscriptions_active->links() }}
+                    @endforeach
+                </tbody>
+            </table>
+            @if (! $subscriptions_active->count())
+                <div class="margin-10">
+                    {{ __('common.no-result') }}
                 </div>
-            </div>
+            @endif
+            {{ $subscriptions_inactive->links() }}
+        </div>
+    </section>
 
-            <!-- Old VIP User-Records -->
-            <h3>Previous VIP User Records</h3>
-            <div class="table-responsive">
-                <table class="table table-condensed table-striped table-bordered">
-                    <thead>
+    <!-- Old VIP User-Records -->
+    <section class="panelV2">
+        <h2 class="panel__heading">Previous VIP User Records</h2>
+        <div class="data-table-wrapper">
+            <table class="data-table">
+                <thead>
+                    <tr>
+                        <th>{{ __('common.user') }}</th>
+                        <th>{{ __('common.group') }}</th>
+                        <th>VIP Start</th>
+                        <th>VIP End</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($subscriptions_inactive as $subscription)
                         <tr>
-                            <th>{{ __('common.user') }}</th>
-                            <th>{{ __('common.group') }}</th>
-                            <th>VIP Start</th>
-                            <th>VIP End</th>
-                            <th width="10%">{{ __('common.action') }}</th>
+                            <td>
+                                <a href="{{ route('users.show', ['user' => $subscription->user->username]) }}">
+                                    {{ $subscription->user->username }}
+                                </a>
+                                @if ($subscription->user->is_donor && !in_array($subscription->user_id, $subscriptions_active_arr) == true)
+                                    <i class="fas fa-exclamation-circle text-orange" aria-hidden="true" data-toggle="tooltip" title="" data-original-title="User is still VIP but subscription has ended!"></i>
+                                @endif
+                            </td>
+
+                            <td>
+                                <span class="badge-user text-bold"
+                                    style="color:{{ $subscription->user->group->color }}; background-image:{{ $subscription->user->group->effect }};">
+                                    <i class="{{ $subscription->user->group->icon }}" data-toggle="tooltip"
+                                        data-original-title="{{ $subscription->user->group->name }}"></i>
+                                    {{ $subscription->user->group->name }}
+                                </span>
+                            </td>
+
+                            <td>
+                                {{ $subscription->start_at->format('Y-m-d') }}
+                            </td>
+
+                            <td>
+                                {{ $subscription->end_at->format('Y-m-d') }}
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($subscriptions_inactive as $subscription)
-                            <tr>
-                                <td>
-                                    <a href="{{ route('users.show', ['user' => $subscription->user->username]) }}">
-                                        {{ $subscription->user->username }}
-                                    </a>
-                                    @if ($subscription->user->is_donor && !in_array($subscription->user_id, $subscriptions_active_arr) == true)
-                                        <i class="fas fa-exclamation-circle text-orange" aria-hidden="true" data-toggle="tooltip" title="" data-original-title="User is still VIP but subscription has ended!"></i>
-                                    @endif
-                                </td>
-
-                                <td>
-                                    <span class="badge-user text-bold"
-                                        style="color:{{ $subscription->user->group->color }}; background-image:{{ $subscription->user->group->effect }};">
-                                        <i class="{{ $subscription->user->group->icon }}" data-toggle="tooltip"
-                                            data-original-title="{{ $subscription->user->group->name }}"></i>
-                                        {{ $subscription->user->group->name }}
-                                    </span>
-                                </td>
-
-                                <td>
-                                    {{ $subscription->start_at->format('Y-m-d') }}
-                                </td>
-
-                                <td>
-                                    {{ $subscription->end_at->format('Y-m-d') }}
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                @if (! $subscriptions_inactive->count())
-                    <div class="margin-10">
-                        {{ __('common.no-result') }}
-                    </div>
-                @endif
-                <br>
-                <div class="text-center">
-                    {{ $subscriptions_inactive->links() }}
+                    @endforeach
+                </tbody>
+            </table>
+            @if (! $subscriptions_inactive->count())
+                <div class="margin-10">
+                    {{ __('common.no-result') }}
                 </div>
-            </div>
+            @endif
+            {{ $subscriptions_inactive->links() }}
         </div>
     </section>
 @endsection
