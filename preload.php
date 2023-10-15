979c0fe25d24f8148f2c20c9ed33f589f1e00598
HDVinnie <hdinnovations@protonmail.com>
2023-10-15 03:01:22 -0400

    optimize: preloader

--- preload.php
+++ preload.php
@@ -109,6 +109,34 @@ class Preloader
 
 (new Preloader())
     ->paths(
-        __DIR__.'/app',
+        __DIR__ . '/vendor/laravel',
+        __DIR__ . '/app/Http/Controllers/AnnounceController.php',
+        __DIR__ . '/app/Http/Controllers/RssController.php',
+        __DIR__ . '/app/Http/Controllers/API/TorrentController.php',
+    )
+    ->ignore(
+        'Laravel\Telescope',
+        'Laravel\Tinker',
+        'Illuminate\Queue',
+        'Illuminate\Contracts\Queue',
+        'Illuminate\View',
+        'Illuminate\Contracts\View',
+        'Illuminate\Foundation\Console',
+        'Illuminate\Notification',
+        'Illuminate\Contracts\Notifications',
+        'Illuminate\Bus',
+        'Illuminate\Session',
+        'Illuminate\Contracts\Session',
+        'Illuminate\Console',
+        'Illuminate\Testing',
+        'Illuminate\Http\Testing',
+        'Illuminate\Support\Testing',
+        'Illuminate\Cookie',
+        'Illuminate\Contracts\Cookie',
+        'Illuminate\Broadcasting',
+        'Illuminate\Contracts\Broadcasting',
+        'Illuminate\Mail',
+        'Illuminate\Carbon',
+        'Illuminate\Contracts\Mail',
     )
     ->load();
