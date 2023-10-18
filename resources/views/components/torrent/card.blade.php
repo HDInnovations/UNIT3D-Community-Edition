bc18aa8706601beb1bb966fce76c4e7edd3dafd9
Roardom <roardom@protonmail.com>
2023-10-18 09:56:34 +0000

    fix: torrent card posters for other meta

--- resources/views/components/torrent/card.blade.php
+++ resources/views/components/torrent/card.blade.php
@@ -80,8 +80,11 @@
                         @case ($torrent->category->game_meta && isset($torrent->meta) && $meta->cover->image_id && $meta->name)
                             src="https://images.igdb.com/igdb/image/upload/t_cover_big/{{ $torrent->meta->cover->image_id }}.jpg"
                             @break
-                        @case ($torrent->category->no_meta || $torrent->category->music_meta)
+                        @case ($torrent->category->music_meta)
                             src="https://via.placeholder.com/160x240"
+                            @break
+                        @case ($torrent->category->no_meta && file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg'))
+                            src="{{ url('files/img/torrent-cover_'.$torrent->id.'.jpg') }}"
                             @break
                     @endswitch
                     alt="{{ __('torrent.poster') }}"
