5ed2deb10a994c0d347095bd12661c328b45dc30
Shift <shift@laravelshift.com>
2023-11-26 01:19:11 +0000

    Use ES modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
-window._ = require('lodash');
+import _ from 'lodash';
+window._ = _;
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,7 +7,8 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+import axios from 'axios';
+window.axios = axios;
 window.axios.defaults.headers.common = {
     'X-Requested-With': 'XMLHttpRequest',
     'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
@@ -45,4 +47,5 @@ if (document.getElementById('vue')) {
  * NPM Packages
  */
 // Sweet Alert
-window.Swal = require('sweetalert2');
+import Swal from 'sweetalert2';
+window.Swal = Swal;
