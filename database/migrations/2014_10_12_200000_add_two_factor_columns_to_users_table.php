dfb398763b2119ca423cea7dc0aa1ec9e341e524
HDVinnie <hdinnovations@protonmail.com>
2023-11-25 20:05:42 -0500

    fix: 2014_10_12_200000_add_two_factor_columns_to_users_table.php

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -20,11 +20,9 @@ return new class () extends Migration {
                 ->after('two_factor_secret')
                 ->nullable();
 
-            if (Fortify::confirmsTwoFactorAuthentication()) {
-                $table->timestamp('two_factor_confirmed_at')
-                    ->after('two_factor_recovery_codes')
-                    ->nullable();
-            }
+            $table->timestamp('two_factor_confirmed_at')
+                ->after('two_factor_recovery_codes')
+                ->nullable();
         });
     }
 
