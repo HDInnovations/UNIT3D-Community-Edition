ba298f0fa4da2dea757c4b114182ed45d5571d4e
Yswag <91155348+Yswag@users.noreply.github.com>
2024-01-04 02:02:50 +0800

    update: Traditional Chinese Translations

---
+++ lang/zh-TW/comment.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'added'          => '你的評論已送出!',
+    'deleted'        => '評論已刪除.',
+    'edited'         => '評論已編輯.',
+    'rights-revoked' => '你的評論權限已被拔除!',
+    'slow-down'      => '請稍後 - 太多評論了!',
+    'torrent-status' => '種子被審核後才可以發表評論.',
+];
---
+++ lang/zh-TW/playlist.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'about'             => '在這裡你會找到用戶提供的播放清單，裏面包含了他們喜歡的內容!',
+    'attached-success'  => '此種子已成功加入你的播放清單',
+    'add-to-playlist'   => '新增種子到播放清單',
+    'add-torrent'       => '新增種子',
+    'added-by'          => 'By:',
+    'cover'             => '選擇一張封面',
+    'create'            => '建立新的播放清單',
+    'delete-playlist'   => '刪除播放清單',
+    'deleted'           => '播放清單已刪除!',
+    'detached-success'  => '此種子已成功從播放清單中移除',
+    'desc'              => '播放清單描述',
+    'download-all'      => '下載播放清單中的所有種子',
+    'edit-playlist'     => '編輯播放清單',
+    'list-by'           => 'A list made by:',
+    'list-about'        => 'A list about:',
+    'is-private'        => '設為私人播放清單?',
+    'playlists'         => '播放清單',
+    'private-error'     => '這是一個私人播放清單! 你無法察看其他用戶的私人播放清單!',
+    'published-success' => '你的播放清單已建立!',
+    'title'             => '種子',
+    'titles'            => '種子',
+    'update-success'    => '你的播放清單已成功更新!',
+];
