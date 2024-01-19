c681c0514c4f4d93c3d45896d8c57fedeb161465
HDVinnie <hdinnovations@protonmail.com>
2024-01-19 02:40:40 -0500

    update: overides

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -63,7 +63,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting(): void
     {
-        RateLimiter::for('login-get', fn (Request $request) => Limit::perMinute(3)->by($request->ip()));
+        RateLimiter::for('fortify-overide', fn (Request $request) => Limit::perMinute(3)->by($request->ip()));
         RateLimiter::for('web', fn (Request $request): Limit => $request->user()
             ? Limit::perMinute(30)->by($request->user()->id)
             : Limit::perMinute(5)->by($request->ip()));
