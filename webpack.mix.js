e6677a269777f287aefbce61af5ce4b879b39f6b
Roardom <roardom@protonmail.com>
2024-01-21 12:40:48 +0000

    refactor: move scss files to fit 7-1 sass architecture conventions

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,34 +19,34 @@ mix.options({
      * Sourced asset dependencies via node_modules and JS bootstrapping
      */
     .js('resources/js/app.js', 'public/js').vue({ version: 2 })
-    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/main.scss', 'public/css/app.css')
     .purgeCss()
 
     /*
      * Themes
      */
-    .sass('resources/sass/themes/light.scss', 'public/css/themes/light.css')
-    .sass('resources/sass/themes/galactic.scss', 'public/css/themes/galactic.css')
-    .sass('resources/sass/themes/dark-blue.scss', 'public/css/themes/dark-blue.css')
-    .sass('resources/sass/themes/dark-green.scss', 'public/css/themes/dark-green.css')
-    .sass('resources/sass/themes/dark-pink.scss', 'public/css/themes/dark-pink.css')
-    .sass('resources/sass/themes/dark-purple.scss', 'public/css/themes/dark-purple.css')
-    .sass('resources/sass/themes/dark-red.scss', 'public/css/themes/dark-red.css')
-    .sass('resources/sass/themes/dark-teal.scss', 'public/css/themes/dark-teal.css')
-    .sass('resources/sass/themes/dark-yellow.scss', 'public/css/themes/dark-yellow.css')
-    .sass('resources/sass/themes/cosmic-void.scss', 'public/css/themes/cosmic-void.css')
-    .sass('resources/sass/themes/nord.scss', 'public/css/themes/nord.css')
-    .sass('resources/sass/themes/revel.scss', 'public/css/themes/revel.css')
-    .sass('resources/sass/themes/material-design-v3-light.scss', 'public/css/themes/material-design-v3-light.css')
-    .sass('resources/sass/themes/material-design-v3-dark.scss', 'public/css/themes/material-design-v3-dark.css')
-    .sass('resources/sass/themes/material-design-v3-amoled.scss', 'public/css/themes/material-design-v3-amoled.css')
+    .sass('resources/sass/themes/_light.scss', 'public/css/themes/light.css')
+    .sass('resources/sass/themes/_galactic.scss', 'public/css/themes/galactic.css')
+    .sass('resources/sass/themes/_dark-blue.scss', 'public/css/themes/dark-blue.css')
+    .sass('resources/sass/themes/_dark-green.scss', 'public/css/themes/dark-green.css')
+    .sass('resources/sass/themes/_dark-pink.scss', 'public/css/themes/dark-pink.css')
+    .sass('resources/sass/themes/_dark-purple.scss', 'public/css/themes/dark-purple.css')
+    .sass('resources/sass/themes/_dark-red.scss', 'public/css/themes/dark-red.css')
+    .sass('resources/sass/themes/_dark-teal.scss', 'public/css/themes/dark-teal.css')
+    .sass('resources/sass/themes/_dark-yellow.scss', 'public/css/themes/dark-yellow.css')
+    .sass('resources/sass/themes/_cosmic-void.scss', 'public/css/themes/cosmic-void.css')
+    .sass('resources/sass/themes/_nord.scss', 'public/css/themes/nord.css')
+    .sass('resources/sass/themes/_revel.scss', 'public/css/themes/revel.css')
+    .sass('resources/sass/themes/_material-design-v3-light.scss', 'public/css/themes/material-design-v3-light.css')
+    .sass('resources/sass/themes/_material-design-v3-dark.scss', 'public/css/themes/material-design-v3-dark.css')
+    .sass('resources/sass/themes/_material-design-v3-amoled.scss', 'public/css/themes/material-design-v3-amoled.css')
 
     /*
      * Auth styles
      *
      * We compile each of these separately since they should only be loaded with the certain views
      */
-    .sass('resources/sass/main/login.scss', 'public/css/main/login.css')
+    .sass('resources/sass/pages/_auth.scss', 'public/css/main/login.css')
 
     /*
      * Here we take all these scripts and compile them into a single 'unit3d.js' file that will be loaded after 'app.js'
