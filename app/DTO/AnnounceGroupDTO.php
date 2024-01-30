9ad5bed8e269e50d73e390ef6c63cd523a2804da
Roardom <roardom@protonmail.com>
2024-01-30 14:44:34 +0000

    update: use queued announce updates
    There were very minor edge cases with the previous system that had issues when users paused a torrent immediately after they completed it, causing data to be duplicated. We now opt to use DTOs to transfer the data into the job queue instead of models to prevent the extra database queries caused by passing models into a job queue.

---
+++ app/DTO/AnnounceGroupDTO.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Roardom <roardom@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+namespace App\DTO;
+
+readonly class AnnounceGroupDTO
+{
+    public function __construct(
+        public bool $isFreeleech,
+        public bool $isDoubleUpload,
+        public bool $isImmune,
+    ) {
+    }
+}
