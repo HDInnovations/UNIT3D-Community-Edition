3547c242179732648f69345a05fb1950d04b7ff5
HDVinnie <hdinnovations@protonmail.com>
2024-03-04 00:57:44 -0500

    revert: secure-headers.php

--- config/secure-headers.php
+++ config/secure-headers.php
@@ -120,7 +120,7 @@ return [
      */
 
     'hsts' => [
-        'enable' => false,
+        'enable' => true,
 
         'max-age' => 31536000,
 
@@ -449,7 +449,7 @@ return [
      */
 
     'csp' => [
-        'enable' => env('CSP_ENABLED', false),
+        'enable' => env('CSP_ENABLED', true),
 
         // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
         'report-only' => env('CSP_REPORT_ONLY', false),
