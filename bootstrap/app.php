9d05536910257684d0aca7ce86f0cc74b63af641
Shift <shift@laravelshift.com>
2024-03-05 04:01:53 +0000

    Re-register exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -68,5 +68,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->dontReport([
+            \Illuminate\Queue\MaxAttemptsExceededException::class,
+        ]);
+
+        $exceptions->reportable(function (Throwable $e): void {
+        });
     })->create();
