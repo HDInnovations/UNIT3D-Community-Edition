72a3370a94efdd036934cc8241a0265a2c14d172
Shift <shift@laravelshift.com>
2024-03-05 04:02:42 +0000

    Remove `CreatesApplication` testing trait

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -8,18 +8,4 @@ use Illuminate\Support\Facades\Hash;
 
 trait CreatesApplication
 {
-    /**
-     * Creates the application.
-     */
-    public function createApplication(): Application
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        // Set the bcrypt hashing rounds to just 4 for testing
-        Hash::setRounds(4);
-
-        return $app;
-    }
 }
