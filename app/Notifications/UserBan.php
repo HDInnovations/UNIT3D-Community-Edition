84fa1741cec870f40898f03fa5f143b11dbfa6fa
EkoNesLeg <155665062+EkoNesLeg@users.noreply.github.com>
2024-03-05 01:34:36 +0000

    Update UserBan.php

--- app/Notifications/UserBan.php
+++ app/Notifications/UserBan.php
@@ -33,12 +33,19 @@ class UserBan extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
-        $chatdUrl = config('unit3d.chat-link-url');
+        $chatdUrl = config('unit3d.support-link-url');
 
         return (new MailMessage())
-            ->greeting('You have been banned 😭')
-            ->line('You have been banned from '.config('other.title').' for '.$this->ban->ban_reason)
-            ->action('Need Support?', $chatdUrl)
-            ->line('Thank you for using 🚀'.config('other.title'));
+            ->greeting('Notice of Account Suspension')
+            ->line(
+                'Your account on '.config(
+                    'other.title'
+                ).' has been suspended due to the following reason: '.$this->ban->ban_reason.'.'
+            )
+            ->action('Request Support', $chatdUrl)
+            ->line(
+                'We value every member of our community and understand that misunderstandings can occur. We encourage you to reach out for support if you believe this suspension to be in error or if you wish to discuss the matter further.'
+            )
+            ->line('Thank you for your understanding and cooperation.');
     }
 }
