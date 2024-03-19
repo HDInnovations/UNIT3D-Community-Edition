4cb85788a980c23f3667bf0a4131539fe0bba44e
HDVinnie <hdinnovations@protonmail.com>
2024-03-19 18:01:57 -0400

    chore: license

--- database/migrations/2019_07_30_210850_create_cast_table.php
+++ database/migrations/2019_07_30_210850_create_cast_table.php
@@ -1,4 +1,5 @@
-<?php/**
+<?php
+/**
  * NOTICE OF LICENSE.
  *
  * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
@@ -8,7 +9,9 @@
  *
  * @author     HDVinnie <hdinnovations@protonmail.com>
  * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
- */use Illuminate\Database\Migrations\Migration;
+ */
+
+use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
