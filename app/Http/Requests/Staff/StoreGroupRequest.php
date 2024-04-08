a1b03edf1e3e0d2e2d157e1d361eca1590c3e30c
maxwgod <30300032+maxwbot@users.noreply.github.com>
2024-04-08 17:08:33 -0300

    (Update) StoreGroupRequest.php
    Resolved problem where not selecting the checkbox resulted in an empty or unfilled field error.

--- app/Http/Requests/Staff/StoreGroupRequest.php
+++ app/Http/Requests/Staff/StoreGroupRequest.php
@@ -32,110 +32,146 @@ class StoreGroupRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array<\Illuminate\Contracts\Validation\Rule|string>|string>
      */
     public function rules(): array
-    {
-        return [
-            'name' => [
-                'required',
-                'string',
-                'unique:groups',
-            ],
-            'position' => [
-                'required',
-                'integer',
-            ],
-            'level' => [
-                'required',
-                'integer',
-            ],
-            'download_slots' => [
-                'nullable',
-                'integer',
-            ],
-            'description' => [
-                'nullable',
-            ],
-            'color' => [
-                'required',
-            ],
-            'icon' => [
-                'required',
-            ],
-            'effect' => [
-                'sometimes',
-            ],
-            'is_internal' => [
-                'required',
-                'boolean',
-            ],
-            'is_editor' => [
-                'required',
-                'boolean',
-            ],
-            'is_modo' => [
-                'required',
-                'boolean',
-            ],
-            'is_admin' => [
-                'required',
-                'boolean',
-            ],
-            'is_owner' => [
-                'required',
-                'boolean',
-            ],
-            'is_trusted' => [
-                'required',
-                'boolean',
-            ],
-            'is_immune' => [
-                'required',
-                'boolean',
-            ],
-            'is_freeleech' => [
-                'required',
-                'boolean',
-            ],
-            'is_double_upload' => [
-                'required',
-                'boolean',
-            ],
-            'is_incognito' => [
-                'required',
-                'boolean',
-            ],
-            'can_upload' => [
-                'required',
-                'boolean',
-            ],
-            'autogroup' => [
-                'required',
-                'boolean',
-            ],
-            'min_uploaded' => [
-                'sometimes',
-                'integer',
-                'min:0',
-            ],
-            'min_ratio' => [
-                'sometimes',
-                'min:0',
-                'max:99.99',
-            ],
-            'min_age' => [
-                'sometimes',
-                'integer',
-                'min:0',
-            ],
-            'min_avg_seedtime' => [
-                'sometimes',
-                'integer',
-                'min:0',
-            ],
-            'min_seedsize' => [
-                'sometimes',
-                'integer',
-                'min:0',
-            ],
-        ];
-    }
+	{
+		$rules = [
+			'name' => [
+				'required',
+				'string',
+				'unique:groups',
+			],
+			'position' => [
+				'required',
+				'integer',
+			],
+			'level' => [
+				'required',
+				'integer',
+			],
+			'download_slots' => [
+				'nullable',
+				'integer',
+			],
+			'description' => [
+				'nullable',
+			],
+			'color' => [
+				'required',
+			],
+			'icon' => [
+				'required',
+			],
+			'effect' => [
+				'nullable',
+			],
+			'is_internal' => [
+				'required',
+				'boolean',
+			],
+			'is_editor' => [
+				'required',
+				'boolean',
+			],
+			'is_modo' => [
+				'required',
+				'boolean',
+			],
+			'is_admin' => [
+				'required',
+				'boolean',
+			],
+			'is_owner' => [
+				'required',
+				'boolean',
+			],
+			'is_trusted' => [
+				'required',
+				'boolean',
+			],
+			'is_immune' => [
+				'required',
+				'boolean',
+			],
+			'is_freeleech' => [
+				'required',
+				'boolean',
+			],
+			'is_double_upload' => [
+				'required',
+				'boolean',
+			],
+			'is_incognito' => [
+				'required',
+				'boolean',
+			],
+			'can_upload' => [
+				'required',
+				'boolean',
+			],
+			'autogroup' => [
+				'required',
+				'boolean',
+			],
+		];
+		
+		if ($this->input('autogroup')) {
+			$rules += [
+				'min_uploaded' => [
+					'required',
+					'integer',
+					'min:0',
+				],
+				'min_ratio' => [
+					'required',
+					'numeric',
+					'min:0',
+					'max:99.99',
+				],
+				'min_age' => [
+					'required',
+					'integer',
+					'min:0',
+				],
+				'min_avg_seedtime' => [
+					'required',
+					'integer',
+					'min:0',
+				],
+				'min_seedsize' => [
+					'required',
+					'integer',
+					'min:0',
+				],
+			];
+		} else {
+			$rules += [
+				'min_uploaded' => [
+					'nullable',
+					'integer',
+					'min:0',
+				],
+				'min_ratio' => [
+					'nullable',
+					'numeric',
+					'min:0',
+					'max:99.99',
+				],
+				'min_age' => [
+					'nullable',
+					'integer',
+					'min:0',
+				],
+				'min_avg_seedtime' => [
+					'nullable',
+					'integer',
+					'min:0',
+				],
+				'min_seedsize' => [
+					'nullable',
+					'integer',
+					'min:0',
+				],
+			];
+		}
+		return $rules;
+	}
 }
