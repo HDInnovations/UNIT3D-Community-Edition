c24be7b6b626f29e257c106499c37911fd8b25fc
maxwgod <30300032+maxwbot@users.noreply.github.com>
2024-04-08 17:11:30 -0300

    (Update) UpdateGroupRequest.php
    Resolved problem where not selecting the checkbox resulted in an empty or unfilled field error.

--- app/Http/Requests/Staff/UpdateGroupRequest.php
+++ app/Http/Requests/Staff/UpdateGroupRequest.php
@@ -33,123 +33,164 @@ class UpdateGroupRequest extends FormRequest
      *
      * @return array<string, array<\Illuminate\Validation\ConditionalRules|string>|string>
      */
-    public function rules(Request $request): array
-    {
-        /** @var Group $group */
-        $group = $request->route('group');
-
-        return [
-            'name' => [
-                Rule::when(!$group->system_required, [
-                    'required',
-                    'string',
-                ]),
-                Rule::prohibitedIf($group->system_required && $request->name !== $group->name),
-            ],
-            'position' => [
-                'required',
-                'integer',
-            ],
-            'level' => [
-                'required',
-                'integer',
-            ],
-            'download_slots' => [
-                'nullable',
-                'integer',
-            ],
-            'description' => [
-                'nullable',
-            ],
-            'color' => [
-                'required',
-            ],
-            'icon' => [
-                'required',
-            ],
-            'effect' => [
-                'sometimes',
-            ],
-            'is_internal' => [
-                'required',
-                'boolean',
-            ],
-            'is_editor' => [
-                'required',
-                'boolean',
-            ],
-            'is_modo' => [
-                'required',
-                'boolean',
-            ],
-            'is_admin' => [
-                'required',
-                'boolean',
-            ],
-            'is_owner' => [
-                'required',
-                'boolean',
-            ],
-            'is_trusted' => [
-                'required',
-                'boolean',
-            ],
-            'is_immune' => [
-                'required',
-                'boolean',
-            ],
-            'is_freeleech' => [
-                'required',
-                'boolean',
-            ],
-            'is_double_upload' => [
-                'required',
-                'boolean',
-            ],
-            'is_incognito' => [
-                'required',
-                'boolean',
-            ],
-            'can_upload' => [
-                'required',
-                'boolean',
-            ],
-            'autogroup' => [
-                'required',
-                'boolean',
-            ],
-            'min_uploaded' => [
-                'sometimes',
-                'nullable',
-                'integer',
-                'min:0',
-            ],
-            'min_ratio' => [
-                'sometimes',
-                'nullable',
-                'min:0',
-                'max:99.99',
-            ],
-            'min_age' => [
-                'sometimes',
-                'nullable',
-                'integer',
-                'min:0',
-            ],
-            'min_avg_seedtime' => [
-                'sometimes',
-                'nullable',
-                'integer',
-                'min:0',
-            ],
-            'min_seedsize' => [
-                'sometimes',
-                'nullable',
-                'integer',
-                'min:0',
-            ],
-        ];
+	public function rules(Request $request): array
+	{
+		/** @var Group $group */
+		$group = $request->route('group');
+		
+    if (!$request->has('autogroup')) {
+        $request->merge([
+            'min_uploaded' => 0,
+            'min_ratio' => 0,
+            'min_age' => 0,
+            'min_avg_seedtime' => 0,
+            'min_seedsize' => 0,
+        ]);
     }
+	
+		$rules = [
+			'name' => [
+				Rule::when(!$group->system_required, [
+					'required',
+					'string',
+				]),
+				Rule::prohibitedIf($group->system_required && $request->name !== $group->name),
+			],
+			'position' => [
+				'required',
+				'integer',
+			],
+			'level' => [
+				'required',
+				'integer',
+			],
+			'download_slots' => [
+				'nullable',
+				'integer',
+			],
+			'description' => [
+				'nullable',
+			],
+			'color' => [
+				'required',
+			],
+			'icon' => [
+				'required',
+			],
+			'effect' => [
+				'nullable',
+			],
+			'is_internal' => [
+				'required',
+				'boolean',
+			],
+			'is_editor' => [
+				'required',
+				'boolean',
+			],
+			'is_modo' => [
+				'required',
+				'boolean',
+			],
+			'is_admin' => [
+				'required',
+				'boolean',
+			],
+			'is_owner' => [
+				'required',
+				'boolean',
+			],
+			'is_trusted' => [
+				'required',
+				'boolean',
+			],
+			'is_immune' => [
+				'required',
+				'boolean',
+			],
+			'is_freeleech' => [
+				'required',
+				'boolean',
+			],
+			'is_double_upload' => [
+				'required',
+				'boolean',
+			],
+			'is_incognito' => [
+				'required',
+				'boolean',
+			],
+			'can_upload' => [
+				'required',
+				'boolean',
+			],
+			'autogroup' => [
+				'required',
+				'boolean',
+			],
+		];
+		
+		if ($this->input('autogroup')) {
+			$rules += [
+				'min_uploaded' => [
+					'required',
+					'integer',
+					'min:0',
+				],
+				'min_ratio' => [
+					'required',
+					'numeric',
+					'min:0',
+					'max:99.99',
+				],
+				'min_age' => [
+					'required',
+					'integer',
+					'min:0',
+				],
+				'min_avg_seedtime' => [
+					'required',
+					'integer',
+					'min:0',
+				],
+				'min_seedsize' => [
+					'required',
+					'integer',
+					'min:0',
+				],
+			];
+		} else {
+			$rules += [
+				'min_uploaded' => [
+					'nullable',
+					'integer',
+					'min:0',
+				],
+				'min_ratio' => [
+					'nullable',
+					'numeric',
+					'min:0',
+					'max:99.99',
+				],
+				'min_age' => [
+					'nullable',
+					'integer',
+					'min:0',
+				],
+				'min_avg_seedtime' => [
+					'nullable',
+					'integer',
+					'min:0',
+				],
+				'min_seedsize' => [
+					'nullable',
+					'integer',
+					'min:0',
+				],
+			];
+		}
+		return $rules;
+	}
 
     /**
      * Get the error messages for the defined validation rules.
