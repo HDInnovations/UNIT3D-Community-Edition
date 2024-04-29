6cbffb2f6f3cb7e78e21dd06c44f0ba47034f778
HDVinnie <hdinnovations@protonmail.com>
2024-04-29 11:01:10 -0400

    update: AutoTorrentBalance.php
    - Use DB facade instead of model for performance.
    - Wrap update query in a database transaction to help against deadlocks with external tracker.
    - Better documentation.

--- app/Console/Commands/AutoTorrentBalance.php
+++ app/Console/Commands/AutoTorrentBalance.php
@@ -13,10 +13,17 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Torrent;
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Throwable;
 
+/**
+ * Class AutoTorrentBalance.
+ *
+ * This class is responsible for calculating the balance for all torrents.
+ * It is a console command that can be executed manually or scheduled.
+ */
 class AutoTorrentBalance extends Command
 {
     /**
@@ -35,22 +42,33 @@ class AutoTorrentBalance extends Command
 
     /**
      * Execute the console command.
+     *
+     * This method is the entry point of the command. It wraps the balance calculation
+     * in a database transaction and retries up to 5 times in case of a deadlock.
+     *
+     * @throws Exception|Throwable If there is an error during the execution of the command.
      */
     public function handle(): void
     {
-        Torrent::joinSub(
-            DB::table('history')
-                ->select('torrent_id')
-                ->selectRaw('SUM(actual_uploaded) - SUM(actual_downloaded) AS balance')
-                ->groupBy('torrent_id'),
-            'balances',
-            fn ($join) => $join->on('balances.torrent_id', '=', 'torrents.id')
-        )
-            ->update([
-                'torrents.balance' => DB::raw('balances.balance'),
-                'updated_at'       => DB::raw('updated_at'),
-            ]);
+        // Join the 'torrents' table with a subquery on the 'history' table.
+        // The subquery calculates the balance for each torrent.
+        DB::transaction(static function (): void {
+            DB::table('torrents')->joinSub(
+                DB::table('history')
+                    ->select('torrent_id')
+                    ->selectRaw('SUM(actual_uploaded) - SUM(actual_downloaded) AS balance')
+                    ->groupBy('torrent_id'),
+                'balances',
+                static fn ($join) => $join->on('balances.torrent_id', '=', 'torrents.id')
+            )
+                // Update the balance and updated_at fields in the 'torrents' table.
+                ->update([
+                    'torrents.balance' => DB::raw('balances.balance'),
+                    'updated_at'       => DB::raw('updated_at'),
+                ]);
+        }, 5); // 5 is the number of attempts if deadlock occurs.
 
+        // Output a success message to the console.
         $this->comment('Torrent balance calculations completed.');
     }
 }
