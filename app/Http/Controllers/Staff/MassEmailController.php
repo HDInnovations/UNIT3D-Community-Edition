50be17e37f5ab210c03d23711fe1a2b160365689
HDVinnie <hdinnovations@protonmail.com>
2024-04-30 14:03:57 -0400

    update: return types

--- app/Http/Controllers/Staff/MassEmailController.php
+++ app/Http/Controllers/Staff/MassEmailController.php
@@ -21,12 +21,12 @@ use App\Models\User;
 
 class MassEmailController extends Controller
 {
-    public function create()
+    public function create(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         return view('Staff.mass_email.create', ['groups' => Group::orderBy('position')->get()]);
     }
 
-    public function store(MassEmailRequest $request)
+    public function store(MassEmailRequest $request): \Illuminate\Http\RedirectResponse
     {
         $request->validated();
 
