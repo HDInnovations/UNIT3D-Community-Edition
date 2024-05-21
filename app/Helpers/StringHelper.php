d2311b6889ba0d85d3fe234d88fe339a851fa5d6
Roardom <roardom@protonmail.com>
2024-05-21 00:37:38 +0000

    remove: unused timeRemaining helper

--- app/Helpers/StringHelper.php
+++ app/Helpers/StringHelper.php
@@ -63,62 +63,6 @@ class StringHelper
         return $result."\u{a0}".$suffix;
     }
 
-    public static function timeRemaining(int $seconds): string
-    {
-        $minutes = 0;
-        $hours = 0;
-        $days = 0;
-        $weeks = 0;
-        $months = 0;
-        $years = 0;
-
-        $seconds = config('hitrun.seedtime') - $seconds;
-
-        if ($seconds == 0) {
-            return 'N/A';
-        }
-
-        while ($seconds >= 31_536_000) {
-            $years++;
-            $seconds -= 31_536_000;
-        }
-
-        while ($seconds >= 2_592_000) {
-            $months++;
-            $seconds -= 2_592_000;
-        }
-
-        while ($seconds >= 604_800) {
-            $weeks++;
-            $seconds -= 604_800;
-        }
-
-        while ($seconds >= 86_400) {
-            $days++;
-            $seconds -= 86_400;
-        }
-
-        while ($seconds >= 3_600) {
-            $hours++;
-            $seconds -= 3_600;
-        }
-
-        while ($seconds >= 60) {
-            $minutes++;
-            $seconds -= 60;
-        }
-
-        $years = ($years === 0) ? '' : $years.trans('common.abbrev-years');
-        $months = ($months === 0) ? '' : $months.trans('common.abbrev-months');
-        $weeks = ($weeks === 0) ? '' : $weeks.trans('common.abbrev-weeks');
-        $days = ($days === 0) ? '' : $days.trans('common.abbrev-days');
-        $hours = ($hours === 0) ? '' : $hours.trans('common.abbrev-hours');
-        $minutes = ($minutes === 0) ? '' : $minutes.trans('common.abbrev-minutes');
-        $seconds = ($seconds == 0) ? '' : $seconds.trans('common.abbrev-seconds');
-
-        return $years.$months.$weeks.$days.$hours.$minutes.$seconds;
-    }
-
     public static function timeElapsed(int $seconds): string
     {
         $minutes = 0;
