80b9aa32bf4fb83f4cd1e1ff5cd447f7b31665c7
Roardom <roardom@protonmail.com>
2024-06-01 15:43:35 +0000

    add: livewire top users
    More cleanly modularized using livewire computed attributes. Abuses the fact that only one of the tabs is selected and most users won't access the other tabs. This significantly reduces the time spent loading the home page when the cache is cleared.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,21 +18,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Bookmark;
-use App\Models\Comment;
 use App\Models\FeaturedTorrent;
 use App\Models\FreeleechToken;
 use App\Models\Group;
-use App\Models\History;
-use App\Models\Peer;
 use App\Models\Poll;
 use App\Models\Post;
-use App\Models\Thank;
 use App\Models\Topic;
-use App\Models\Torrent;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Exception;
 
 /**
@@ -75,7 +69,7 @@ class HomeController extends Controller
                     ->where('last_action', '>', now()->subMinutes(60))
                     ->orderByRaw('(select position from `groups` where `groups`.id = users.group_id), group_id, username')
                     ->get()
-                    ->sortBy(fn ($user) => $user->privacy?->hidden || !$user->isVisible($user, 'other', 'show_online'))
+                    ->sortBy(fn ($user) => $user->privacy?->hidden || ! $user->isVisible($user, 'other', 'show_online')),
             ),
             'groups' => cache()->remember(
                 'user-groups',
@@ -85,7 +79,7 @@ class HomeController extends Controller
                     'name',
                     'color',
                     'effect',
-                    'icon'
+                    'icon',
                 ])
                     ->oldest('position')
                     ->get()
@@ -111,141 +105,17 @@ class HomeController extends Controller
                     ->authorized(canReadTopic: true)
                     ->latest()
                     ->take(5)
-                    ->get()
+                    ->get(),
             ),
             'featured' => cache()->remember(
                 'latest_featured',
                 $expiresAt,
                 fn () => FeaturedTorrent::with([
                     'torrent' => ['resolution', 'type', 'category'],
-                    'user.group'
-                ])->get()
+                    'user.group',
+                ])->get(),
             ),
-            'poll'      => cache()->remember('latest_poll', $expiresAt, fn () => Poll::latest()->first()),
-            'uploaders' => cache()->remember(
-                'top-users:uploaders',
-                3_600,
-                fn () => Torrent::with(['user' , 'user.group'])
-                    ->select(DB::raw('user_id, COUNT(user_id) as value'))
-                    ->where('user_id', '!=', User::SYSTEM_USER_ID)
-                    ->where('anon', '=', false)
-                    ->groupBy('user_id')
-                    ->orderByDesc('value')
-                    ->take(8)
-                    ->get()
-            ),
-            'downloaders' => cache()->remember(
-                'top-users:downloaders',
-                3_600,
-                fn () => History::with(['user' , 'user.group'])
-                    ->select(DB::raw('user_id, count(distinct torrent_id) as value'))
-                    ->whereNotNull('completed_at')
-                    ->where('user_id', '!=', User::SYSTEM_USER_ID)
-                    ->groupBy('user_id')
-                    ->orderByDesc('value')
-                    ->take(8)
-                    ->get()
-            ),
-            'uploaded' => cache()->remember(
-                'top-users:uploaded',
-                3_600,
-                fn () => User::select(['id', 'group_id', 'username', 'uploaded', 'image'])
-                    ->where('id', '!=', User::SYSTEM_USER_ID)
-                    ->whereNotIn('group_id', Group::select('id')->whereIn('slug', ['banned', 'validating', 'disabled', 'pruned']))
-                    ->orderByDesc('uploaded')
-                    ->take(8)
-                    ->get(),
-            ),
-            'downloaded' => cache()->remember(
-                'top-users:downloaded',
-                3_600,
-                fn () => User::select(['id', 'group_id', 'username', 'downloaded', 'image'])
-                    ->where('id', '!=', User::SYSTEM_USER_ID)
-                    ->whereNotIn('group_id', Group::select('id')->whereIn('slug', ['banned', 'validating', 'disabled', 'pruned']))
-                    ->orderByDesc('downloaded')
-                    ->take(8)
-                    ->get(),
-            ),
-            'seeders' => cache()->remember(
-                'top-users:seeders',
-                3_600,
-                fn () => Peer::with(['user' , 'user.group'])
-                    ->select(DB::raw('user_id, count(distinct torrent_id) as value'))
-                    ->where('user_id', '!=', User::SYSTEM_USER_ID)
-                    ->where('seeder', '=', 1)
-                    ->where('active', '=', 1)
-                    ->groupBy('user_id')
-                    ->orderByDesc('value')
-                    ->take(8)
-                    ->get(),
-            ),
-            'seedtimes' => cache()->remember(
-                'top-users:seedtimes',
-                3_600,
-                fn () => User::withSum('history as seedtime', 'seedtime')
-                    ->where('id', '!=', User::SYSTEM_USER_ID)
-                    ->whereNotIn('group_id', Group::select('id')->whereIn('slug', ['banned', 'validating', 'disabled', 'pruned']))
-                    ->orderByDesc('seedtime')
-                    ->take(8)
-                    ->get(),
-            ),
-            'served' => cache()->remember(
-                'top-users:served',
-                3_600,
-                fn () => User::withCount('uploadSnatches')
-                    ->where('id', '!=', User::SYSTEM_USER_ID)
-                    ->whereNotIn('group_id', Group::select('id')->whereIn('slug', ['banned', 'validating', 'disabled', 'pruned']))
-                    ->orderByDesc('upload_snatches_count')
-                    ->take(8)
-                    ->get(),
-            ),
-            'commenters' => cache()->remember(
-                'top-users:commenters',
-                3_600,
-                fn () => Comment::with(['user' , 'user.group'])
-                    ->select(DB::raw('user_id, COUNT(user_id) as value'))
-                    ->where('user_id', '!=', User::SYSTEM_USER_ID)
-                    ->where('anon', '=', false)
-                    ->groupBy('user_id')
-                    ->orderByRaw('COALESCE(value, 0) DESC')
-                    ->take(8)
-                    ->get()
-            ),
-            'posters' => cache()->remember(
-                'top-users:posters',
-                3_600,
-                fn () => Post::with(['user' , 'user.group'])
-                    ->select(DB::raw('user_id, COUNT(user_id) as value'))
-                    ->where('user_id', '!=', User::SYSTEM_USER_ID)
-                    ->groupBy('user_id')
-                    ->orderByRaw('COALESCE(value, 0) DESC')
-                    ->take(8)
-                    ->get()
-            ),
-            'thankers' => cache()->remember(
-                'top-users:thankers',
-                3_600,
-                fn () => Thank::with(['user' , 'user.group'])
-                    ->select(DB::raw('user_id, COUNT(user_id) as value'))
-                    ->where('user_id', '!=', User::SYSTEM_USER_ID)
-                    ->groupBy('user_id')
-                    ->orderByRaw('COALESCE(value, 0) DESC')
-                    ->take(8)
-                    ->get()
-            ),
-            'personals' => cache()->remember(
-                'top-users:personals',
-                3_600,
-                fn () => Torrent::with(['user' , 'user.group'])
-                    ->select(DB::raw('user_id, COUNT(user_id) as value'))
-                    ->where('user_id', '!=', User::SYSTEM_USER_ID)
-                    ->where('anon', '=', false)
-                    ->where('personal_release', '=', 1)
-                    ->groupBy('user_id')
-                    ->orderByDesc('value')
-                    ->take(8)
-                    ->get()
-            ),
+            'poll'             => cache()->remember('latest_poll', $expiresAt, fn () => Poll::latest()->first()),
             'freeleech_tokens' => FreeleechToken::where('user_id', $user->id)->get(),
             'bookmarks'        => Bookmark::where('user_id', $user->id)->get(),
         ]);
