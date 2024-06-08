782cc729d287da81bad416b1e44c469df51873a9
Audionut <audionut11@gmail.com>
2024-06-08 21:59:44 +1000

    Change to defined property

--- app/Http/Resources/TorrentResource.php
+++ app/Http/Resources/TorrentResource.php
@@ -64,7 +64,7 @@ class TorrentResource extends JsonResource
                 'seeders'          => $this->seeders,
                 'leechers'         => $this->leechers,
                 'times_completed'  => $this->times_completed,
-                'status'           => route('users.peers.index', ['id' => $this->id, 'seeding' => auth('api')->user()->seeding]),
+                'status'           => route('users.peers.index', ['id' => $this->id, 'seeding' => auth('api')->user()->seedingTorrents]),
                 'tmdb_id'          => $this->tmdb,
                 'imdb_id'          => $this->imdb,
                 'tvdb_id'          => $this->tvdb,
