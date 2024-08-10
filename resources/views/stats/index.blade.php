2f7cb5aaa8093351c8b11cb5746b4c6908d046ff
Roardom <roardom@protonmail.com>
2024-08-10 21:07:34 +0000

    fix: viewing stats page on fresh site
    Null values caused by the sum of no records existing will error when passed into the formatBytes function.

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -130,27 +130,31 @@
             <dl class="key-value">
                 <div class="key-value__group">
                     <dt>{{ __('stat.real') }} {{ __('stat.total-upload') }}</dt>
-                    <dd>{{ \App\Helpers\StringHelper::formatBytes($actual_upload, 2) }}</dd>
+                    <dd>{{ \App\Helpers\StringHelper::formatBytes($actual_upload ?? 0, 2) }}</dd>
                 </div>
                 <div class="key-value__group">
                     <dt>{{ __('stat.real') }} {{ __('stat.total-download') }}</dt>
-                    <dd>{{ \App\Helpers\StringHelper::formatBytes($actual_download, 2) }}</dd>
+                    <dd>{{ \App\Helpers\StringHelper::formatBytes($actual_download ?? 0, 2) }}</dd>
                 </div>
                 <div class="key-value__group">
                     <dt>{{ __('stat.real') }} {{ __('stat.total-traffic') }}</dt>
-                    <dd>{{ \App\Helpers\StringHelper::formatBytes($actual_up_down, 2) }}</dd>
+                    <dd>{{ \App\Helpers\StringHelper::formatBytes($actual_up_down ?? 0, 2) }}</dd>
                 </div>
                 <div class="key-value__group">
                     <dt>{{ __('stat.credited') }} {{ __('stat.total-upload') }}</dt>
-                    <dd>{{ \App\Helpers\StringHelper::formatBytes($credited_upload, 2) }}</dd>
+                    <dd>{{ \App\Helpers\StringHelper::formatBytes($credited_upload ?? 0, 2) }}</dd>
                 </div>
                 <div class="key-value__group">
                     <dt>{{ __('stat.credited') }} {{ __('stat.total-download') }}</dt>
-                    <dd>{{ \App\Helpers\StringHelper::formatBytes($credited_download, 2) }}</dd>
+                    <dd>
+                        {{ \App\Helpers\StringHelper::formatBytes($credited_download ?? 0, 2) }}
+                    </dd>
                 </div>
                 <div class="key-value__group">
                     <dt>{{ __('stat.credited') }} {{ __('stat.total-traffic') }}</dt>
-                    <dd>{{ \App\Helpers\StringHelper::formatBytes($credited_up_down, 2) }}</dd>
+                    <dd>
+                        {{ \App\Helpers\StringHelper::formatBytes($credited_up_down ?? 0, 2) }}
+                    </dd>
                 </div>
             </dl>
         </section>
