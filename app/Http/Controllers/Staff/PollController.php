ee55abcd3ba6eeebf6f53bda6898ea66b40713d9
Roardom <roardom@protonmail.com>
2024-08-10 09:42:15 +0000

    refactor: use relational upserts
    New feature added to laravel 11.18-11.19 that results in cleaner code.

--- app/Http/Controllers/Staff/PollController.php
+++ app/Http/Controllers/Staff/PollController.php
@@ -19,7 +19,6 @@ namespace App\Http\Controllers\Staff;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePoll;
 use App\Http\Requests\UpdatePollRequest;
-use App\Models\Option;
 use App\Models\Poll;
 use App\Repositories\ChatRepository;
 use Exception;
@@ -71,7 +70,8 @@ class PollController extends Controller
     public function store(StorePoll $request): \Illuminate\Http\RedirectResponse
     {
         $poll = Poll::create(['user_id' => $request->user()->id] + $request->safe()->only(['title', 'expires_at', 'multiple_choice']));
-        Option::upsert(array_map(fn ($item) => ['poll_id' => $poll->id] + $item, $request->safe()->only(['options'])['options']), ['id'], []);
+
+        $poll->options()->upsert($request->validated('options'), ['id'], []);
 
         $this->chatRepository->systemMessage(
             \sprintf('A new poll has been created [url=%s]%s[/url] vote on it now!', href_poll($poll), $poll->title)
@@ -101,9 +101,10 @@ class PollController extends Controller
         $poll->update($request->safe()->only(['title', 'expires_at', 'multiple_choice']));
 
         $poll->options()
-            ->whereNotIn('id', Arr::flatten($request->safe()->only(['options.*.id'])))
+            ->whereNotIn('id', Arr::flatten($request->validated('options.*.id')))
             ->delete();
-        Option::upsert(array_map(fn ($item) => ['poll_id' => $poll->id] + $item, $request->safe()->only(['options'])['options']), ['id'], ['name']);
+
+        $poll->options()->upsert($request->validated('options'), ['id'], ['name']);
 
         $this->chatRepository->systemMessage(
             \sprintf('A poll has been updated [url=%s]%s[/url] vote on it now!', href_poll($poll), $poll->title)
