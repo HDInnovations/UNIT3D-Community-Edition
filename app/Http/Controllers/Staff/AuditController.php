66b82e6ae13fddd7a446f74c158412aae28826a8
HDVinnie <hdinnovations@protonmail.com>
2024-08-13 20:57:06 -0400

    add: staff audit activity

--- app/Http/Controllers/Staff/AuditController.php
+++ app/Http/Controllers/Staff/AuditController.php
@@ -30,7 +30,19 @@ class AuditController extends Controller
      */
     public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\View\View
     {
-        return view('Staff.audit.index');
+        $staffActivities = Audit::with('user')
+            ->whereHas('user.group', function ($query): void {
+                $query->where('is_modo', true);
+            })
+            ->where('action', '!=', 'create') // Exclude audits with action 'create'
+            ->select('user_id')
+            ->selectRaw('COUNT(*) as total_actions')
+            ->selectRaw('SUM(CASE WHEN created_at > NOW() - INTERVAL 60 DAY THEN 1 ELSE 0 END) as last_60_days')
+            ->selectRaw('SUM(CASE WHEN created_at > NOW() - INTERVAL 30 DAY THEN 1 ELSE 0 END) as last_30_days')
+            ->groupBy('user_id')
+            ->get();
+
+        return view('Staff.audit.index', ['staffActivities' => $staffActivities]);
     }
 
     /**
--- resources/views/Staff/audit/index.blade.php
+++ resources/views/Staff/audit/index.blade.php
@@ -22,5 +22,33 @@
 @section('page', 'page__audit-log--index')
 
 @section('main')
+    <section class="panelV2">
+        <h2 class="panel__heading">{{ __('common.staff') }} {{ __('common.stats') }}</h2>
+        <div class="data-table-wrapper">
+            <table class="data-table">
+                <thead>
+                    <tr>
+                        <th>{{ __('common.name') }}</th>
+                        <th>{{ __('stat.last30days') }}</th>
+                        <th>{{ __('stat.last60days') }}</th>
+                        <th>{{ __('stat.all-time') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($staffActivities as $activity)
+                        <tr>
+                            <td>
+                                <x-user_tag :anon="false" :user="$activity->user" />
+                            </td>
+                            <td>{{ $activity->last_30_days }}</td>
+                            <td>{{ $activity->last_60_days }}</td>
+                            <td>{{ $activity->total_actions }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+
     @livewire('audit-log-search')
 @endsection
