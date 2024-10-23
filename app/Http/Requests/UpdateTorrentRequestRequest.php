7be91a0606de64ebe2c12492ee78ae91845e4d2c
thepolishplumber <nomail@nomail.com>
2024-10-23 06:17:51 +0200

    fix: update function return type signature

--- app/Http/Requests/UpdateTorrentRequestRequest.php
+++ app/Http/Requests/UpdateTorrentRequestRequest.php
@@ -34,7 +34,7 @@ class UpdateTorrentRequestRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array<\Illuminate\Contracts\Validation\Rule|string>|string>
+     * @return array<string, array<\Illuminate\Validation\ConditionalRules|string>|string>
      */
     public function rules(Request $request): array
     {
