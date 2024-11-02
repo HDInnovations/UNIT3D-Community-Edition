282d2f25270db6e8fb387d7165e784f942b7238c
kolankaya <152019802+kolankaya@users.noreply.github.com>
2024-11-02 12:59:40 +0100

    Create tr event.php

---
+++ lang/tr/event.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Roardom <roardom@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'add-event'  => 'Etkinlik ekle',
+    'add-prize'  => 'Ödül Ekle',
+    'edit-event' => 'Etkinlik düzenle',
+    'edit-prize' => 'Ödülü düzenle',
+    'ends-at'    => 'Etkinlik bitiş',
+    'events'     => 'Etkinlikler',
+    'minimum'    => 'En az',
+    'maximum'    => 'En çok',
+    'no-prizes'  => 'Ödül yok.',
+    'prize'      => 'Ödül',
+    'prizes'     => 'Ödüller',
+    'starts-at'  => 'Etkinlik başlama',
+    'weight'     => 'Önem derecesi',
+];
