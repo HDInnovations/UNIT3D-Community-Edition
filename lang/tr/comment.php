e329ed044a3b42d802b17bd4f66f4e92a9c9a842
kolankaya <152019802+kolankaya@users.noreply.github.com>
2024-11-02 12:55:34 +0100

    Create tr comment.php

---
+++ lang/tr/comment.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'added'          => 'Yorumun eklendi!',
+    'deleted'        => 'Yorum silindi',
+    'edited'         => 'Yorum düzenlendi',
+    'rights-revoked' => 'Yorum hakkın yasaklandı!',
+    'slow-down'      => 'Çok fazla yorum yazdın, biraz bekle!',
+    'torrent-status' => 'Yorum yapman için Torrent yönetimce onaylanmalı',
+];
