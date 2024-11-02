5bd5ed615950fb07ad183a522af164f7063f4e50
kolankaya <152019802+kolankaya@users.noreply.github.com>
2024-11-02 13:37:46 +0100

    Create tr ticket.php

---
+++ lang/tr/ticket.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'assign'               => 'Çözüm için birini seç',
+    'assigned-staff'       => 'Görev atanmış ekip',
+    'assigned-success'     => 'Yardım-çözüm ataması başarılı!',
+    'attachment-limit'     => '(görsel başına en fazla boyut 1 MB)',
+    'attachments'          => 'Ekler',
+    'attachments-save'     => 'Ekleri kaydet',
+    'body'                 => 'Açıklama',
+    'body-enter'           => 'Açıklamayı yazın...',
+    'category'             => 'Kategori',
+    'close'                => 'Çözdüysen Kapat',
+    'closed'               => 'Kapandı-çözüldü',
+    'closed-success'       => 'Yardım isteği başarıyla cevaplandı!',
+    'create-ticket'        => 'Yardım isteği oluştur',
+    'created'              => 'Oluşturuldu',
+    'created-success'      => 'Yardım isteği başarıyla oluşturuldu!',
+    'delete'               => 'İsteği sil',
+    'deleted-success'      => 'Yardım isteği başarıyla silindi!',
+    'description'          => 'Açıklama',
+    'download'             => 'İndir',
+    'fix-errors'           => 'Lütfen aşağıdaki hataları düzeltip tekrar deneyin:',
+    'helpdesk'             => 'Yardım-Destek Bölümü',
+    'no-attach'            => 'Ek bulunamadı',
+    'note-create-success'  => 'Yönetim notu kaydedildi!',
+    'note-destroy-success' => 'Yönetim notu silindi!',
+    'opened-by'            => 'Kim açtı:',
+    'priority'             => 'Öncelik',
+    'reopen'               => 'Tekrar aç',
+    'reopen-success'       => 'Yardım-destek isteğin tekrar oluşturuldu',
+    'reset'                => 'Resetle',
+    'staff-notes'          => 'Yönetim notları',
+    'subject'              => 'Konu',
+    'subject-enter'        => 'Konuyu yazın...',
+    'submit-ticket'        => 'Yardım-destek isteğini gönder',
+    'ticket'               => 'İstek',
+    'unassign'             => 'Yardım atamasını kaldır',
+    'unassigned-success'   => 'Yardım ataması kaldırıldı!',
+    'updated-success'      => 'Yardım-destek talebin güncellendi!',
+];
