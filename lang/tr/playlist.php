bad35782ada4daf9bea0eb7a802f58e3de147e42
kolankaya <152019802+kolankaya@users.noreply.github.com>
2024-11-02 13:26:49 +0100

    Create tr playlist.php

---
+++ lang/tr/playlist.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'about'             => 'Kullanıcı beğenisine göre oluşturulan oynatma listelerini burda bulabilirsin!!',
+    'attached-success'  => 'Torrent oynatma listene başarıyla eklendi.',
+    'add-to-playlist'   => 'Torrenti oynatma listene ekle',
+    'add-torrent'       => 'Torrent ekle',
+    'added-by'          => 'Tarafından eklendi:',
+    'cover'             => 'Kapak görseli seç',
+    'create'            => 'Yeni oynatma listesi oluştur',
+    'delete-playlist'   => 'Oynatma listesini sil',
+    'deleted'           => 'Oynatma listesi silindi!',
+    'detached-success'  => 'Torrent, oynatma listenden çıkarıldı.',
+    'desc'              => 'Oynatma lsitesi açıklaması',
+    'download-all'      => 'Tüm oynatma listesi torrentleri indir',
+    'edit-playlist'     => 'Oynatma listesini düzenle',
+    'list-by'           => 'Listeyi yapan:',
+    'list-about'        => 'Liste hakkında:',
+    'is-private'        => 'Özel oynatma listesi?',
+    'playlists'         => 'Oynatma listeleri',
+    'private-error'     => 'Bu özel oynatma listesidir. Erişim hakkın yok!',
+    'published-success' => 'Oynatma listen başarıyla oluşturuldu!',
+    'title'             => 'Başlık',
+    'titles'            => 'Başlıklar',
+    'update-success'    => 'Oynatma listen başarıyla güncellendi!',
+];
