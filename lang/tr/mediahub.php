661f07cf6f4a6aaa5539797459fc3b3b6a15bdd1
kolankaya <152019802+kolankaya@users.noreply.github.com>
2024-11-02 13:22:44 +0100

    Create tr mediahub.php

---
+++ lang/tr/mediahub.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Howard Lau <howardlau1999@hotmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'born'            => 'Çıkış:',
+    'collections'     => 'Koleksiyon',
+    'companies'       => 'Şirket',
+    'disclaimer'      => 'Bu ürün TMDB API kullanır fakat TMDB tarafından onaylı veya sertifikalı değildir.',
+    'episodes'        => 'Bölümler',
+    'first-seen'      => 'İlk yayınlanma:',
+    'genres'          => 'Tür',
+    'includes'        => 'Includes:',
+    'latest-project'  => 'En sonuncu proje:',
+    'networks'        => 'Ağlar',
+    'no-data'         => 'Bilgi bulunamadı!',
+    'movie'           => 'Film',
+    'movies'          => 'Filmler',
+    'movie-credits'   => 'Film künyesi:',
+    'persons'         => 'Kişiler',
+    'plot'            => 'Özet:',
+    'release-date'    => 'Yayın tarihi:',
+    'seasons'         => 'Sezonlar',
+    'select-hub'      => 'Bir ağ seç',
+    'show'            => 'TV Dizisi',
+    'shows'           => 'TV Dizileri',
+    'title'           => 'MediaHub',
+    'tv-credits'      => 'TV künyesi:',
+    'view-collection' => 'Koleksiyonu gör',
+    'wiki-read'       => 'Wikipedia dan detaylı bilgi oku:',
+];
