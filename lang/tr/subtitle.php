378080dcf82ed8cad09e722f15de191bf2f42114
kolankaya <152019802+kolankaya@users.noreply.github.com>
2024-11-02 13:36:51 +0100

    Create tr subtitle.php

---
+++ lang/tr/subtitle.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Howard Lau <howardlau1999@hotmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    'downloads'      => 'İndirilenler',
+    'extension'      => 'Eklentiler',
+    'delete-confirm' => 'Bunu silmek istediğin emin misin?',
+    'note'           => 'Not',
+    'note-help'      => 'Bu altyazı için extra bilgi',
+    'rules-title'    => 'Altyazı kuralları',
+    'rules'          => '<ul>
+                                        <li>Sadece uygun altyazılar kabul edilir (doğru frame rate, fps, zaman ayarı, doğru çeviri, yazım-imla kurallarına uyum)</li>
+                                        <li>Google tranlate-makine çeviriler kabul edilmez</li>
+                                        <li>Altyazılar video ile senkron olmalıdır</li>
+                                        <li>ZIP arşiv dosyalarına sadece SUB+IDX için müsade edilir veya bir dizi boxset, sezon paketi gibi olanlar için kabul edilir</li>
+                                        <li>spam altyazıların sürekli yüklenmesi yasaktır ve ceza verilir</li>
+                                        <li>Altyazı notunu kısa tutmaya çalışın, url/link kullanımı yasaktır</li>
+                                      </ul>',
+    'size'                => 'Boyut',
+    'subtitle-file'       => 'Altyazı dosyası',
+    'subtitle-file-types' => 'SRT, ASS, SUP and ZIP formatlar kabul edilir',
+    'uploaded'            => 'Yüklendi',
+    'uploader'            => 'Yükleyen',
+];
