146e7f66fa56db6c40b99d121930e9f6a4b71469
HDVinnie <hdinnovations@protonmail.com>
2024-11-24 10:07:14 -0500

    update: unit3d config
    Bump version

--- config/unit3d.php
+++ config/unit3d.php
@@ -23,7 +23,7 @@ return [
     |
     */
 
-    'powered-by' => 'Powered By UNIT3D Community Edition v8.3.3',
+    'powered-by' => 'Powered By UNIT3D Community Edition v8.3.4',
 
     /*
     |--------------------------------------------------------------------------
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'version' => 'v8.3.3',
+    'version' => 'v8.3.4',
 
     /*
     |--------------------------------------------------------------------------
