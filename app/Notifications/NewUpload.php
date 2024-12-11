c196c703734ba8727537addcfb70063375da0417
Jay Sizzla <aithercc_heaven@protonmail.com>
2024-12-11 15:01:12 +0000

    Use shouldSend for NewUpload notifications

--- app/Notifications/NewUpload.php
+++ app/Notifications/NewUpload.php
@@ -16,6 +16,7 @@ declare(strict_types=1);
 
 namespace App\Notifications;
 
+use App\Models\User;
 use App\Models\Torrent;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -40,6 +41,33 @@ class NewUpload extends Notification implements ShouldQueue
     public function via(object $notifiable): array
     {
         return ['database'];
+    }
+
+    /**
+     * Determine if the notification should be sent.
+     *
+     * @return bool
+     */
+    public function shouldSend(User $notifiable): bool
+    {
+        $targetGroup = 'json_following_groups';
+
+        // Do not notify the uploader theirself
+        if ($this->torrent->user_id === $notifiable->id) {
+            return false;
+        }
+
+        if ($notifiable->notification?->block_notifications == 1) {
+            return false;
+        }
+
+        if (!$notifiable->notification?->show_following_upload) {
+            return false;
+        }
+
+        // If the sender's group ID is found in the "Block all notifications from the selected groups" array,
+        // the expression will return false.
+        return ! \in_array($this->torrent->user->group_id, $notifiable->notification->json_following_groups, true);
     }
 
     /**
