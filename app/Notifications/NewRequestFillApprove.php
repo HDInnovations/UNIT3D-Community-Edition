8753950e6011a8bbbc3845c0200a24905ce60eba
Jay Sizzla <aithercc_heaven@protonmail.com>
2024-12-18 13:48:28 +0000

    Use shouldSend for NewRequestFillApprove notifications

--- app/Notifications/NewRequestFillApprove.php
+++ app/Notifications/NewRequestFillApprove.php
@@ -16,6 +16,7 @@ declare(strict_types=1);
 
 namespace App\Notifications;
 
+use App\Models\User;
 use App\Models\TorrentRequest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -40,6 +41,32 @@ class NewRequestFillApprove extends Notification implements ShouldQueue
     public function via(object $notifiable): array
     {
         return ['database'];
+    }
+
+    /**
+     * Determine if the notification should be sent.
+     *
+     * @return bool
+     */
+    public function shouldSend(User $notifiable): bool
+    {
+        $targetGroup = 'json_request_groups';
+
+        if ($notifiable->notification?->block_notifications == 1) {
+            return false;
+        }
+
+        if (!$notifiable->notification?->show_request_fill_approve) {
+            return false;
+        }
+
+        if (\is_array($notifiable->notification->$targetGroup)) {
+            // If the sender's group ID is found in the "Block all notifications from the selected groups" array,
+            // the expression will return false.
+            return !\in_array($this->torrentRequest->approver->group->id, $notifiable->notification->$targetGroup, true);
+        }
+
+        return true;
     }
 
     /**
