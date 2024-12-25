e5105b2011ef33450886d836366d353606b142be
Jay Sizzla <aithercc_heaven@protonmail.com>
2024-12-25 15:09:03 +0000

    Use shouldSend for NewPostTag notifications

--- app/Notifications/NewPostTag.php
+++ app/Notifications/NewPostTag.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 namespace App\Notifications;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
@@ -40,6 +41,32 @@ class NewPostTag extends Notification implements ShouldQueue
     public function via(object $notifiable): array
     {
         return ['database'];
+    }
+
+    /**
+     * Determine if the notification should be sent.
+     *
+     * @return bool
+     */
+    public function shouldSend(User $notifiable): bool
+    {
+        $targetGroup = 'json_mention_groups';
+
+        if ($notifiable->notification?->block_notifications == 1) {
+            return false;
+        }
+
+        if (!$notifiable->notification?->show_mention_forum_post) {
+            return false;
+        }
+
+        if (\is_array($notifiable->notification->$targetGroup)) {
+            // If the sender's group ID is found in the "Block all notifications from the selected groups" array,
+            // the expression will return false.
+            return !\in_array($this->post->user->group->id, $notifiable->notification->$targetGroup, true);
+        }
+
+        return true;
     }
 
     /**
