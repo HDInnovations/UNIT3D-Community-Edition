e33134e549321bfe055357eecb0bd3e6259f0f2d
Roardom <roardom@protonmail.com>
2025-01-29 15:41:52 +0000

    update: use only orderable meilisearch filters when necessary
    Should improve indexing performance.
    
    Depends on upstream meilisearch change scheduled for v1.14: https://www.github.com/meilisearch/meilisearch/issues/5163

--- app/DTO/TorrentSearchFiltersDTO.php
+++ app/DTO/TorrentSearchFiltersDTO.php
@@ -607,7 +607,7 @@ readonly class TorrentSearchFiltersDTO
         }
 
         if ($this->alive) {
-            $filters[] = 'seeders > 0';
+            $filters[] = 'seeders != 0';
         }
 
         if ($this->dying) {
