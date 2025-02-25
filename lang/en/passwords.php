cad18eff81d88030d73a96c0dc044ef67aea4090
Shift <shift@laravelshift.com>
2025-02-25 12:56:27 +0000

    Slim `lang` files

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -28,6 +28,6 @@ return [
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset'    => 'Your password has been reset!',
     'sent'     => 'Thanks! If the email address matches an account, a password reset link will be sent.',
-    'token'    => 'This password reset token is invalid.',
+
     'user'     => 'We can\'t find a user with that e-mail address.',
 ];
--- lang/en/validation.php
+++ lang/en/validation.php
@@ -42,9 +42,9 @@ return [
         'string'  => 'The :attribute must be between :min and :max characters.',
         'array'   => 'The :attribute must have between :min and :max items.',
     ],
-    'boolean'          => 'The :attribute field must be true or false.',
+
     'confirmed'        => 'The :attribute confirmation does not match.',
-    'current_password' => 'The password is incorrect.',
+
     'date'             => 'The :attribute is not a valid date.',
     'date_equals'      => 'The :attribute must be a date equal to :date.',
     'date_format'      => 'The :attribute does not match the format :format.',
@@ -54,13 +54,12 @@ return [
     'digits'           => 'The :attribute must be :digits digits.',
     'digits_between'   => 'The :attribute must be between :min and :max digits.',
     'dimensions'       => 'The :attribute has invalid image dimensions.',
-    'distinct'         => 'The :attribute field has a duplicate value.',
+
     'email'            => 'The :attribute must be a valid email address.',
     'ends_with'        => 'The :attribute must end with one of the following: :values.',
-    'enum'             => 'The selected :attribute is invalid.',
-    'exists'           => 'The selected :attribute is invalid.',
+
     'file'             => 'The :attribute must be a file.',
-    'filled'           => 'The :attribute field must have a value.',
+
     'gt'               => [
         'numeric' => 'The :attribute must be greater than :value.',
         'file'    => 'The :attribute must be greater than :value kilobytes.',
@@ -74,7 +73,7 @@ return [
         'array'   => 'The :attribute must have :value items or more.',
     ],
     'image'       => 'The :attribute must be an image.',
-    'in'          => 'The selected :attribute is invalid.',
+
     'in_array'    => 'The :attribute field does not exist in :other.',
     'integer'     => 'The :attribute must be an integer.',
     'ip'          => 'The :attribute must be a valid IP address.',
@@ -109,7 +108,7 @@ return [
         'array'   => 'The :attribute must have at least :min items.',
     ],
     'multiple_of' => 'The :attribute must be a multiple of :value.',
-    'not_in'      => 'The selected :attribute is invalid.',
+
     'not_regex'   => 'The :attribute format is invalid.',
     'numeric'     => 'The :attribute must be a number.',
     'password'    => [
@@ -117,21 +116,11 @@ return [
         'mixed'         => 'The :attribute must contain at least one uppercase and one lowercase letter.',
         'numbers'       => 'The :attribute must contain at least one number.',
         'symbols'       => 'The :attribute must contain at least one symbol.',
-        'uncompromised' => 'The given :attribute has appeared in a data leak. Please choose a different :attribute.',
+
     ],
-    'present'              => 'The :attribute field must be present.',
-    'prohibited'           => 'The :attribute field is prohibited.',
-    'prohibited_if'        => 'The :attribute field is prohibited when :other is :value.',
-    'prohibited_unless'    => 'The :attribute field is prohibited unless :other is in :values.',
-    'prohibits'            => 'The :attribute field prohibits :other from being present.',
+
     'regex'                => 'The :attribute format is invalid.',
-    'required'             => 'The :attribute field is required.',
-    'required_if'          => 'The :attribute field is required when :other is :value.',
-    'required_unless'      => 'The :attribute field is required unless :other is in :values.',
-    'required_with'        => 'The :attribute field is required when :values is present.',
-    'required_with_all'    => 'The :attribute field is required when :values are present.',
-    'required_without'     => 'The :attribute field is required when :values is not present.',
-    'required_without_all' => 'The :attribute field is required when none of :values are present.',
+
     'same'                 => 'The :attribute and :other must match.',
     'size'                 => [
         'numeric' => 'The :attribute must be :size.',
@@ -142,8 +131,7 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string'      => 'The :attribute must be a string.',
     'timezone'    => 'The :attribute must be a valid timezone.',
-    'unique'      => 'The :attribute has already been taken.',
-    'uploaded'    => 'The :attribute failed to upload.',
+
     'url'         => 'The :attribute must be a valid URL.',
     'uuid'        => 'The :attribute must be a valid UUID.',
 
@@ -163,7 +151,7 @@ return [
 
     'custom' => [
         'attribute-name' => [
-            'rule-name' => 'custom-message',
+
         ],
     ],
 
